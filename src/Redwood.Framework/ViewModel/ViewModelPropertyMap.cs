fa30e61407119645d090064c076ddb6c861f945b
exyi <exyi@outlook.cz>
2014-12-13 08:37:27 +0000

    Added missing ViewModelPropertyMap.cs file

---
+++ src/Redwood.Framework/ViewModel/ViewModelPropertyMap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Redwood.Framework.ViewModel
+{
+    public class ViewModelPropertyMap
+    {
+        public string Name { get; set; }
+        public Type Type { get; set; }
+        public bool TransferToClient { get; set; }
+        public bool TransferToServer { get; set; }
+        public CryptoSettings Crypto { get; set; }
+        public ViewModelSerializationMap ViewModelMap { get; set; }
+    }
+}
