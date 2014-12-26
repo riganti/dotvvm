b2d2e66bf147e1a654c8fb79b50ca859d218d736
exyi <exyi@outlook.cz>
2014-12-26 17:44:37 +0100

    default cdn for jquery and bootstrap

--- src/Redwood.Framework/ConfigurationHelper.cs
+++ src/Redwood.Framework/ConfigurationHelper.cs
@@ -11,12 +11,13 @@ namespace Redwood.Framework
     {
         public static void RegisterCommonResources(this RwResourceRepository repo, 
             string jqueryLocal = "Scripts/jquery-2.1.1.min.js",
-            string jqueryCdn = "https://code.jquery.com/jquery-migrate-1.2.1.min.js",
+            string jqueryCdn = "https://code.jquery.com/jquery-2.1.3.min.js",
             string knockoutLocal = "/Scripts/knockout-3.2.0.js",
             string knockoutMapper = "/Scripts/knockout.mapper.js",
             string redwood = "/Scripts/Redwood.js",
             string bootstrapCss = "Content/bootstrap/bootstrap.min.css",
-            string bootstrapJsLocal = "Scripts/bootstrap.min.js"
+            string bootstrapJsLocal = "Scripts/bootstrap.min.js",
+            string bootstrapJsCdn = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"
             )
         {
 
@@ -25,7 +26,7 @@ namespace Redwood.Framework
             repo.Register("knockout", new ScriptResource(knockoutMapper, "knockout-core"));
             repo.Register("redwood", new ScriptResource(redwood, "knockout"));
             repo.Register("bootstrap-css", new StyleResource(bootstrapCss));
-            repo.Register("bootstrap", new ScriptResource(bootstrapJsLocal, new string[] { "bootstrap-css", "jquery" }));
+            repo.Register("bootstrap", new ScriptResource(bootstrapJsLocal, bootstrapJsCdn, "$.fn.modal", "bootstrap-css", "jquery"));
 
         }
     }
