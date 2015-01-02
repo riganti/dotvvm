d1297567e0bac2124b677793f78d75914de0fb6e
Altair <michal.valasek@altairis.cz>
2015-01-02 12:17:14 +0100

    Added ViewModel protection infrastructure (part)

---
+++ src/Redwood.Framework/Security/DefaultViewModelProtector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Redwood.Framework.Hosting;
+
+namespace Redwood.Framework.Security {
+    public class DefaultViewModelProtector : IViewModelProtector {
+        public string Protect(string serializedData, RedwoodRequestContext context) {
+            // TODO
+            return serializedData;
+        }
+
+        public string Unprotect(string protectedData, RedwoodRequestContext context) {
+            // TODO
+            return protectedData;
+        }
+    }
+}
