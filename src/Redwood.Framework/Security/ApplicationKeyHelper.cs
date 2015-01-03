733e5039fcbca4e619a0d688224f5bf0b06b58a3
Altair <michal.valasek@altairis.cz>
2015-01-03 01:23:07 +0100

    Changed internal format of protected data to be more straightforward.

--- src/Redwood.Framework/Security/ApplicationKeyHelper.cs
+++ src/Redwood.Framework/Security/ApplicationKeyHelper.cs
@@ -45,35 +45,39 @@ namespace Redwood.Framework.Security {
             byte[] derivedEncKey, derivedSigKey;
             this.GetDerivedKeys(label, context, out derivedEncKey, out derivedSigKey);
 
+            // Prepare AES-256 in CBC mode
+            byte[] iv, encryptedData, signatureData;
+            using (var aes = new RijndaelManaged()) {
+                aes.Key = derivedEncKey;
+                aes.BlockSize = 128;
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+
+                // Generate 16-byte IV and put it at beginning of output
+                aes.GenerateIV();
+                iv = aes.IV;
+
+                // Encrypt payload and write it to output
+                using (var enc = aes.CreateEncryptor()) {
+                    encryptedData = enc.TransformFinalBlock(data, 0, data.Length);
+                }
+            }
+
+            // Sign IV + encrypted data 
+            using (var mac = new HMACSHA512(derivedSigKey)) {
+                var dataToSign = new byte[iv.Length + encryptedData.Length];
+                iv.CopyTo(dataToSign, 0);
+                encryptedData.CopyTo(dataToSign, iv.Length);
+                signatureData = mac.ComputeHash(dataToSign);
+            }
+
+            // Return encrypted and signed data
             using (var ms = new MemoryStream()) {
-                // Prepare AES-256 in CBC mode
-                using (var aes = new RijndaelManaged()) {
-                    aes.Key = derivedEncKey;
-                    aes.BlockSize = 128;
-                    aes.Mode = CipherMode.CBC;
-                    aes.Padding = PaddingMode.PKCS7;
-
-                    // Generate 16-byte IV and put it at beginning of output
-                    aes.GenerateIV();
-                    ms.Write(aes.IV, 0, aes.IV.Length);
-
-                    // Encrypt payload and write it to output
-                    using (var enc = aes.CreateEncryptor()) {
-                        var encData = enc.TransformFinalBlock(data, 0, data.Length);
-                        ms.Write(encData, 0, encData.Length);
-                    }
-                }
-
-                // Sign IV + encrypted data 
-                using (var mac = new HMACSHA512(derivedSigKey)) {
-                    var sigData = mac.ComputeHash(ms.ToArray());
-
-                    // Write signature to output (last 64 bytes)
-                    ms.Write(sigData, 0, sigData.Length);
-                }
-
-                // Return encrypted and signed data
-                return ms.ToArray();
+                ms.Write(signatureData, 0, signatureData.Length);
+                ms.Write(iv, 0, iv.Length);
+                ms.Write(encryptedData, 0, iv.Length);
+                var outData = ms.ToArray();
+                return outData;
             }
         }
 
@@ -82,8 +86,14 @@ namespace Redwood.Framework.Security {
             if (label == null) throw new ArgumentNullException("label");
             if (string.IsNullOrWhiteSpace(label)) throw new ArgumentException("Value cannot be empty or whitespace only string.", "label");
 
-            var data = Convert.FromBase64String(s);
-            data = this.UnprotectData(data, label, context);
+            byte[] protectedData;
+            try {
+                protectedData = Convert.FromBase64String(s);
+            }
+            catch (Exception ex) {
+                throw new CryptographicException("Invalid signature. The data is corrupted or was being tampered with.", ex);
+            }
+            var data = this.UnprotectData(protectedData, label, context);
             return Encoding.UTF8.GetString(data);
         }
 
@@ -98,15 +108,26 @@ namespace Redwood.Framework.Security {
             byte[] derivedEncKey, derivedSigKey;
             this.GetDerivedKeys(label, context, out derivedEncKey, out derivedSigKey);
 
+            // Get data to work with
+            var signatureData = new byte[64];   // 512 bits - length of SHA512
+            Buffer.BlockCopy(data, 0, signatureData, 0, signatureData.Length);
+
+            var dataToSign = new byte[data.Length - signatureData.Length];
+            Buffer.BlockCopy(data, signatureData.Length, dataToSign, 0, dataToSign.Length);
+
+            var iv = new byte[16];  // 128 bits - length of IV in this case
+            Buffer.BlockCopy(data, signatureData.Length, iv, 0, iv.Length);
+
+            var encryptedData = new byte[data.Length - signatureData.Length - iv.Length];
+            Buffer.BlockCopy(data, signatureData.Length + iv.Length, encryptedData, 0, encryptedData.Length);
+
             // Verify signature
             using (var mac = new HMACSHA512(derivedSigKey)) {
-                // Compute signature for all but last 64 bytes of data (that's the old signature)
-                var sigData = mac.ComputeHash(data, 0, data.Length - 64);
+                // Compute signature
+                var expectedSignatureData = mac.ComputeHash(dataToSign);
 
                 // Compare computed signature with supplied one
-                var supSigData = new byte[64];
-                Buffer.BlockCopy(data, data.Length - 64, supSigData, 0, 64);
-                if (!CompareByteArrays(sigData, supSigData)) throw new CryptographicException("Invalid signature. The data is corrupted or was being tampered with.");
+                if (!expectedSignatureData.SequenceEqual(signatureData)) throw new CryptographicException("Invalid signature. The data is corrupted or was being tampered with.");
             }
 
             // Decrypt data
@@ -118,24 +139,14 @@ namespace Redwood.Framework.Security {
                 aes.Padding = PaddingMode.PKCS7;
 
                 // Read IV (first 16 bytes)
-                Buffer.BlockCopy(data, 0, aes.IV, 0, 16);
+                aes.IV = iv;
 
                 // Decrypt rest of data, except last 64 bytes (sig)
                 using (var dec = aes.CreateDecryptor()) {
-                    var decData = dec.TransformFinalBlock(data, 16, data.Length - 64 - 16);
-                    return decData;
+                    var decryptedData = dec.TransformFinalBlock(encryptedData, 0, encryptedData.Length);
+                    return decryptedData;
                 }
             }
-        }
-
-        private bool CompareByteArrays(byte[] a, byte[] b) {
-            if (a == null) throw new ArgumentNullException("a");
-            if (b == null) throw new ArgumentNullException("b");
-            if (a.Length != b.Length) return false;
-            for (int i = 0; i < a.Length; i++) {
-                if (a[i] != b[i]) return false;
-            }
-            return true;
         }
 
         private void GetDerivedKeys(string label, string[] context, out byte[] encryptionKey, out byte[] signingKey) {
@@ -152,5 +163,6 @@ namespace Redwood.Framework.Security {
                 signingKey = kdf.GetBytes(128);
             }
         }
+
     }
 }
