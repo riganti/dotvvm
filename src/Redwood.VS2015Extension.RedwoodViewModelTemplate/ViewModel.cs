b914416801a6665fb9fc4eac3d688678ece2c6e2
Tomas Herceg <tomas.herceg@riganti.cz>
2015-03-08 22:11:48 +0100

    Visual Studio Extension completed

--- src/Redwood.VS2015Extension.RedwoodViewModelTemplate/ViewModel.cs
+++ src/Redwood.VS2015Extension.RedwoodViewModelTemplate/ViewModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using Redwood.Framework.ViewModels;
 
-namespace $safeprojectname$.ViewModels
+namespace $rootnamespace$.ViewModel
 {
 	public class $safeitemname$ViewModel : RedwoodViewModelBase
 	{
