82f73232a31caf94c8074b14b894e37b71fc4237
Tomáš Hübelbauer <Tomas@Hubelbauer.net>
2015-04-20 15:00:33 +0200

    Render hashbang URLs instead of plain fragment URLs

--- src/Redwood.Framework/Resources/Scripts/Redwood.js
+++ src/Redwood.Framework/Resources/Scripts/Redwood.js
@@ -29,7 +29,11 @@ var Redwood = (function () {
         var viewModel = this.viewModels[viewModelName].viewModel;
         ko.applyBindings(viewModel, document.documentElement);
         this.events.init.trigger(new RedwoodEventArgs(viewModel));
-        if (document.location.hash.indexOf("#/") === 0) {
+        // Attempt to server Googlebot hashbang escaped fragment URL, then fall back onto classical hash to avoid breaking backwards compatibility.
+        if (document.location.hash.indexOf("#!/") === 0) {
+            this.navigateCore(viewModelName, document.location.hash.substring(2));
+        }
+        else if (document.location.hash.indexOf("#/") === 0) {
             this.navigateCore(viewModelName, document.location.hash.substring(1));
         }
         // persist the viewmodel in the hidden field so the Back button will work correctly
@@ -157,7 +161,8 @@ var Redwood = (function () {
             document.location.href = fullUrl;
             return;
         }
-        document.location.hash = url;
+        // Render the hash and exclamation point so the fragment URLs are recognized by Googlebot and reconstructed to escaped fragment URL.
+        document.location.hash = "#!" + url;
         // send the request
         var spaPlaceHolderUniqueId = spaPlaceHolder.attributes["data-rw-spacontentplaceholder"].value;
         this.getJSON(fullUrl, "GET", spaPlaceHolderUniqueId, function (result) {
