a1eebe79ffa0f47179bd0cb90a9ff6a6d7edd406
exyi <exyi@outlook.cz>
2015-05-01 13:25:33 +0200

    added canopy UI test project with two tests (Sample 10 and 11)

---
+++ src/Redwood.Samples.CanopyTests/Program.fs
@@ -0,0 +1,42 @@
+﻿open canopy
+open runner
+open types
+open System
+  
+let redwoodPath = "http://localhost:8628"
+
+
+"Redirect Sample (10)" &&& fun _ ->
+    url (redwoodPath + "/Sample10")
+    let button = "input[type='button']"
+
+    on "/Sample10?time="
+    let url1 = currentUrl()
+
+    click button
+    on "/Sample10?time="
+    if url1 = currentUrl() then raise (CanopyException("not redirected"))
+
+"Validation Sample (11)" &&& fun _ ->
+    url (redwoodPath + "/Sample11")
+    let button = "input[type='button']"
+    let input = "input[type='text']"
+    let checkNotValid() =
+        displayed "#hideWhenValid"
+        displayed "#addCssClass.validator"
+        displayed "#displayErrorMessage"
+        displayed "#validationSummary li"
+
+    click button
+    checkNotValid()
+    input << "not valid email"
+    click button
+    checkNotValid()
+    input << "email@addre.ss"
+    click button
+    count "table.table tr" 4
+
+  
+start firefox
+run()
+quit()
