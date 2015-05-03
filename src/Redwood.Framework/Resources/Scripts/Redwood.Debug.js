e6b0375687547155cd5e039827f70bf9810fe981
exyi <exyi@outlook.cz>
2015-05-03 14:11:23 +0200

    Redwood.Debug little improvements

--- src/Redwood.Framework/Resources/Scripts/Redwood.Debug.js
+++ src/Redwood.Framework/Resources/Scripts/Redwood.Debug.js
@@ -1,4 +1,6 @@
-var debugWindow = $(document.body).append("<div id='debugWindow'><h1></h1><iframe /><div id='debugFooter'><button type='button' id='closeDebugWindow'>Close</button></div></div>").find("#debugWindow");
+var debugWindow = $(document.body)
+    .append("<div id='debugWindow'><h1></h1><button type='button' id='closeDebugWindow'>Close</button><iframe /><div id='debugFooter'></div></div>")
+    .find("#debugWindow");
 debugWindow.css({
     display: "none",
     flexFlow: "column",
@@ -9,9 +11,17 @@ debugWindow.css({
     backgroundColor: "white",
     top: 0
 });
-debugWindow.find("#closeDebugWindow").click(function () { return debugWindow.css({ display: "none" }); });
-debugWindow.find("#debugFooter").css({ flex: "0 1 auto" });
-debugWindow.find("h1").css({ flex: "0 1 auto" });
+debugWindow.find("#closeDebugWindow")
+    .click(function () { return debugWindow.css({ display: "none" }); })
+    .css({
+    position: "absolute",
+    top: 5,
+    right: 5
+});
+debugWindow.find("#debugFooter")
+    .css({ flex: "0 1 auto" });
+debugWindow.find("h1")
+    .css({ flex: "0 1 auto" });
 debugWindow.find("iframe").css({
     flex: "1 1 auto",
     width: "100%"
@@ -27,11 +37,13 @@ redwood.events.error.subscribe(function (e) {
     debugWindow.css({ display: "flex" });
     e.handled = true;
 });
-redwood.events.afterPostback.subscribe(function (e) {
-    Object.defineProperty(redwood.viewModels[e.viewModelName], "$debugMap", {
+function setDebugMapProperty(obj) {
+    Object.defineProperty(obj, "$debugMap", {
         enumerable: false,
         configurable: true,
-        get: function () { return ko.mapper.toJS(redwood.viewModels[e.viewModelName]); }
+        get: function () { return ko.mapper.toJS(obj); }
     });
-});
+}
+redwood.events.afterPostback.subscribe(function (e) { return setDebugMapProperty(redwood.viewModels[e.viewModelName]); });
+redwood.events.init.subscribe(function (e) { return setDebugMapProperty(redwood.viewModels["root"]); });
 //# sourceMappingURL=Redwood.Debug.js.map
