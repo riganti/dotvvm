7540fe61e3a8bc9297c8f946652c281b7a131c31
exyi <exyi@outlook.cz>
2015-07-27 16:52:57 +0200

    support for interface methods in JavascriptTranslator and implemented Array.Length and ICollection.Count property translations resolves #89

---
+++ src/DotVVM.Framework/Runtime/Compilation/JavascriptCompilation/StringJsMethodCompiler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotVVM.Framework.Runtime.Compilation.JavascriptCompilation
+{
+    public class StringJsMethodCompiler: IJsMethodTranslator
+    {
+        public string FormatString { get; set; }
+
+        public string TranslateCall(string context, string[] arguments, MethodInfo method)
+        {
+            return string.Format(FormatString, new[] { context }.Concat(arguments).ToArray());
+        }
+
+        public StringJsMethodCompiler(string formatString)
+        {
+            FormatString = formatString;
+        }
+    }
+}
