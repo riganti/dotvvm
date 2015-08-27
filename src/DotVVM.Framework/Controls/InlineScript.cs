0c0ab881a59770476deb24be5b13802abad4c13b
exyi <exyi@outlook.cz>
2015-08-27 16:20:08 +0200

    Inline script dependencies and multiple literal support

--- src/DotVVM.Framework/Controls/InlineScript.cs
+++ src/DotVVM.Framework/Controls/InlineScript.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using DotVVM.Framework.Hosting;
 using DotVVM.Framework.Parser;
 using DotVVM.Framework.Runtime;
+using DotVVM.Framework.Binding;
 
 namespace DotVVM.Framework.Controls
 {
@@ -12,18 +13,27 @@ namespace DotVVM.Framework.Controls
     /// </summary>
     public class InlineScript : DotvvmControl
     {
-        
+
+        public string Dependencies
+        {
+            get { return (string)GetValue(DependenciesProperty); }
+            set { SetValue(DependenciesProperty, value); }
+        }
+        public static readonly DotvvmProperty DependenciesProperty =
+            DotvvmProperty.Register<string, InlineScript>(c => c.Dependencies);
+
         internal override void OnPreRenderComplete(IDotvvmRequestContext context)
         {
             EnsureControlHasId();
 
-            if (Children.Count != 1 || !(Children[0] is Literal))
+            if (!Children.All(c => c is Literal))
             {
                 throw new Exception("The <dot:InlineScript>...</dot:InlineScript> control can only contain text content!");
             }
             
-            var script = ((Literal)Children[0]).Text;
-            context.ResourceManager.AddStartupScript("inlinescript_" + ID, script, Constants.DotvvmResourceName);
+            var script = string.Concat(Children.Cast<Literal>().Select(c => c.Text));
+            var dep = Dependencies?.Split(',') ?? new string[] { Constants.DotvvmResourceName };
+            context.ResourceManager.AddStartupScript("inlinescript_" + ID, script, dep);
             
             base.OnPreRenderComplete(context);
         }
