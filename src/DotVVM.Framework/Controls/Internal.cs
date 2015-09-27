484bc58281bbbb00f88ba5625d10a389e4f15fe5
Tomas Herceg <tomas.herceg@riganti.cz>
2015-09-27 13:38:07 +0200

    Internal properties added

--- src/DotVVM.Framework/Controls/Internal.cs
+++ src/DotVVM.Framework/Controls/Internal.cs
@@ -27,6 +27,12 @@ namespace DotVVM.Framework.Controls
             DotvvmProperty.Register<string, Internal>("PathFragment");
 
         public static readonly DotvvmProperty IsCommentProperty =
-            DotvvmProperty.Register<bool, Internal>("IsComment", defaultValue: false); 
+            DotvvmProperty.Register<bool, Internal>("IsComment", defaultValue: false);
+
+        public static readonly DotvvmProperty MarkupFileNameProperty =
+            DotvvmProperty.Register<string, Internal>("MarkupFileName", isValueInherited: true);
+
+        public static readonly DotvvmProperty MarkupLineNumberProperty =
+            DotvvmProperty.Register<int, Internal>("MarkupLineNumber", defaultValue: 0);
     }
 }
