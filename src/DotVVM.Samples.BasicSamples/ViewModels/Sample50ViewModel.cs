75470f1820c2004f5d8dacfe90490b7de63333ec
Tomáš Herceg <admin@sense4code.com>
2015-11-03 21:40:08 +0100

    Sample50 added

---
+++ src/DotVVM.Samples.BasicSamples/ViewModels/Sample50ViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DotVVM.Samples.BasicSamples.ViewModels
+{
+    public class Sample50ViewModel
+    {
+
+        public Sample50Child Child { get; set; }
+
+        public Sample50ViewModel()
+        {
+            Child = new Sample50Child()
+            {
+                Child2 = new Sample50Child2()
+                {
+                    Child3 = new Sample50Child3()
+                    {
+                        Test = "Hello"
+                    }
+                }
+            };
+        }
+
+    }
+
+    public class Sample50Child
+    {
+
+        public Sample50Child2 Child2 { get; set; }
+
+    }
+
+    public class Sample50Child2
+    {
+
+        public Sample50Child3 Child3 { get; set; }
+
+
+    }
+
+    public class Sample50Child3
+    {
+
+        public string Test { get; set; }
+
+    }
+}
