0c61b551aa2356f5b6b7579ef8823256d9185451
MacakM <410452@mail.muni.cz>
2015-11-11 21:43:58 +0100

    PropertyUpdate test and sample

---
+++ src/DotVVM.Samples.Tests/Control/PropertyUpdateTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using Riganti.Utils.Testing.SeleniumCore;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace DotVVM.Samples.Tests.Control
+{
+    [TestClass]
+    public class PropertyUpdateTests : SeleniumTestBase
+    {
+        [TestMethod]
+        public void PropertyUpdateTest()
+        {
+            RunInAllBrowsers(browser =>
+            {
+                browser.NavigateToUrl("ControlSamples/PropertyUpdate");
+
+                // enter number of lines and click the button
+                browser.ClearElementsContent("input[type=text]");
+                browser.SendKeys("input[type=text]", "15");
+                browser.Click("input[type=button]");
+                browser.Wait();
+
+                browser.First("span")
+                    .CheckIfInnerText(s => Regex.Matches(s, "Lorem ipsum").Count == 15,
+                        "Output doesn't contain expected number of values");
+                //Assert.AreEqual(14, browser.FindElements("br").Count);
+
+                // change number of lines and click the button
+                browser.ClearElementsContent("input[type=text]");
+                browser.SendKeys("input[type=text]", "5");
+                browser.Click("input[type=button]");
+                browser.Wait();
+
+                browser.First("span")
+                    .CheckIfInnerText(s => Regex.Matches(s, "Lorem ipsum").Count == 5,
+                        "Output doesn't contain expected number of values");
+                //Assert.AreEqual(4, browser.FindElements("br").Count);
+            });
+        }
+    }
+}
