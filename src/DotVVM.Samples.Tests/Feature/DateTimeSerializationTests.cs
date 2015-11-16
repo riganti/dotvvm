4b6e24d51611491cb70d78e0a47b958225d00d3c
MacakM <410452@mail.muni.cz>
2015-11-16 11:03:59 +0100

    Improved JavascriptEvents and added DateTimeSerialization

---
+++ src/DotVVM.Samples.Tests/Feature/DateTimeSerializationTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using Riganti.Utils.Testing.SeleniumCore;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using Dotvvm.Samples.Tests;
+using DotVVM.Framework.Routing;
+
+namespace DotVVM.Samples.Tests.Feature
+{
+    [TestClass]
+    public class DateTimeSerializationTests : SeleniumTestBase
+    {
+        [TestMethod]
+        public void Feature_DateTimeSerialization()
+        {
+            var culture = new CultureInfo("cs-CZ");
+
+            RunInAllBrowsers(browser =>
+            {
+                browser.NavigateToUrl("FeatureSamples/DateTimeSerialization/DateTimeSerialization");
+
+                // verify the first date
+                browser.ElementAt("input[type=text]", 0).Clear().SendKeys("18.2.1988");
+                browser.ElementAt("input[type=button]", 1).Click().Wait();
+                
+                //TODO: check with some other function
+                Assert.AreEqual(new DateTime(1988, 2, 18), DateTime.Parse(browser.FindElements("span")[0].GetText()));
+
+                browser.ElementAt("input[type=text]", 0).Clear().SendKeys("test");
+                browser.ElementAt("input[type=button]", 1).Click().Wait();
+
+                Assert.AreEqual(DateTime.MinValue, DateTime.Parse(browser.FindElements("span")[0].GetText()));
+
+                // verify the second date
+                browser.ElementAt("input[type=text]", 1).Clear().SendKeys("2011-03-19 16:48:17");
+                browser.ElementAt("input[type=button]", 3).Click().Wait();
+
+                Assert.AreEqual(new DateTime(2011, 3, 19, 16, 48, 0),
+                    DateTime.Parse(browser.FindElements("span")[1].GetText()));
+
+                browser.ElementAt("input[type=text]", 1).Clear().SendKeys("test");
+                browser.ElementAt("input[type=button]", 3).Click().Wait();
+
+                browser.ElementAt("span", 1).CheckIfInnerTextEquals("null");
+
+                // try to set dates from server
+                browser.ElementAt("input[type=button]", 0).Click().Wait();
+                browser.ElementAt("input[type=button]", 2).Click().Wait();
+
+                Assert.IsTrue((DateTime.Now - DateTime.Parse(browser.FindElements("input[type=text]")[0].GetAttribute("value"), culture)).TotalHours < 24); // there is no time in the field
+                Assert.IsTrue((DateTime.Now - DateTime.Parse(browser.FindElements("input[type=text]")[1].GetAttribute("value"), culture)).TotalMinutes < 1); // the minutes can differ slightly
+            });
+        }
+    }
+}
--- src/DotVVM.Samples.Tests/Feature/JavascriptEventsTests.cs
+++ src/DotVVM.Samples.Tests/Feature/JavascriptEventsTests.cs
@@ -23,28 +23,29 @@ namespace DotVVM.Samples.Tests.Feature
 
                 // init alert
                 browser.Wait();
-                //TODO: Change to CheckIfAlertTextContains
+
+                browser.CheckIfAlertTextEquals("init");
                 Assert.AreEqual("init", browser.GetAlertText());
                 browser.ConfirmAlert();
 
                 // postback alerts
                 browser.ElementAt("input[type=button]", 0).Click().Wait();
-                //TODO: Change to CheckIfAlertTextContains
-                Assert.AreEqual("beforePostback", browser.GetAlertText());
+
+                browser.CheckIfAlertTextEquals("beforePostback");
                 browser.ConfirmAlert();
                 browser.Wait();
-                //TODO: Change to CheckIfAlertTextContains
-                Assert.AreEqual("afterPostback", browser.GetAlertText());
+
+                browser.CheckIfAlertTextEquals("afterPostback");
                 browser.ConfirmAlert();
 
                 // error alerts
                 browser.ElementAt("input[type=button]", 1).Click().Wait();
-                //TODO: Change to CheckIfAlertTextContains
-                Assert.AreEqual("beforePostback", browser.GetAlertText());
+
+                browser.CheckIfAlertTextEquals("beforePostback");
                 browser.ConfirmAlert();
                 browser.Wait();
-                //TODO: Change to CheckIfAlertTextContains
-                Assert.AreEqual("custom error handler", browser.GetAlertText());
+
+                browser.CheckIfAlertTextEquals("custom error handler");
                 browser.ConfirmAlert();
             });
         }
