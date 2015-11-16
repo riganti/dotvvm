7c07e3f82a5742fbaa4091e7d52bd5eea5092529
Tomas Jurasek <jurasek.tomas@hotmail.cz>
2015-11-16 14:01:59 +0100

    CascadeSelectors Tests

---
+++ src/DotVVM.Samples.Tests/Complex/CascadeSelectorsTests.cs
@@ -0,0 +1,50 @@
+﻿using Dotvvm.Samples.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Riganti.Utils.Testing.SeleniumCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotVVM.Samples.Tests.Complex
+{
+    [TestClass]
+    public class CascadeSelectorsTests : SeleniumTestBase
+    {
+        [TestMethod]
+        public void Complex_CascadeSelectors()
+        {
+            RunInAllBrowsers(browser =>
+            {
+                browser.NavigateToUrl(SamplesRouteUrls.ComplexSamples_CascadeSelectors);
+
+                // select city
+                browser.First("select").Select(1);
+                browser.First("input[type=button]").Click();
+                browser.Wait();
+
+                // select hotel
+                browser.Last("select").Select(1);
+                browser.Last("input[type=button]").Click();
+                browser.Wait();
+
+                browser.First("h2").CheckIfInnerTextEquals("Hotel Seattle #2");
+
+                // select city
+                browser.First("select").Select(0);
+                browser.First("input[type=button]").Click();
+                browser.Wait();
+
+                // select hotel
+                browser.Last("select")
+                    .First("option").Click();
+
+                browser.Last("input[type=button]").Click();
+                browser.Wait();
+
+                browser.First("h2").CheckIfInnerTextEquals("Hotel Prague #1");
+            });
+        }
+    }
+}
--- src/DotVVM.Samples.Tests/SamplesRouteUrls.cs
+++ src/DotVVM.Samples.Tests/SamplesRouteUrls.cs
@@ -2,53 +2,56 @@
 
 using System.Runtime.Remoting.Messaging;
 
-namespace Dotvvm.Samples.Tests{
-	public class SamplesRouteUrls{
-		
-    		public const string Default = "Default";
-			public const string DefaultRoute = "";
-			public const string ComplexSamples_GridViewDataSet_GridViewDataSet = "ComplexSamples/GridViewDataSet/GridViewDataSet";
-			public const string ComplexSamples_ChangedEvent_ChangedEvent = "ComplexSamples/ChangedEvent/ChangedEvent";
-			public const string ComplexSamples_InvoiceCalculator_InvoiceCalculator = "ComplexSamples/InvoiceCalculator/InvoiceCalculator";
-			public const string ComplexSamples_TaskList_TaskList = "ComplexSamples/TaskList/TaskList";
-            public const string ComplexSamples_TaskList_ServerRenderedTaskList = "ComplexSamples/TaskList/ServerRenderedTaskList";
-            public const string ControlSamples_Button_Button = "ControlSamples/Button/Button";
-			public const string ControlSamples_Button_InputTypeButton_HtmlContentInside = "ControlSamples/Button/InputTypeButton_HtmlContentInside";
-			public const string ControlSamples_Button_InputTypeButton_TextContentInside = "ControlSamples/Button/InputTypeButton_TextContentInside";
-	        public const string ControlSamples_Button_ButtonTagName = "ControlSamples/Button/ButtonTagName";
-            public const string ControlSamples_CheckBox_CheckBox = "ControlSamples/CheckBox/CheckBox";
-			public const string ControlSamples_ComboBox_ComboBox = "ControlSamples/ComboBox/ComboBox";
-            public const string ControlSamples_EnabledProperty_EnabledProperty = "ControlSamples/EnabledProperty/EnabledProperty";
-            public const string ControlSamples_FileUpload_FileUpload = "ControlSamples/FileUpload/FileUpload";
-	        public const string ControlSamples_LinkButton_LinkButton = "ControlSamples/LinkButton/LinkButton";
-            public const string ControlSamples_RadioButton_RadioButton = "ControlSamples/RadioButton/RadioButton";
-			public const string ControlSamples_TextBox_IntBoundTextBox = "ControlSamples/TextBox/IntBoundTextBox";
-			public const string ControlSamples_TextBox_TextBox = "ControlSamples/TextBox/TextBox";
-			public const string Errors_BindingInvalidCommand = "Errors/BindingInvalidCommand";
-			public const string Errors_BindingInvalidProperty = "Errors/BindingInvalidProperty";
-			public const string Errors_InvalidViewModel = "Errors/InvalidViewModel";
-			public const string Errors_MalformedBinding = "Errors/MalformedBinding";
-			public const string Errors_MasterPageRequiresDifferentViewModel = "Errors/MasterPageRequiresDifferentViewModel";
-			public const string Errors_MissingRequiredProperty = "Errors/MissingRequiredProperty";
-			public const string Errors_MissingRequiredProperty2 = "Errors/MissingRequiredProperty2";
-			public const string Errors_MissingViewModel = "Errors/MissingViewModel";
-			public const string Errors_NonExistingControl = "Errors/NonExistingControl";
-			public const string Errors_NonExistingProperty = "Errors/NonExistingProperty";
-			public const string Errors_NotAllowedHardCodedPropertyValue = "Errors/NotAllowedHardCodedPropertyValue";
-			public const string Errors_WrongPropertyValue = "Errors/WrongPropertyValue";
-	        public const string Feature_DateTimeSerialization_DateTimeSerialization = "FeatureSamples/DateTimeSerialization/DateTimeSerialization";
-			public const string FeatureSamples_DoublePostBackPrevention_DoublePostBackPrevention = "FeatureSamples/DoublePostBackPrevention/DoublePostBackPrevention";
-	        public const string FeatureSamples_Formatting_Formatting = "FeatureSamples/Formatting/Formatting";
-            public const string FeatureSamples_JavascriptEvents_JavascriptEvents = "FeatureSamples/JavascriptEvents/JavascriptEvents";
-			public const string FeatureSamples_MarkupControl_MarkupControlRegistration = "FeatureSamples/MarkupControl/MarkupControlRegistration";
-			public const string FeatureSamples_NestedMasterPages_Content = "FeatureSamples/NestedMasterPages/Content";
-			public const string FeatureSamples_PostBackUpdate_PostbackUpdate = "FeatureSamples/PostBackUpdate/PostbackUpdate";
-			public const string FeatureSamples_Redirect_Redirect = "FeatureSamples/Redirect/Redirect";
-			public const string FeatureSamples_ServerComments_ServerComments = "FeatureSamples/ServerComments/ServerComments";
-			public const string FeatureSamples_Validation_NestedValidation = "FeatureSamples/Validation/NestedValidation";
-			public const string FeatureSamples_Validation_RegexValidation = "FeatureSamples/Validation/RegexValidation";
-            public const string FeatureSamples_Serialization_Serialization = "FeatureSamples/Serialization/Serialization";
-            public const string FeatureSamples_Validation_SimpleValidation = "FeatureSamples/Validation/SimpleValidation";
-	        public const string FeatureSamples_ViewModelProtection_ViewModelProtection = "FeatureSamples/ViewModelProtection/ViewModelProtection";
-		}
+namespace Dotvvm.Samples.Tests
+{
+    public class SamplesRouteUrls
+    {
+
+        public const string Default = "Default";
+        public const string DefaultRoute = "";
+        public const string ComplexSamples_GridViewDataSet_GridViewDataSet = "ComplexSamples/GridViewDataSet/GridViewDataSet";
+        public const string ComplexSamples_ChangedEvent_ChangedEvent = "ComplexSamples/ChangedEvent/ChangedEvent";
+        public const string ComplexSamples_InvoiceCalculator_InvoiceCalculator = "ComplexSamples/InvoiceCalculator/InvoiceCalculator";
+        public const string ComplexSamples_TaskList_TaskList = "ComplexSamples/TaskList/TaskList";
+        public const string ComplexSamples_CascadeSelectors = "ComplexSamples/CascadeSelectors/CascadeSelectors";
+        public const string ComplexSamples_TaskList_ServerRenderedTaskList = "ComplexSamples/TaskList/ServerRenderedTaskList";
+        public const string ControlSamples_Button_Button = "ControlSamples/Button/Button";
+        public const string ControlSamples_Button_InputTypeButton_HtmlContentInside = "ControlSamples/Button/InputTypeButton_HtmlContentInside";
+        public const string ControlSamples_Button_InputTypeButton_TextContentInside = "ControlSamples/Button/InputTypeButton_TextContentInside";
+        public const string ControlSamples_Button_ButtonTagName = "ControlSamples/Button/ButtonTagName";
+        public const string ControlSamples_CheckBox_CheckBox = "ControlSamples/CheckBox/CheckBox";
+        public const string ControlSamples_ComboBox_ComboBox = "ControlSamples/ComboBox/ComboBox";
+        public const string ControlSamples_EnabledProperty_EnabledProperty = "ControlSamples/EnabledProperty/EnabledProperty";
+        public const string ControlSamples_FileUpload_FileUpload = "ControlSamples/FileUpload/FileUpload";
+        public const string ControlSamples_LinkButton_LinkButton = "ControlSamples/LinkButton/LinkButton";
+        public const string ControlSamples_RadioButton_RadioButton = "ControlSamples/RadioButton/RadioButton";
+        public const string ControlSamples_TextBox_IntBoundTextBox = "ControlSamples/TextBox/IntBoundTextBox";
+        public const string ControlSamples_TextBox_TextBox = "ControlSamples/TextBox/TextBox";
+        public const string Errors_BindingInvalidCommand = "Errors/BindingInvalidCommand";
+        public const string Errors_BindingInvalidProperty = "Errors/BindingInvalidProperty";
+        public const string Errors_InvalidViewModel = "Errors/InvalidViewModel";
+        public const string Errors_MalformedBinding = "Errors/MalformedBinding";
+        public const string Errors_MasterPageRequiresDifferentViewModel = "Errors/MasterPageRequiresDifferentViewModel";
+        public const string Errors_MissingRequiredProperty = "Errors/MissingRequiredProperty";
+        public const string Errors_MissingRequiredProperty2 = "Errors/MissingRequiredProperty2";
+        public const string Errors_MissingViewModel = "Errors/MissingViewModel";
+        public const string Errors_NonExistingControl = "Errors/NonExistingControl";
+        public const string Errors_NonExistingProperty = "Errors/NonExistingProperty";
+        public const string Errors_NotAllowedHardCodedPropertyValue = "Errors/NotAllowedHardCodedPropertyValue";
+        public const string Errors_WrongPropertyValue = "Errors/WrongPropertyValue";
+        public const string Feature_DateTimeSerialization_DateTimeSerialization = "FeatureSamples/DateTimeSerialization/DateTimeSerialization";
+        public const string FeatureSamples_DoublePostBackPrevention_DoublePostBackPrevention = "FeatureSamples/DoublePostBackPrevention/DoublePostBackPrevention";
+        public const string FeatureSamples_Formatting_Formatting = "FeatureSamples/Formatting/Formatting";
+        public const string FeatureSamples_JavascriptEvents_JavascriptEvents = "FeatureSamples/JavascriptEvents/JavascriptEvents";
+        public const string FeatureSamples_MarkupControl_MarkupControlRegistration = "FeatureSamples/MarkupControl/MarkupControlRegistration";
+        public const string FeatureSamples_NestedMasterPages_Content = "FeatureSamples/NestedMasterPages/Content";
+        public const string FeatureSamples_PostBackUpdate_PostbackUpdate = "FeatureSamples/PostBackUpdate/PostbackUpdate";
+        public const string FeatureSamples_Redirect_Redirect = "FeatureSamples/Redirect/Redirect";
+        public const string FeatureSamples_ServerComments_ServerComments = "FeatureSamples/ServerComments/ServerComments";
+        public const string FeatureSamples_Validation_NestedValidation = "FeatureSamples/Validation/NestedValidation";
+        public const string FeatureSamples_Validation_RegexValidation = "FeatureSamples/Validation/RegexValidation";
+        public const string FeatureSamples_Serialization_Serialization = "FeatureSamples/Serialization/Serialization";
+        public const string FeatureSamples_Validation_SimpleValidation = "FeatureSamples/Validation/SimpleValidation";
+        public const string FeatureSamples_ViewModelProtection_ViewModelProtection = "FeatureSamples/ViewModelProtection/ViewModelProtection";
+    }
 }
