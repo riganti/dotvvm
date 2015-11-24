06156c0bf7128c4c242ca101cc171740093f2303
Mylan719 <Mylan719@gmail.com>
2015-11-24 19:58:16 +0100

    Work in progress visitor

---
+++ src/DotVVM.Framework/Parser/Dothtml/Parser/IDothtmlSyntaxTreeVisitor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotVVM.Framework.Parser.Dothtml.Parser
+{
+    public interface IDothtmlSyntaxTreeVisitor
+    {
+        void Visit(DothtmlAttributeNode attribute);
+        void Visit(DothtmlBindingNode binding);
+        void Visit(DothtmlDirectiveNode directive);
+        void Visit(DothtmlElementNode element);
+        void Visit(DothtmlLiteralNode literal);
+    }
+}
