9a4cb21c82d4f48fc5b67f2acbd7c01f5922e3fb
exyi <exyi@outlook.cz>
2015-12-16 17:27:26 +0100

    active property binding fix

--- src/DotVVM.Framework/Controls/DotvvmControl.cs
+++ src/DotVVM.Framework/Controls/DotvvmControl.cs
@@ -151,7 +151,7 @@ namespace DotVVM.Framework.Controls
             {
                 if (item.Key is ActiveDotvvmProperty)
                 {
-                    ((ActiveDotvvmProperty)item.Key).AddAttributesToRender(writer, context, item.Value, this);
+                    ((ActiveDotvvmProperty)item.Key).AddAttributesToRender(writer, context, GetValue(item.Key), this);
                 }
             }
 
