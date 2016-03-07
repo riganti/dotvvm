dd4b9f34be7bb3563640ea406cbc6738d206232b
Tomáš Herceg <herceg@vbnet.cz>
2016-03-07 14:36:33 +0100

    Sample project fixed

--- src/DotVVM.Samples.BasicSamples/DotvvmStartup.cs
+++ src/DotVVM.Samples.BasicSamples/DotvvmStartup.cs
@@ -9,11 +9,38 @@ namespace DotVVM.Samples.BasicSamples
         public void Configure(DotvvmConfiguration config, string applicationPath)
         {
             config.DefaultCulture = "en-US";
+
             config.Markup.DefaultDirectives.Add(ParserConstants.ResourceTypeDirective, "DotVVM.Samples.BasicSamples.Resources.Resource, DotVVM.Samples.BasicSamples");
+            config.Markup.Controls.Add(new DotvvmControlConfiguration()
+            {
+                Assembly = "DotVVM.Samples.BasicSamples",
+                Namespace = "DotVVM.Samples.BasicSamples.Controls",
+                TagPrefix = "PropertyUpdate"
+            });
+            config.Markup.Controls.Add(new DotvvmControlConfiguration()
+            {
+                TagPrefix = "IdGeneration",
+                TagName = "Control",
+                Src = "Views/FeatureSamples/IdGeneration/IdGeneration_control.dotcontrol"
+            });
+            config.Markup.Controls.Add(new DotvvmControlConfiguration()
+            {
+                TagPrefix = "FileUploadInRepeater",
+                TagName = "FileUploadWrapper",
+                Src = "Views/ComplexSamples/FileUploadInRepeater/FileUploadWrapper.dotcontrol"
+            });
+            config.Markup.Controls.Add(new DotvvmControlConfiguration()
+            {
+                TagPrefix = "sample",
+                TagName = "ControlPropertyUpdating",
+                Src = "Views/FeatureSamples/MarkupControl/ControlPropertyUpdating.dotcontrol"
+            });
 
             config.RouteTable.Add("Default", "", "Views/Default.dothtml");
             config.RouteTable.AutoDiscoverRoutes(new DefaultRouteStrategy(config));
             config.RouteTable.Add("RepeaterRouteLink-PageDetail", "ControlSamples/Repeater/RouteLink/{Id}", "Views/ControlSamples/Repeater/RouteLink.dothtml");
         }
+        
     }
 }
+ 
