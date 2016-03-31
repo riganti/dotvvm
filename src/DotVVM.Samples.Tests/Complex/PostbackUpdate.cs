63b3839e231334d792463cf17b8a77114a1aa339
Milan Mikus <mylan719@gmail.com>
2016-03-31 22:31:24 +0200

    Fixed the samples based on current bug.

--- src/DotVVM.Samples.Tests/Complex/PostbackUpdate.cs
+++ src/DotVVM.Samples.Tests/Complex/PostbackUpdate.cs
@@ -15,27 +15,29 @@ namespace DotVVM.Samples.Tests.Complex
         [TestMethod]
         public void PostbackUpdate_DataContext_WrongUpdateOnPostback()
         {
+            //As I am writing this, test should fail because on postback there will be two <!-- ko with: EditedArticle --/> elements inside each other instead of one.
+            //Caused by DataContext, Visible, RenderSettings.Mode, PostBack.Update being all on the same div
             RunInAllBrowsers(browser =>
             {
                 browser.NavigateToUrl(SamplesRouteUrls.ComplexSamples_ServerRendering_ControlUsageSample);
                 browser.First("a[data-ui=show-link]").Click();
                 browser.Wait(200);
-                browser.First("input[data-ui=textbox]").CheckAttribute("value", v => v.StartsWith("a"));
+                browser.First("input[data-ui=textbox]").CheckAttribute("value", v => v.Contains("a"));
             });
         }
 
         [TestMethod]
         public void PostbackUpdate_DataContext_RewritingAndHiding()
         {
+            //As I am writing this, test should work because RenderSettings.Mode, PostBack.Update are on div that is inside div with DataContext.
             RunInAllBrowsers(browser =>
             {
                 browser.NavigateToUrl(SamplesRouteUrls.ComplexSamples_ServerRendering_ControlUsageSampleRewriting);
                 browser.First("a[data-ui=show-link]").Click();
                 browser.Wait(200);
-                var elem = browser.First("div[data-ui='context-1']").First("input[data-ui=textbox]");
-                elem.CheckAttribute("value", v => v.StartsWith("a"));
+                browser.First("div[data-ui='context-1']").First("input[data-ui=textbox]").CheckAttribute("value", v => v.Contains("a"));
                 browser.First("a[data-ui=rewrite-link]").Click();
-                browser.First("div[data-ui='context-2']").First("input[data-ui=textbox]").CheckAttribute("value", v => v.StartsWith("b"), "value does not start with 'b'");
+                browser.First("div[data-ui='context-2']").First("input[data-ui=textbox]").CheckAttribute("value", v => v.Contains("b"));
             });
         }
     }
