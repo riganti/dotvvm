e04c6c0c18f2ef76a2ab90d4a3ffe2848aee3213
Milan Mikus <mylan719@gmail.com>
2016-04-02 18:23:47 +0200

    Modified tests and sample to match Redirecting behavior as discussed

--- src/DotVVM.Samples.BasicSamples/ViewModels/ComplexSamples/RedirectAndUrl/ScrollingPageViewModel.cs
+++ src/DotVVM.Samples.BasicSamples/ViewModels/ComplexSamples/RedirectAndUrl/ScrollingPageViewModel.cs
@@ -13,14 +13,25 @@ namespace DotVVM.Samples.BasicSamples.ViewModels.ComplexSamples.RedirectAndUrl
         public void GoToParagraph1()
         {
             Message = "ToParagraph1";
-            Context.RedirectToUrl("#paragraph1");
         }
 
         public void GoToParagraph2()
         {
             Message = "ToParagraph2";
+        }
+
+        public void GoToParagraph2WithRedirectToUrl()
+        {
+            Message = "GoToParagraph2_With_RedirectToUrl";
             var path = Context.OwinContext.Request.Uri;
             Context.RedirectToUrl($"{path}#paragraph2");
+        }
+
+        public void TestQueryString()
+        {
+            Message = "TestQuerystring";
+            var path = Context.OwinContext.Request.Uri;
+            Context.RedirectToUrl($"{path}?q='delf'&meep");
         }
 
         public void TestMessage ()
