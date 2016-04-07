3e8f693dbe4a53b222f97bab01abb84de7c0ed84
MacakM <410452@mail.muni.cz>
2016-04-07 12:29:41 +0200

    Added ResizableGrid

---
+++ src/DotVVM.Samples.BasicSamples/Scripts/GridViewSizable.js
@@ -0,0 +1,23 @@
+﻿var $jq = jQuery.noConflict();
+
+$jq(function () {
+
+    var onSampleResized = function (e) {
+        var columns = $jq(e.currentTarget).find("th");
+        var msg = "columns widths: ";
+        columns.each(function () { msg += $jq(this).width() + "px; "; });
+        $jq("#sizableGridTxt").html(msg);
+
+    };
+
+    $jq("#sizableGrid").colResizable({
+        fixed: false,
+        liveDrag: true,
+        gripInnerHtml: "<div class='grip'></div>",
+        draggingClass: "dragging",
+        postbackSafe: true,
+        partialRefresh: true,
+        onResize: onSampleResized
+    });
+
+});
