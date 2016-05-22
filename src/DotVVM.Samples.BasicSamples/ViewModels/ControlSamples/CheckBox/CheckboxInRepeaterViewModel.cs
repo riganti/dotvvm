effd9a37488ccb9a19e9c44cf12a6eaca32b52ae
Ladislav Šesták <sestakladislav.jr@gmail.com>
2016-05-23 00:41:46 +0200

    Checkbox in repeater - not completed test

---
+++ src/DotVVM.Samples.BasicSamples/ViewModels/ControlSamples/CheckBox/CheckboxInRepeaterViewModel.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotVVM.Framework.ViewModel;
+
+namespace DotVVM.Samples.BasicSamples.ViewModels.ControlSamples.CheckBox
+{
+	public class CheckboxInRepeaterViewModel : DotvvmViewModelBase
+	{
+        public override Task PreRender()
+        {
+            CheckBoxes = new List<CheckBox>();
+            CheckBoxes = GetData();
+            return base.PreRender();
+        }
+
+	    public List<CheckBox> CheckBoxes { get; set; }
+
+	    public List<string> Colors { get; set; }
+
+        public void UpdateSelectedColors()
+        {
+            SelectedColors = string.Join(", ", Colors.Select(i => i.ToString()));
+        }
+
+	    public string SelectedColors { get; set; }
+
+	    public void SetCheckedItems()
+        {
+            Colors = new List<string>() { "one", "three" };
+            UpdateSelectedColors();
+        }
+        private List<CheckBox> GetData()
+        {
+            return new List<CheckBox>
+            {
+                new CheckBox()
+                {
+                    Text="CheckBox 1",
+                    Checked = false,
+                    Visible = true,
+                    CheckedValue = "one"
+                },
+                new CheckBox()
+                {
+                    Text="CheckBox 2",
+                    Checked = false,
+                    Visible = true,
+                    CheckedValue = "two"
+                },
+                new CheckBox()
+                {
+                    Text="CheckBox 3",
+                    Checked = false,
+                    Visible = true,
+                    CheckedValue = "three"
+                },new CheckBox()
+                {
+                    Text="CheckBox 3",
+                    Checked = false,
+                    Visible = true,
+                    CheckedValue = "four"
+                }
+            };
+        }
+
+    }   public class CheckBox
+        {
+            public string Text { get; set; }
+            public bool Checked { get; set; }
+            public bool Visible { get; set; }
+            public string CheckedValue { get; set; }
+        }
+}
+
