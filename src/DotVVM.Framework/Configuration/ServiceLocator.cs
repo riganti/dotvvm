67c6bc8244e6e38688d9450f58176aeb7a78d258
exyi <exyi@outlook.cz>
2016-07-12 20:07:39 +0200

    ServiceLocator tries to find service in IServiceProvider

--- src/DotVVM.Framework/Configuration/ServiceLocator.cs
+++ src/DotVVM.Framework/Configuration/ServiceLocator.cs
@@ -1,40 +1,63 @@
 using System;
 using System.Collections.Concurrent;
+using System.Reflection;
 
 namespace DotVVM.Framework.Configuration
 {
-    public class ServiceLocator
-    {
+	public class ServiceLocator
+	{
+		//#if DotNetCore
+		//		// wrapper around IServiceProvider
+		//		private IServiceProvider sericeProvider;
 
-        private ConcurrentDictionary<Type, Func<object>> factories = new ConcurrentDictionary<Type, Func<object>>();
-        private ConcurrentDictionary<Type, object> singletonInstances = new ConcurrentDictionary<Type, object>(); 
-
-
-        public void RegisterTransient<T>(Func<T> factory)
-        {
-            factories[typeof (T)] = () => factory();
-        }
-
-        public void RegisterSingleton<T>(Func<T> factory)
-        {
-            factories[typeof (T)] = CreateSingletonFactory(factory);
-            object temp;
-            singletonInstances.TryRemove(typeof(T), out temp); //removes old service instance and allows to create new one
-        }
+		//		public void RegisterSingleton<T>(Func<T> factory)
+		//		{
+		//			factories[typeof(T)] = CreateSingletonFactory(factory);
+		//			object temp;
+		//			singletonInstances.TryRemove(typeof(T), out temp); //removes old service instance and allows to create new one
+		//		}
 
 
 
-        public T GetService<T>()
-        {
-            return (T)factories[typeof (T)]();
-        }
+		//		public T GetService<T>()
+		//		{
+		//			return sericeProvider.GetService<T>();
+		//		}
+
+		//		private Func<object> CreateSingletonFactory<T>(Func<T> factory)
+		//		{
+		//			return () => singletonInstances.GetOrAdd(typeof(T), t => factory());
+		//		}
+		//#else
+		private ConcurrentDictionary<Type, Func<object>> factories = new ConcurrentDictionary<Type, Func<object>>();
+		private ConcurrentDictionary<Type, object> singletonInstances = new ConcurrentDictionary<Type, object>();
+
+
+		public void RegisterTransient<T>(Func<T> factory)
+		{
+			factories[typeof(T)] = () => factory();
+		}
+
+		public void RegisterSingleton<T>(Func<T> factory)
+		{
+			factories[typeof(T)] = CreateSingletonFactory(factory);
+			object temp;
+			singletonInstances.TryRemove(typeof(T), out temp); //removes old service instance and allows to create new one
+		}
 
 
 
+		public T GetService<T>()
+		{
+			Func<object> result;
+			return factories.TryGetValue(typeof(T), out result) ? (T)result() : (T)((IServiceProvider)factories[typeof(IServiceProvider)]()).GetService(typeof(T));
 
-        private Func<object> CreateSingletonFactory<T>(Func<T> factory)
-        {
-            return () => singletonInstances.GetOrAdd(typeof (T), t => factory());
-        }
-    }
+		}
+
+		private Func<object> CreateSingletonFactory<T>(Func<T> factory)
+		{
+			return () => singletonInstances.GetOrAdd(typeof(T), t => factory());
+		}
+		//#endif
+	}
 }
--- src/DotVVM.Samples.BasicSamples/Startup.cs
+++ src/DotVVM.Samples.BasicSamples/Startup.cs
@@ -27,83 +27,83 @@ namespace DotVVM.Samples.BasicSamples
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
 		{
-			
-       //     app.UseMiddleware<SwitchMiddleware>(
-       //         new List<SwitchMiddlewareCase>() {
-       //             new SwitchMiddlewareCase(
-       //                 c => c.Request.Uri.PathAndQuery.StartsWith("/ComplexSamples/Auth"), next =>
-       //                 new CookieAuthenticationMiddleware(next, app, new CookieAuthenticationOptions()
-       //                 {
-       //                     LoginPath = new PathString("/ComplexSamples/Auth/Login"),
-							//AuthenticationScheme = CookieAuthenticationDefaults.AuthenticationScheme,
-							
-       //                     Provider = new CookieAuthenticationProvider()
-       //                     {
-       //                         OnApplyRedirect = c =>
-       //                         {
-       //                             // redirect to login page on 401 request
-       //                             if (c.Response.StatusCode == 401 && c.Request.Method == "GET")
-       //                             {
-       //                                 c.Response.StatusCode = 302;
-       //                                 c.Response.Headers["Location"] = c.RedirectUri;
-       //                             }
-       //                             // do not do anything on redirection to returnurl
-       //                             // to not return page when ViewModel is expected
-       //                             // we should implement this in DotVVM framework,
-       //                             // not samples
-       //                         }
-       //                     }
-       //                 })
-       //             ),
-       //             new SwitchMiddlewareCase(
-       //                 c => c.Request.Uri.PathAndQuery.StartsWith("/ComplexSamples/SPARedirect"), next =>
-       //                 new CookieAuthenticationMiddleware(next, app, new CookieAuthenticationOptions()
-       //                 {
-       //                     LoginPath = new PathString("/ComplexSamples/SPARedirect/login"),
-       //                     AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-       //                     Provider = new CookieAuthenticationProvider()
-       //                     {
-       //                         OnApplyRedirect = c =>
-       //                         {
-       //                             // redirect to login page on 401 request
-       //                             if (c.Response.StatusCode == 401 && c.Request.Method == "GET")
-       //                             {
-       //                                 c.Response.StatusCode = 302;
-       //                                 c.Response.Headers["Location"] = c.RedirectUri;
-       //                             }
-       //                             // do not do anything on redirection to returnurl
-       //                             // to not return page when ViewModel is expected
-       //                             // we should implement this in DotVVM framework,
-       //                             // not samples
-       //                         }
-       //                     }
-       //                 })
-       //             ),
-       //              new SwitchMiddlewareCase(
-       //                 c => c.Request.Uri.PathAndQuery.StartsWith("/ControlSamples/AuthenticatedView")
-       //                     || c.Request.Uri.PathAndQuery.StartsWith("/ControlSamples/RoleView"), next =>
-       //                 new CookieAuthenticationMiddleware(next, app, new CookieAuthenticationOptions()
-       //                 {
-       //                     AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie                            
-       //                 })
-       //             ),
-       //         }
-       //     );
 
-            var applicationPhysicalPath = env.WebRootPath;
+			//     app.UseMiddleware<SwitchMiddleware>(
+			//         new List<SwitchMiddlewareCase>() {
+			//             new SwitchMiddlewareCase(
+			//                 c => c.Request.Uri.PathAndQuery.StartsWith("/ComplexSamples/Auth"), next =>
+			//                 new CookieAuthenticationMiddleware(next, app, new CookieAuthenticationOptions()
+			//                 {
+			//                     LoginPath = new PathString("/ComplexSamples/Auth/Login"),
+			//AuthenticationScheme = CookieAuthenticationDefaults.AuthenticationScheme,
 
-            // use DotVVM
-            DotvvmConfiguration dotvvmConfiguration = app.UseDotVVM<DotvvmStartup>(applicationPhysicalPath);
-            dotvvmConfiguration.Debug = true;
+			//                     Provider = new CookieAuthenticationProvider()
+			//                     {
+			//                         OnApplyRedirect = c =>
+			//                         {
+			//                             // redirect to login page on 401 request
+			//                             if (c.Response.StatusCode == 401 && c.Request.Method == "GET")
+			//                             {
+			//                                 c.Response.StatusCode = 302;
+			//                                 c.Response.Headers["Location"] = c.RedirectUri;
+			//                             }
+			//                             // do not do anything on redirection to returnurl
+			//                             // to not return page when ViewModel is expected
+			//                             // we should implement this in DotVVM framework,
+			//                             // not samples
+			//                         }
+			//                     }
+			//                 })
+			//             ),
+			//             new SwitchMiddlewareCase(
+			//                 c => c.Request.Uri.PathAndQuery.StartsWith("/ComplexSamples/SPARedirect"), next =>
+			//                 new CookieAuthenticationMiddleware(next, app, new CookieAuthenticationOptions()
+			//                 {
+			//                     LoginPath = new PathString("/ComplexSamples/SPARedirect/login"),
+			//                     AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+			//                     Provider = new CookieAuthenticationProvider()
+			//                     {
+			//                         OnApplyRedirect = c =>
+			//                         {
+			//                             // redirect to login page on 401 request
+			//                             if (c.Response.StatusCode == 401 && c.Request.Method == "GET")
+			//                             {
+			//                                 c.Response.StatusCode = 302;
+			//                                 c.Response.Headers["Location"] = c.RedirectUri;
+			//                             }
+			//                             // do not do anything on redirection to returnurl
+			//                             // to not return page when ViewModel is expected
+			//                             // we should implement this in DotVVM framework,
+			//                             // not samples
+			//                         }
+			//                     }
+			//                 })
+			//             ),
+			//              new SwitchMiddlewareCase(
+			//                 c => c.Request.Uri.PathAndQuery.StartsWith("/ControlSamples/AuthenticatedView")
+			//                     || c.Request.Uri.PathAndQuery.StartsWith("/ControlSamples/RoleView"), next =>
+			//                 new CookieAuthenticationMiddleware(next, app, new CookieAuthenticationOptions()
+			//                 {
+			//                     AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie                            
+			//                 })
+			//             ),
+			//         }
+			//     );
 
-            dotvvmConfiguration.ServiceLocator.RegisterSingleton<IUploadedFileStorage>(
-                () => new FileSystemUploadedFileStorage(Path.Combine(applicationPhysicalPath, "Temp"), TimeSpan.FromMinutes(30)));
+			var applicationPhysicalPath = env.ContentRootPath;
 
-            // use static files
-            app.UseStaticFiles(new StaticFileOptions
+			// use DotVVM
+			DotvvmConfiguration dotvvmConfiguration = app.UseDotVVM<DotvvmStartup>(applicationPhysicalPath);
+			dotvvmConfiguration.Debug = true;
+
+			dotvvmConfiguration.ServiceLocator.RegisterSingleton<IUploadedFileStorage>(
+				() => new FileSystemUploadedFileStorage(Path.Combine(applicationPhysicalPath, "Temp"), TimeSpan.FromMinutes(30)));
+
+			// use static files
+			app.UseStaticFiles(new StaticFileOptions
 			{
 				FileProvider = new Microsoft.Extensions.FileProviders.PhysicalFileProvider(applicationPhysicalPath)
 			});
-        }
+		}
     }
 }
