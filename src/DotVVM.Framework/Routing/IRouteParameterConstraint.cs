de3dba989b7308ab0e17d6e392a35b1f516ccca7
exyi <exyi@outlook.cz>
2016-07-18 18:45:07 +0200

    route constraints: implemented more of them, configuration moved to DotvvmConfiguration

---
+++ src/DotVVM.Framework/Routing/IRouteParameterConstraint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotVVM.Framework.Routing
+{
+    public interface IRouteParameterConstraint
+    {
+        string GetPartRegex(string parameter);
+        ParameterParseResult ParseString(string value, string parameter);
+    }
+
+    public struct ParameterParseResult
+    {
+        public readonly bool IsOK;
+        public readonly object Value;
+        public ParameterParseResult(bool ok, object value)
+        {
+            IsOK = ok;
+            Value = value;
+        }
+
+        public static readonly ParameterParseResult Failed = new ParameterParseResult(false, null);
+        public static ParameterParseResult Create(object obj) => new ParameterParseResult(true, obj);
+    }
+}
