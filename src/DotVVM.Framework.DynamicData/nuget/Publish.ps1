3c17126cb2e542c0d534d063eada370cc0c716a5
Tomas Herceg <tomas.herceg@riganti.cz>
2016-08-08 14:40:42 +0200

    dtto

--- src/DotVVM.Framework.DynamicData/nuget/Publish.ps1
+++ src/DotVVM.Framework.DynamicData/nuget/Publish.ps1
@@ -2,14 +2,14 @@ param([String]$key, [String]$server = "")
 
 del ./DotVVM.DynamicData.*.nupkg
 
-& ..\Tools\nuget.exe pack "./DotVVM.DynamicData.nuspec"
+& .\nuget.exe pack "./DotVVM.DynamicData.nuspec"
 
 $file = dir ./DotVVM.DynamicData.*.nupkg
 $file = $file.Name
 
 if ($server -eq "") {
-	& ..\Tools\nuget.exe push $file $key
+	& .\nuget.exe push $file $key
 }
 else {
-	& ..\Tools\nuget.exe push $file $key -Source $server
+	& .\nuget.exe push $file $key -Source $server
 }
