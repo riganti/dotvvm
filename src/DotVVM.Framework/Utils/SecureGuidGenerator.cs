8027c95a0d4cf23d7b26ccedddf2313e04f74412
Sejsel <sejseloid@gmail.com>
2016-08-16 17:07:11 +0200

    Use a cryptographically secure RNG for file GUIDs

---
+++ src/DotVVM.Framework/Utils/SecureGuidGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace DotVVM.Framework.Utils
+{
+    public static class SecureGuidGenerator
+    {
+        /// <summary>
+        /// Generates a random Guid with the <see cref="RNGCryptoServiceProvider"/>.
+        /// The Guid is NOT a valid v4 Guid, that would only lower the security.
+        /// </summary>
+        public static Guid GenerateGuid()
+        {
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                byte[] bytes = new byte[16];
+                rng.GetBytes(bytes);
+
+                return new Guid(bytes);
+            }
+        }
+    }
+}
