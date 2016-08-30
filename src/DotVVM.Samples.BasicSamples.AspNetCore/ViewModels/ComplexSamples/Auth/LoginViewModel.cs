0c3d43f7d02b0408324bf7a7ad87cff77f3417e2
exyi <exyi@outlook.cz>
2016-08-30 13:34:45 +0200

    sample projects splitted

---
+++ src/DotVVM.Samples.BasicSamples.AspNetCore/ViewModels/ComplexSamples/Auth/LoginViewModel.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DotVVM.Framework.ViewModel;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Net;
+using DotVVM.Framework.AspNetCore.Hosting;
+
+namespace DotVVM.Samples.BasicSamples.ViewModels.ComplexSamples.Auth
+{
+    public class LoginViewModel : DotvvmViewModelBase
+    {
+        public string UserName { get; set; }
+
+        public bool AdminRole { get; set; }
+
+        public bool LoggedIn { get; set; }
+
+        public LoginViewModel()
+        {
+            UserName = "";
+            AdminRole = false;
+        }
+
+        public override Task Init()
+        {
+            if (!Context.IsPostBack && Context.HttpContext.User?.Identity?.IsAuthenticated == true)
+            {
+                this.UserName = Context.HttpContext.User.Identity.Name;
+                this.AdminRole = Context.HttpContext.User.IsInRole("admin");
+            }
+            return base.Init();
+        }
+
+        public async Task Login()
+        {
+            var auth = Context.GetAuthenticationManager();
+            await auth.SignOutAsync("Scheme1");
+            var id = new ClaimsIdentity(new Claim[] {
+                    new Claim(ClaimTypes.Name, UserName)
+                    }, "ApplicationCookie");
+            if (AdminRole)
+            {
+                id.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+            }
+            await auth.SignInAsync("Scheme1", new ClaimsPrincipal(new[] { id }));
+        }
+    }
+}
