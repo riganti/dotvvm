6601e8ca6c6d600cf6ece19f356539d1b6e6805d
Ladislav Šesták <sestakladislav.jr@gmail.com>
2016-09-01 20:02:22 +0200

    Publish to private nuget

--- src/DotVVM.Framework.Hosting.Owin/Properties/AssemblyInfo.cs
+++ src/DotVVM.Framework.Hosting.Owin/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Dotvvm.Framework.Owin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Riganti s.r.o.")]
 [assembly: AssemblyProduct("Dotvvm.Framework.Owin")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
