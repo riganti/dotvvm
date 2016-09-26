a0e3c1ad6885f91ba8cddbcacd8869cce03409c3
Ladislav Šesták <sestakladislav.jr@gmail.com>
2016-09-27 01:40:37 +0200

    Added framework tests common project lock restore

--- src/Tools/CI/restore.cmd
+++ src/Tools/CI/restore.cmd
@@ -7,3 +7,4 @@ dotnet restore ../../DotVVM.Framework.Tests.AspCore
 dotnet restore ../../DotVVM.Framework.Tools.SeleniumGenerator
 dotnet restore ../../DotVVM.Samples.BasicSamples.AspNetCore
 dotnet restore ../../DotVVM.Samples.Common
+dotnet restore ../../DotVVM.Framework.Tests.Common
