db71fec69f47482a7e9fd9728f03dd05ae253b5d
Dušan Janošík <dusan@djanosik.cz>
2016-09-30 15:54:17 +0200

    Updates the GetResult method to properly handle VoidTaskResult.

--- src/DotVVM.Framework/Utils/TaskUtils.cs
+++ src/DotVVM.Framework/Utils/TaskUtils.cs
@@ -1,10 +1,23 @@
-﻿using System.Threading.Tasks;
+﻿using System.Reflection;
+using System.Threading.Tasks;
 
 namespace DotVVM.Framework.Utils
 {
     public static class TaskUtils
     {
         public static object GetResult(Task task)
-            => task.GetType() == typeof(Task) ? null : ((dynamic)task).Result;
+            => IsVoidTask(task) ? null : ((dynamic)task).Result;
+
+        private static bool IsVoidTask(Task task)
+        {
+            var type = task.GetType();
+
+            if (type != typeof(Task))
+            {
+                return type.GetProperty("Result").PropertyType.Name == "VoidTaskResult";
+            }
+
+            return true;
+        }
     }
 }
