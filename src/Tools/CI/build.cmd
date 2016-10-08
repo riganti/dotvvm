70c7826b325b3f6d72812b809aa91e58c2efe3cc
Ladislav Šesták <sestakladislav.jr@gmail.com>
2016-10-08 14:48:23 +0200

    CI DotVVM.CommandLine.Light

--- src/Tools/CI/build.cmd
+++ src/Tools/CI/build.cmd
@@ -24,5 +24,5 @@ dotnet restore ../../DotVVM.Samples.Common
 dotnet build ../../DotVVM.Samples.Common
 
 
-dotnet restore ../../DotVVM.CommandLine 
-dotnet build ../../DotVVM.CommandLine 
+dotnet restore ../../DotVVM.CommandLine.Light 
+dotnet build ../../DotVVM.CommandLine.Light
