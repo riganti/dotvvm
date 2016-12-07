eaf2cfeaaff7d5302b6911fb1536e557e821cd8a
Tomas Herceg <tomas.herceg@riganti.cz>
2016-12-07 14:10:26 +0100

    CI fix

--- src/Tools/CI/restore.cmd
+++ src/Tools/CI/restore.cmd
@@ -1,11 +1,15 @@
 @echo restoring dot net core packages
 dotnet restore ../../DotVVM.CommandLine
+dotnet restore ../../DotVVM.Compiler.Light
 dotnet restore ../../DotVVM.Core
 dotnet restore ../../DotVVM.Framework
 dotnet restore ../../DotVVM.Framework.Hosting.AspNetCore
+dotnet restore ../../DotVVM.Framework.Hosting.Owin
+dotnet restore ../../DotVVM.Framework.PerfTests
 dotnet restore ../../DotVVM.Framework.Tests.AspCore
+dotnet restore ../../DotVVM.Framework.Tests.Common
+dotnet restore ../../DotVVM.Framework.Tests.Owin
 dotnet restore ../../DotVVM.Framework.Tools.SeleniumGenerator
 dotnet restore ../../DotVVM.Samples.BasicSamples.AspNetCore
 dotnet restore ../../DotVVM.Samples.Common
-dotnet restore ../../DotVVM.Framework.Tests.Common
-dotnet restore ../../DotVVM.Compiler.Light
+dotnet restore ../../DotVVM.Samples.Tests
