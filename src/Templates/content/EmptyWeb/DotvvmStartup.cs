5d6467e28931bcaa4016a1f7e525c27e78c59083
Tomas Herceg <tomas.herceg@riganti.cz>
2017-02-18 01:39:35 +0100

    DotVVM empty project template added

---
+++ src/Templates/content/EmptyWeb/DotvvmStartup.cs
@@ -0,0 +1,35 @@
+using DotVVM.Framework;
+using DotVVM.Framework.Configuration;
+using DotVVM.Framework.Routing;
+
+namespace DotvvmApplication1
+{
+    public class DotvvmStartup : IDotvvmStartup
+    {
+        // For more information about this class, visit https://dotvvm.com/docs/tutorials/basics-project-structure
+        public void Configure(DotvvmConfiguration config, string applicationPath)
+        {
+            ConfigureRoutes(config, applicationPath);
+            ConfigureControls(config, applicationPath);
+            ConfigureResources(config, applicationPath);
+        }
+
+        private void ConfigureRoutes(DotvvmConfiguration config, string applicationPath)
+        {
+            config.RouteTable.Add("Default", "", "Views/default.dothtml");
+
+            // Uncomment the following line to auto-register all dothtml files in the Views folder
+            // config.RouteTable.AutoDiscoverRoutes(new DefaultRouteStrategy(config));    
+        }
+
+        private void ConfigureControls(DotvvmConfiguration config, string applicationPath)
+        {
+            // register code-only controls and markup controls
+        }
+
+        private void ConfigureResources(DotvvmConfiguration config, string applicationPath)
+        {
+            // register custom resources and adjust paths to the built-in resources
+        }
+    }
+}
