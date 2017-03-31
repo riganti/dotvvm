29ffb79cbc93d4ea95345e35da0099f2e90bb2ac
Tomas Herceg <tomas.herceg@riganti.cz>
2017-03-31 20:36:07 +0200

    MultiControlHierarchy test added

--- src/DotVVM.Samples.Tests/Feature/MarkupControlTests.cs
+++ src/DotVVM.Samples.Tests/Feature/MarkupControlTests.cs
@@ -159,5 +159,18 @@ namespace DotVVM.Samples.Tests.Feature
                 browser.ElementAt("input[type=text]", 1).CheckIfValue("26");
             });
         }
+
+
+        [TestMethod]
+        public void Feature_MarkupControl_MultiControlHierarchy()
+        {
+            RunInAllBrowsers(browser =>
+            {
+                browser.NavigateToUrl(SamplesRouteUrls.FeatureSamples_MarkupControl_MultiControlHierarchy);
+
+                var ul = browser.First("ul", By.CssSelector);
+                ul.FindElements("li", By.CssSelector).ThrowIfDifferentCountThan(20);
+            });
+        }
     }
 }
