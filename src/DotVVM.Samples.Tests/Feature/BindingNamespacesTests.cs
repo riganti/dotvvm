d0a0b743753df908ad5a516c89b945b66c978b25
Adam Štěpánek <adam.stepanek@riganti.cz>
2017-06-14 10:37:12 +0200

    Added the 'Feature_BindingNamespaces_BindingUsingNamespace' test

---
+++ src/DotVVM.Samples.Tests/Feature/BindingNamespacesTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dotvvm.Samples.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Riganti.Utils.Testing.Selenium.Core;
+
+namespace DotVVM.Samples.Tests.Feature
+{
+    [TestClass]
+    public class BindingNamespacesTests : SeleniumTest
+    {
+        [TestMethod]
+        public void Feature_BindingNamespaces_BindingUsingNamespace()
+        {
+            RunInAllBrowsers(browser =>
+            {
+                browser.NavigateToUrl(SamplesRouteUrls.FeatureSamples_BindingNamespaces_BindingUsingNamespace);
+
+                var dataUis = new[]
+                {
+                    "viewModel-namespace",
+                    "fully-qualified-name",
+                    "alias-in-config",
+                    "import-in-config",
+                    "import-directive"
+                };
+
+                foreach (var dataUi in dataUis)
+                {
+                    browser.Single($"[data-ui='{dataUi}']").CheckIfTextEquals("Works");
+                }
+            });
+        }
+    }
+}
