c93d5f1ae2c6d31ad4301c94f10cfaaef3f67094
Tomas Herceg <tomas.herceg@riganti.cz>
2017-07-06 09:43:10 +0200

    Parameter binding UI tests Resolves #229

--- src/DotVVM.Samples.Tests/SamplesRouteUrls.cs
+++ src/DotVVM.Samples.Tests/SamplesRouteUrls.cs
@@ -139,6 +139,7 @@ namespace Dotvvm.Samples.Tests{
 			public static string FeatureSamples_MarkupControl_MarkupControlRegistration => "FeatureSamples/MarkupControl/MarkupControlRegistration";
 			public static string FeatureSamples_MarkupControl_MultiControlHierarchy => "FeatureSamples/MarkupControl/MultiControlHierarchy";
 			public static string FeatureSamples_NestedMasterPages_Content => "FeatureSamples/NestedMasterPages/Content";
+			public static string FeatureSamples_ParameterBinding_ParameterBinding => "FeatureSamples/ParameterBinding/ParameterBinding";
 			public static string FeatureSamples_PostBack_PostBackHandlers => "FeatureSamples/PostBack/PostBackHandlers";
 			public static string FeatureSamples_PostBack_PostbackUpdate => "FeatureSamples/PostBack/PostbackUpdate";
 			public static string FeatureSamples_PostBack_PostbackUpdateRepeater => "FeatureSamples/PostBack/PostbackUpdateRepeater";
