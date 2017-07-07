94b6455cd620f8ea7fecf34e00b439e9edc8549b
Tomas Herceg <tomas.herceg@riganti.cz>
2017-07-07 12:19:11 +0200

    Missing AssemblyInfo file added

---
+++ src/DotVVM.Tracing.ApplicationInsights/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DotVVM.Tracing.ApplicationInsights")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DotVVM.Tracing.ApplicationInsights")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("442c940c-47c5-422d-a676-e3e51200fc6a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.1.5")]
+[assembly: AssemblyVersion("1.1.5")]
+[assembly: AssemblyFileVersion("1.1.5")]
+
+[assembly: InternalsVisibleTo("DotVVM.Framework.Tests.Common, PublicKey=002400000480000094000000060200000024000052534131000400000100010007c544d7dbb49a258f8f7509b74f488534c1872c417b2fba85a66b1fba2465caafbaec56663cab0f072cd801f9e22753a85dc55247d4ace012a5eceac50d84c3b9044b9a37ac8baa5eb24dec644ad9fafcc869ad93b6603ffd5321124362cf0ab3684b89db1ed2aca4f175f6fbfc770bdc076396b36017c6dce5a4385c7b67b7")]
+[assembly: InternalsVisibleTo("DotVVM.Framework.Tests.Owin, PublicKey=002400000480000094000000060200000024000052534131000400000100010007c544d7dbb49a258f8f7509b74f488534c1872c417b2fba85a66b1fba2465caafbaec56663cab0f072cd801f9e22753a85dc55247d4ace012a5eceac50d84c3b9044b9a37ac8baa5eb24dec644ad9fafcc869ad93b6603ffd5321124362cf0ab3684b89db1ed2aca4f175f6fbfc770bdc076396b36017c6dce5a4385c7b67b7")]
+[assembly: InternalsVisibleTo("DotVVM.Framework.Tests.Owin, PublicKey=002400000480000094000000060200000024000052534131000400000100010007c544d7dbb49a258f8f7509b74f488534c1872c417b2fba85a66b1fba2465caafbaec56663cab0f072cd801f9e22753a85dc55247d4ace012a5eceac50d84c3b9044b9a37ac8baa5eb24dec644ad9fafcc869ad93b6603ffd5321124362cf0ab3684b89db1ed2aca4f175f6fbfc770bdc076396b36017c6dce5a4385c7b67b7")]
+[assembly: InternalsVisibleTo("DotVVM.Framework.Controls.BusinessPack, PublicKey=0024000004800000940000000602000000240000525341310004000001000100354ca4c100e5c0d5f7b0da9f369ab8ed9b7ac547cd0fffd41191d13c76f07c0635bab3d598bb6a50b9fbb2b3b5272c7d114084cb7484381e5dee0abc4d001e469281cdcc0625288f217ef70a23f2b276f4ce1ee7e7748bbc70f543f2b0c1e8b998e53113d33f22cb0aae963b7dcf08e1e36d98ad31bcf9ae94437807075f3fd5")]
