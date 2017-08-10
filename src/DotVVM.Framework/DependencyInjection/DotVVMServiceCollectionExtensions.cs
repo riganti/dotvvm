782ca8f5b069d9069fccfe4bba605d6a476a0ebf
exyi <exyi@outlook.cz>
2017-08-10 16:39:39 +0000

    added missing http redirect service registration

--- src/DotVVM.Framework/DependencyInjection/DotVVMServiceCollectionExtensions.cs
+++ src/DotVVM.Framework/DependencyInjection/DotVVMServiceCollectionExtensions.cs
@@ -56,6 +56,7 @@ namespace Microsoft.Extensions.DependencyInjection
             services.TryAddSingleton<IResourceHashService, DefaultResourceHashService>();
             services.TryAddSingleton<IStopwatch, DefaultStopwatch>();
             services.TryAddSingleton<JavascriptTranslator, JavascriptTranslator>();
+            services.TryAddSingleton<IHttpRedirectService, DefaultHttpRedirectService>();
 
             services.TryAddScoped<AggregateRequestTracer, AggregateRequestTracer>();
             services.TryAddScoped<ResourceManager, ResourceManager>();
