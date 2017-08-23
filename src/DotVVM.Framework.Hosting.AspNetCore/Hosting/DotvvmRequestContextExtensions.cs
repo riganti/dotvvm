f2ef95f85b8f21a1f44370eed57cb44c16fff4fd
Rumbeer <433545@mail.muni.cz>
2017-08-23 16:30:27 +0200

    Added extension method GetAuthentication that was removed when migrating to AspNet Core 2.0

--- src/DotVVM.Framework.Hosting.AspNetCore/Hosting/DotvvmRequestContextExtensions.cs
+++ src/DotVVM.Framework.Hosting.AspNetCore/Hosting/DotvvmRequestContextExtensions.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+using DotVVM.Framework.Hosting.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Authentication;
 
 namespace DotVVM.Framework.Hosting
 {
@@ -18,8 +18,22 @@ namespace DotVVM.Framework.Hosting
             {
                 throw new PlatformNotSupportedException("This method can be used only in ASP.NET Core hosting!");
             }
-
             return concreteContext.OriginalContext;
+        }
+
+        /// <summary>
+        /// Gets the Authentication Functionality available on current request
+        /// </summary>
+        /// <param name="context">The request context.</param>
+        public static AuthenticationManager GetAuthentication(this IDotvvmRequestContext context)
+        {
+            var concreteContext = context.HttpContext as DotvvmHttpContext;
+
+            if (concreteContext == null)
+            {
+                throw new PlatformNotSupportedException("This method can be used only in ASP.NET Core hosting!");
+            }
+            return new AuthenticationManager(concreteContext.OriginalContext);
         }
     }
 }
