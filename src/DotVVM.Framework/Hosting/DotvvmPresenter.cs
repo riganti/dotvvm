a9ad4681f30235af1f4acd269229f522f4fd1897
Tomas Herceg <tomas.herceg@riganti.cz>
2017-09-20 19:41:37 +0200

    Global filter handlig fixed

--- src/DotVVM.Framework/Hosting/DotvvmPresenter.cs
+++ src/DotVVM.Framework/Hosting/DotvvmPresenter.cs
@@ -110,7 +110,10 @@ namespace DotVVM.Framework.Hosting
             // get action filters
             var viewModelFilters = ActionFilterHelper.GetActionFilters<IViewModelActionFilter>(context.ViewModel.GetType().GetTypeInfo());
             viewModelFilters.AddRange(context.Configuration.Runtime.GlobalFilters.OfType<IViewModelActionFilter>());
+
             var requestFilters = ActionFilterHelper.GetActionFilters<IPageActionFilter>(context.ViewModel.GetType().GetTypeInfo());
+            requestFilters.AddRange(context.Configuration.Runtime.GlobalFilters.OfType<IPageActionFilter>());
+
             foreach (var f in requestFilters)
             {
                 await f.OnPageLoadingAsync(context);
