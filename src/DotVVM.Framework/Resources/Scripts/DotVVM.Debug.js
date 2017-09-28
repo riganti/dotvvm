6338b0966f1ebd378faec3cd8981f574ade75778
exyi <exyi@outlook.cz>
2017-09-28 14:09:02 +0000

    Added animations to debug window without jQuery

--- src/DotVVM.Framework/Resources/Scripts/DotVVM.Debug.js
+++ src/DotVVM.Framework/Resources/Scripts/DotVVM.Debug.js
@@ -1,38 +1,31 @@
 (function() {
-    var div = "<div id='debugWindow'><h1></h1><button type='button' id='closeDebugWindow'>Close</button><iframe /><div id='debugFooter'></div></div>";
+    var div = "<div id='debugWindow'><h1></h1><button type='button' id='closeDebugWindow'>Close</button><iframe></iframe><div id='debugFooter'></div></div>";
     var parser = new DOMParser();
-    var debugDiv = parser.parseFromString(div, "text/xml" );
-	
-    document.body.appendChild(debugDiv);
-
-    var debugWindow = document.body.querySelector("#debugWindow")
-
-    debugWindow.style.display = 'none';	
+    var debugWindow = parser.parseFromString(div, "text/html").firstElementChild.querySelector("#debugWindow");
+    debugWindow.style.display = 'none';
     debugWindow.style.flexFlow = "column";
     debugWindow.style.zIndex = 2147483647;
     debugWindow.style.position = "fixed";
     debugWindow.style.width = "100%";
-    debugWindow.style.height = "100%";
+    debugWindow.style.height = "100vh";
     debugWindow.style.backgroundColor = "white";
     debugWindow.style.top = 0;
     debugWindow.style.left = 0;
+    document.body.appendChild(debugWindow);
 
-    var debugNotification = parser.parseFromString("<div id='debugNotification'></div>", "text/xml");
-
-    document.body.appendChild(debugNotification);
-
-    var notificationWindow = document.body.querySelector("#debugNotification")
-
-    notificationWindow.style.display = "none";
-    notificationWindow.style.zIndex = 2147483647,
-    notificationWindow.style.position = "fixed",
-    notificationWindow.style.top = "0px",
-    notificationWindow.style.right = "0px",
-    notificationWindow.style.backgroundColor = "darkred",
-    notificationWindow.style.color = "white",
-    notificationWindow.style.fontSize = "1.0em",
-    notificationWindow.style.width = "400px",
-    notificationWindow.style.padding =  "20px"
+    var notificationWindow = parser.parseFromString("<div id='debugNotification'></div>", "text/html").firstElementChild.querySelector("#debugnotification");
+    // notificationWindow.style.display = "none";
+    notificationWindow.style.zIndex = 2147483647;
+    notificationWindow.style.position = "fixed";
+    notificationWindow.style.top = "0px";
+    notificationWindow.style.right = "0px";
+    notificationWindow.style.backgroundColor = "darkred";
+    notificationWindow.style.color = "white";
+    notificationWindow.style.fontSize = "1.0em";
+    notificationWindow.style.width = "400px";
+    notificationWindow.style.padding =  "20px";
+    notificationWindow.style.opacity = 0;
+    document.body.appendChild(notificationWindow);
 
     notificationWindow.addEventListener("click", function() {
         setTimeout(function () {
@@ -41,24 +34,24 @@
     });
 
     var closeDebugWindow = debugWindow.querySelector("#closeDebugWindow");
-    closeDebugWindow.addEventListener("click", function() {        
+    closeDebugWindow.addEventListener("click", function() {
         debugWindow.style.display = "none";
     });
 
-    closeDebugWindow.style.position: "absolute";
+    closeDebugWindow.style.position = "absolute";
     closeDebugWindow.style.top = 0;
     closeDebugWindow.style.right = 0;
 
     var debugFooter = debugWindow.querySelector("#debugFooter");
-    debugWindow.querySelector("#debugFooter").style.flex = "0 1 auto";
+    debugFooter.style.flex = "0 1 auto";
 
     var h1 = debugWindow.querySelector("h1");
     h1.style.flex = "0 1 auto";
 
     var iframe = debugWindow.querySelector("iframe");
 
-    iframe.style.flex "1 1 auto";
-    iframe.style.width "100%";
+    iframe.style.flex = "1 100 auto";
+    iframe.style.width = "100%";
 
     dotvvm.evaluator.tryEval = function(func) {
         try {
@@ -74,10 +67,11 @@
         console.log("XmlHttpRequest: ", e.xhr);
         console.log("ViewModel: ", e.viewModel);
         if (e.handled) return;
-        debugWindow.querySelector("h1").textContent = "DotVVM Debugger: Error " + (e.xhr.status ? e.xhr.status + ": " + e.xhr.statusText + "" : "XmlHttpRequest failed, maybe internet connection is lost or url is malformed");        
-        var iframe = debugWindow.querySelector("iframe");		
-        var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;		
-		iframeDocument.querySelector('html').innerHTML = e.xhr.responseText;
+        debugWindow.querySelector("h1").textContent = "DotVVM Debugger: Error " + (e.xhr.status ? e.xhr.status + ": " + e.xhr.statusText + "" : "XmlHttpRequest failed, maybe internet connection is lost or url is malformed");
+        var iframe = debugWindow.querySelector("iframe");
+        var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+        iframeDocument.querySelector('html').innerHTML = e.xhr.responseText;
+        // debugWindow.height = window.innerHeight;
         debugWindow.style.display = "flex";
         e.handled = true;
     });
@@ -93,16 +87,21 @@
     }
 
     function displayPostbackAbortedWarning(message) {
-        notificationWindow.textContent = message;
+        notificationWindow.style.display = "block";
+        notificationWindow.style.opacity = 0;
         setTimeout(function () {
-            notificationWindow.style.display = "block";
-        }, 200);
+            notificationWindow.textContent = message;
+            notificationWindow.style.transition = "opacity 0.5s"
+            notificationWindow.style.opacity = 1;
 
-        setTimeout(function() {
-            setTimeout(function () {
-                notificationWindow.style.display = "none";
-            }, 1000);
-        }, 10000)
+            setTimeout(function() {
+                notificationWindow.style.transition = "opacity 1s"
+                notificationWindow.style.opacity = 0;
+                setTimeout(function () {
+                    notificationWindow.style.display = "none";
+                }, 1000);
+            }, 7000)
+        }, 0)
     }
 
     dotvvm.events.afterPostback.subscribe(function (e) {
