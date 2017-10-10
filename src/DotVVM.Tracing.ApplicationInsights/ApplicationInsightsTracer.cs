9387f3b7924fe40608b6391abee9b497b4d59f61
exyi <exyi@outlook.cz>
2017-10-10 14:03:05 +0000

    Removed IStopwatch from ApplicationInsights

--- src/DotVVM.Tracing.ApplicationInsights/ApplicationInsightsTracer.cs
+++ src/DotVVM.Tracing.ApplicationInsights/ApplicationInsightsTracer.cs
@@ -4,31 +4,25 @@ using DotVVM.Framework.Hosting;
 using DotVVM.Framework.Runtime.Tracing;
 using DotVVM.Framework.Utils;
 using Microsoft.ApplicationInsights;
+using System.Diagnostics;
 
 namespace DotVVM.Tracing.ApplicationInsights
 {
     public class ApplicationInsightsTracer : IRequestTracer
     {
         private readonly TelemetryClient telemetryClient;
-        private readonly IStopwatch stopwatch;
 
-        private long? lastStopwatchTime;
+        private Stopwatch stopwatch = Stopwatch.StartNew();
 
-        public ApplicationInsightsTracer(TelemetryClient telemetryClient, IStopwatch stopwatch)
+        public ApplicationInsightsTracer(TelemetryClient telemetryClient)
         {
             this.telemetryClient = telemetryClient;
-            this.stopwatch = stopwatch;
         }
 
         public Task TraceEvent(string eventName, IDotvvmRequestContext context)
         {
-            if (lastStopwatchTime.HasValue)
-            {
-                var duration = stopwatch.GetElapsedMiliseconds() - lastStopwatchTime.Value;
-                telemetryClient.TrackMetric(eventName, duration);
-            }
-
-            lastStopwatchTime = stopwatch.GetElapsedMiliseconds();
+            telemetryClient.TrackMetric(eventName, stopwatch.ElapsedMilliseconds);
+            stopwatch.Restart();
 
             return TaskUtils.GetCompletedTask();
         }
