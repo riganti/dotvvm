bf0d888d5136fb346559dae44e32e60abb9c9457
exyi <exyi@outlook.cz>
2017-10-29 16:03:39 +0000

    localization Added LocalizablePresenter
    It's a utility for localizing specific route based on route parameter or query string

---
+++ src/DotVVM.Framework/Hosting/LocalizablePresenter.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DotVVM.Framework.Hosting
+{
+    public class LocalizablePresenter : IDotvvmPresenter
+    {
+        private readonly Func<IDotvvmRequestContext, Task> nextPresenter;
+        private readonly Func<IDotvvmRequestContext, CultureInfo> getCulture;
+
+        public LocalizablePresenter(
+            Func<IDotvvmRequestContext, CultureInfo> getCulture,
+            Func<IDotvvmRequestContext, Task> nextPresenter
+        )
+        {
+            this.getCulture = getCulture;
+            this.nextPresenter = nextPresenter;
+        }
+
+        public Task ProcessRequest(IDotvvmRequestContext context)
+        {
+            var culture = this.getCulture(context);
+            if (culture != null)
+                CultureInfo.CurrentCulture = CultureInfo.CurrentUICulture = culture;
+            return this.nextPresenter(context);
+        }
+
+        public static Func<LocalizablePresenter> BasedOnParameter(string name)
+        {
+            var presenter = new LocalizablePresenter(
+                context => context.Parameters.TryGetValue(name, out var value) && !string.IsNullOrEmpty(value as string) ? new CultureInfo((string)value) : null,
+                context => context.Services.GetRequiredService<IDotvvmPresenter>().ProcessRequest(context)
+            );
+            return () => presenter;
+        }
+
+        public static Func<LocalizablePresenter> BasedOnQuery(string name)
+        {
+            var presenter = new LocalizablePresenter(
+                context => context.Query.TryGetValue(name, out var value) && !string.IsNullOrEmpty(value as string) ? new CultureInfo((string)value) : null,
+                context => context.Services.GetRequiredService<IDotvvmPresenter>().ProcessRequest(context)
+            );
+            return () => presenter;
+        }
+    }
+}
--- src/DotVVM.Samples.Common/ViewModels/FeatureSamples/Localization/LocalizationNestedViewModel.cs
+++ src/DotVVM.Samples.Common/ViewModels/FeatureSamples/Localization/LocalizationNestedViewModel.cs
@@ -7,15 +7,6 @@ namespace DotVVM.Samples.BasicSamples.ViewModels.FeatureSamples.Localization
     {
         public override Task Init()
         {
-            if (Context.Query.ContainsKey("lang") && Context.Query["lang"] == "cs-CZ")
-            {
-                Context.ChangeCurrentCulture("cs-CZ");
-            }
-            else
-            {
-                Context.ChangeCurrentCulture("en-US");
-            }
-
             return base.Init();
         }
     }
