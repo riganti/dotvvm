f9152d31a5c6e1bbb8026759449a8f0e2115bd19
Adam Štěpánek <adam.stepanek@riganti.cz>
2017-11-03 08:32:55 +0100

    Added CustomValidation sample.

---
+++ src/DotVVM.Samples.Common/ViewModels/FeatureSamples/Validation/CustomValidationViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using DotVVM.Framework.ViewModel;
+using DotVVM.Framework.ViewModel.Validation;
+
+namespace DotVVM.Samples.Common.ViewModels.FeatureSamples.Validation
+{
+    public class CustomValidationViewModel : DotvvmViewModelBase
+    {
+        private readonly string[] allowedNames = new[]
+        {
+            "John",
+            "James",
+            "Ted"
+        };
+
+        public Customer Detail { get; set; } = new Customer { Name = "Ted", Age = 42 };
+
+        public bool UseKnockoutNotation { get; set; }
+
+        public override Task PreRender()
+        {
+            if (!allowedNames.Contains(Detail.Name))
+            {
+                if (UseKnockoutNotation)
+                {
+                    Context.ModelState.Errors.Add(
+                        new ViewModelValidationError
+                        {
+                            ErrorMessage = "This error uses the Knockout JS notation.",
+                            PropertyPath = "$context.Detail().Name"
+                        });
+                }
+                else
+                {
+                    Context.ModelState.Errors.Add(
+                        new ViewModelValidationError
+                        {
+                            ErrorMessage = "This error uses only the C# dot notation.",
+                            PropertyPath = "Detail.Name"
+                        });
+                }
+            }
+            Context.FailOnInvalidModelState();
+            return base.PreRender();
+        }
+
+        public class Customer
+        {
+            [Required]
+            public int Age { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+        }
+    }
+}
