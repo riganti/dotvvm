a16de33b21b5bcd20b5d92081823676b273d8471
Dušan Janošík <dusan@djanosik.cz>
2017-11-13 13:39:13 +0100

    Reverts changes to NullableDateTime validation test to make it fail again.

--- src/DotVVM.Samples.Common/ViewModels/FeatureSamples/Validation/NullableDateTimeValidationViewModel.cs
+++ src/DotVVM.Samples.Common/ViewModels/FeatureSamples/Validation/NullableDateTimeValidationViewModel.cs
@@ -6,17 +6,10 @@ namespace DotVVM.Samples.BasicSamples.ViewModels.FeatureSamples.Validation
 {
     public class NullableDateTimeValidationViewModel : DotvvmViewModelBase
     {
-
         [DotvvmEnforceClientFormat]
         public DateTime? DateTimeTestValue { get; set; } = DateTime.Now;
 
-        [DotvvmEnforceClientFormat]
-        public DateTime? DateTimeTestValue2 { get; set; } = DateTime.Now;
-
         public bool DateTimeTestResult { get; set; }
-
-        private DateTime defaultValue = new DateTime(2016, 3, 1);
-
 
         public void ValidateNullableDateTime()
         {
