26cbc06b930c1f0bbd5322ed82c30eaee17cbbd9
Ladislav Šesták <ladislav.sestak@riganti.cz>
2017-11-25 19:51:26 +0100

    Owin.AppBuilderExtensions - Missing  namespace

--- src/DotVVM.Framework.Hosting.Owin/AppBuilderExtensions.cs
+++ src/DotVVM.Framework.Hosting.Owin/AppBuilderExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using DotVVM.Framework.Configuration;
 using DotVVM.Framework.Hosting;
 using DotVVM.Framework.Hosting.Middlewares;
