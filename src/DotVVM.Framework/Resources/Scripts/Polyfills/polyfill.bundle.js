fb9d1e87c2336f39d7ac20319efb855f0a847970
Adam Jež <adamjez@outlook.cz>
2017-12-06 15:34:33 +0100

    Removed firstElementChild polyfill

--- src/DotVVM.Framework/Resources/Scripts/Polyfills/polyfill.bundle.js
+++ src/DotVVM.Framework/Resources/Scripts/Polyfills/polyfill.bundle.js
@@ -701,25 +701,3 @@
     }
     self.fetch.polyfill = true
 })(typeof self !== 'undefined' ? self : this);
-
-// Source: https://developer.mozilla.org/en-US/docs/Web/API/ParentNode/firstElementChild
-// Overwrites native 'firstElementChild' prototype.
-// Adds Document & DocumentFragment support for IE9 & Safari.
-// Returns array instead of HTMLCollection.
-; (function (constructor) {
-    if (constructor &&
-        constructor.prototype &&
-        constructor.prototype.firstElementChild == null) {
-        Object.defineProperty(constructor.prototype, 'firstElementChild', {
-            get: function () {
-                var node, nodes = this.childNodes, i = 0;
-                while (node = nodes[i++]) {
-                    if (node.nodeType === 1) {
-                        return node;
-                    }
-                }
-                return null;
-            }
-        });
-    }
-})(window.Node || window.Element);
