4fd1392bb709dd08e2c83f5b2c805cc1785a5e36
Tomas Herceg <tomas.herceg@riganti.cz>
2017-12-19 10:32:04 +0100

    DataPager postback issue fixed

--- src/DotVVM.Framework/Controls/DataPager.cs
+++ src/DotVVM.Framework/Controls/DataPager.cs
@@ -364,7 +364,7 @@ namespace DotVVM.Framework.Controls
             }
             writer.AddKnockoutDataBind("css", "{ 'active': $data == $parent.PagingOptions().PageIndex()}");
             li = new HtmlGenericControl("li");
-            li.SetValue(Internal.PathFragmentProperty, "PagingOptions.NearPageIndexes[$index]");
+            li.SetValue(Internal.PathFragmentProperty, "PagingOptions.nearPageIndexes[$index]");
             var link = new LinkButton();
             li.Children.Add(link);
             link.SetDataContextType(currentPageTextContext);
