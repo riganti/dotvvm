97380bba4f54a995f0df48ac8004887ba9fee78b
Adam Jež <adamjez@outlook.cz>
2018-01-05 18:03:16 +0100

    Added UI tests for url building in SPA page

--- src/DotVVM.Samples.Tests/Control/RouteLinkTests.cs
+++ src/DotVVM.Samples.Tests/Control/RouteLinkTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using DotVVM.Testing.Abstractions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Riganti.Selenium.Core;
 using Riganti.Selenium.Core.Abstractions;
 
 namespace DotVVM.Samples.Tests.Control
@@ -26,36 +27,58 @@ namespace DotVVM.Samples.Tests.Control
         }
 
         [TestMethod]
-        [SampleReference(nameof(SamplesRouteUrls.ControlSamples_RouteLink_TestRoute))]
+        [SampleReference(nameof(SamplesRouteUrls.ControlSamples_RouteLink_RouteLinkUrlGen))]
         public void Control_RouteLink_RouteLinkUrlGeneration()
         {
             RunInAllBrowsers(browser => {
                 browser.NavigateToUrl(SamplesRouteUrls.ControlSamples_RouteLink_RouteLinkUrlGen);
 
-                void checkNavigatedUrl(string selector, string relativeUrl)
-                {
-                    browser.Single(selector).Click();
-                    browser.CheckUrl(relativeUrl, UrlKind.Relative, UriComponents.PathAndQuery);
-                    browser.NavigateBack();
-                }
-
-                checkNavigatedUrl("a[data-ui='optional-parameter-client']", "/ControlSamples/Repeater/RouteLink");
-                checkNavigatedUrl("a[data-ui='optional-parameter-server']", "/ControlSamples/Repeater/RouteLink");
-
-                checkNavigatedUrl("a[data-ui='0-parameters']", "/");
-
-                checkNavigatedUrl("a[data-ui='optional-parameter-prefixed-client']", "/ControlSamples/Repeater/RouteLink");
-                checkNavigatedUrl("a[data-ui='optional-parameter-prefixed-server']", "/ControlSamples/Repeater/RouteLink");
-
-                checkNavigatedUrl("a[data-ui='parameter-prefixed-client']", "/ControlSamples/Repeater/RouteLink/id-1");
-                checkNavigatedUrl("a[data-ui='parameter-prefixed-server']", "/ControlSamples/Repeater/RouteLink/id-1");
-
-                checkNavigatedUrl("a[data-ui='optional-parameter-at-start-client']", "/ControlSamples/Repeater/RouteLink");
-                checkNavigatedUrl("a[data-ui='optional-parameter-at-start-server']", "/ControlSamples/Repeater/RouteLink");
-
-                checkNavigatedUrl("a[data-ui='optional-prefixed-parameter-at-start-client']", "/id-1/ControlSamples/Repeater/RouteLink");
-                checkNavigatedUrl("a[data-ui='optional-prefixed-parameter-at-start-client']", "/id-1/ControlSamples/Repeater/RouteLink");
+                CheckUrlGenerationMethod(browser);
             });
+        }
+
+        [TestMethod]
+        [SampleReference(nameof(SamplesRouteUrls.ControlSamples_RouteLink_RouteLinkSpaUrlGen))]
+        public void Control_RouteLink_RouteLinkSpaUrlGeneration()
+        {
+            RunInAllBrowsers(browser => {
+                browser.NavigateToUrl(SamplesRouteUrls.ControlSamples_RouteLink_RouteLinkSpaUrlGen);
+
+                CheckUrlGenerationMethod(browser, true);
+            });
+        }
+
+        private static void CheckUrlGenerationMethod(IBrowserWrapperFluentApi browser, bool isSpaLink = false)
+        {
+            void checkNavigatedUrl(string selector, string relativeUrl)
+            {
+                var href = browser.Single(selector).GetAttribute("href");
+                if (isSpaLink)
+                {
+                    Assert.AreEqual("#!" + relativeUrl, new Uri(href).Fragment);
+                }
+                else
+                {
+                    Assert.AreEqual(relativeUrl, new Uri(href).AbsolutePath);
+                }
+            }
+
+            checkNavigatedUrl("a[data-ui='optional-parameter-client']", "/ControlSamples/Repeater/RouteLink");
+            checkNavigatedUrl("a[data-ui='optional-parameter-server']", "/ControlSamples/Repeater/RouteLink");
+
+            checkNavigatedUrl("a[data-ui='0-parameters']", "/");
+
+            checkNavigatedUrl("a[data-ui='optional-parameter-prefixed-client']", "/ControlSamples/Repeater/RouteLink");
+            checkNavigatedUrl("a[data-ui='optional-parameter-prefixed-server']", "/ControlSamples/Repeater/RouteLink");
+
+            checkNavigatedUrl("a[data-ui='parameter-prefixed-client']", "/ControlSamples/Repeater/RouteLink/id-1");
+            checkNavigatedUrl("a[data-ui='parameter-prefixed-server']", "/ControlSamples/Repeater/RouteLink/id-1");
+
+            checkNavigatedUrl("a[data-ui='optional-parameter-at-start-client']", "/ControlSamples/Repeater/RouteLink");
+            checkNavigatedUrl("a[data-ui='optional-parameter-at-start-server']", "/ControlSamples/Repeater/RouteLink");
+
+            checkNavigatedUrl("a[data-ui='optional-prefixed-parameter-at-start-client']", "/id-1/ControlSamples/Repeater/RouteLink");
+            checkNavigatedUrl("a[data-ui='optional-prefixed-parameter-at-start-client']", "/id-1/ControlSamples/Repeater/RouteLink");
         }
 
         [TestMethod]
