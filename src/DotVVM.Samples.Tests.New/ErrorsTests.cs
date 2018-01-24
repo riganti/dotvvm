b3b679213772b223921e09358b0c00f09ddb5d48
Ladislav Šesták <ladislav.sestak@riganti.cz>
2018-01-24 22:34:22 +0100

    ErrorTests moved to new test structure

--- src/DotVVM.Samples.Tests.New/ErrorsTests.cs
+++ src/DotVVM.Samples.Tests.New/ErrorsTests.cs
@@ -3,37 +3,38 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using System;
 using System.Linq;
+using DotVVM.Samples.Tests.New;
 using DotVVM.Testing.Abstractions;
+using Riganti.Selenium.Core;
+using Xunit;
+using Xunit.Abstractions;
 
 namespace DotVVM.Samples.Tests
 {
-    [TestClass]
     public class ErrorsTests : AppSeleniumTest
     {
-        [TestMethod]
+        [Fact]
         public void Error_MissingViewModel()
         {
-            RunInAllBrowsers(browser =>
-            {
+            RunInAllBrowsers(browser => {
                 browser.NavigateToUrl(SamplesRouteUrls.Errors_MissingViewModel);
-                browser.First("p.summary")
-                    .CheckIfInnerText(
-                        s =>
-                            s.Contains("DotVVM.Framework.Compilation.DotvvmCompilationException") &&
-                            s.Contains("@viewModel") &&
-                            s.Contains("missing")
-                        );
+                AssertUI.InnerText(browser.First("p.summary")
+                      ,
+                          s =>
+                              s.Contains("DotVVM.Framework.Compilation.DotvvmCompilationException") &&
+                              s.Contains("@viewModel") &&
+                              s.Contains("missing")
+                          );
             });
         }
 
-        [TestMethod]
+        [Fact]
         public void Error_InvalidViewModel()
         {
-            RunInAllBrowsers(browser =>
-            {
+            RunInAllBrowsers(browser => {
                 browser.NavigateToUrl(SamplesRouteUrls.Errors_InvalidViewModel);
-                browser.First("p.summary")
-                    .CheckIfInnerText(
+                AssertUI.InnerText(browser.First("p.summary")
+                    ,
                         s =>
                             s.Contains("DotVVM.Framework.Compilation.DotvvmCompilationException", StringComparison.OrdinalIgnoreCase) &&
                             s.Contains("Could not resolve type 'invalid'", StringComparison.OrdinalIgnoreCase)
@@ -41,210 +42,194 @@ namespace DotVVM.Samples.Tests
             });
         }
 
-        [TestMethod]
+        [Fact]
         public void Error_NonExistingControl()
         {
-            RunInAllBrowsers(browser =>
-            {
+            RunInAllBrowsers(browser => {
                 browser.NavigateToUrl(SamplesRouteUrls.Errors_NonExistingControl);
-                browser.First("[class=exceptionMessage]")
-                    .CheckIfInnerText(
+                AssertUI.InnerText(browser.First("[class=exceptionMessage]")
+                    ,
                         s =>
                             s.Contains("dot:NonExistingControl") &&
                             s.Contains("could not be resolved")
                             );
-                browser.First("[class='errorUnderline']")
-                    .CheckIfInnerTextEquals("<dot:NonExistingControl />", false);
+                AssertUI.InnerTextEquals(browser.First("[class='errorUnderline']")
+                    , "<dot:NonExistingControl />", false);
             });
         }
 
-        [TestMethod]
+        [Fact]
         public void Error_NonExistingProperty()
         {
-            RunInAllBrowsers(browser =>
-            {
+            RunInAllBrowsers(browser => {
                 browser.NavigateToUrl(SamplesRouteUrls.Errors_NonExistingProperty);
-                browser.First("[class='exceptionMessage']")
-                    .CheckIfInnerText(
+                AssertUI.InnerText(browser.First("[class='exceptionMessage']")
+                    ,
                         s =>
                             s.Contains("does not have a property 'NonExistingProperty'")
                             );
 
-                browser.First("[class='errorUnderline']")
-                    .CheckIfInnerText(s => s.Contains("NonExistingProperty"));
+                AssertUI.InnerText(browser.First("[class='errorUnderline']")
+                    , s => s.Contains("NonExistingProperty"));
             });
         }
 
-        [TestMethod]
+        [Fact]
         public void Error_NotAllowedHardCodedPropertyValue()
         {
-            RunInAllBrowsers(browser =>
-            {
+            RunInAllBrowsers(browser => {
                 browser.NavigateToUrl(SamplesRouteUrls.Errors_NotAllowedHardCodedPropertyValue);
-                browser.First("[class='exceptionMessage']")
-                .CheckIfInnerText(
+                AssertUI.InnerText(browser.First("[class='exceptionMessage']")
+                ,
                         s =>
                             s.ToLower().Contains("String was not recognized as a valid Boolean.".ToLower())
                             , "Expected message is 'String was not recognized as a valid Boolean.'");
 
-                browser.First("[class='errorUnderline']")
-                    .CheckIfInnerText(s => s.Contains("NotAllowedHardCodedValue"));
+                AssertUI.InnerText(browser.First("[class='errorUnderline']")
+                    , s => s.Contains("NotAllowedHardCodedValue"));
             });
         }
 
-        [TestMethod]
+        [Fact]
         public void Error_WrongPropertyValue()
         {
-            RunInAllBrowsers(browser =>
-            {
+            RunInAllBrowsers(browser => {
                 browser.NavigateToUrl(SamplesRouteUrls.Errors_WrongPropertyValue);
-                browser.First("[class='exceptionMessage']")
-                    .CheckIfInnerText(s => s.Contains("Could not implicitly convert expression"));
+                AssertUI.InnerText(browser.First("[class='exceptionMessage']")
+                    , s => s.Contains("Could not implicitly convert expression"));
 
-                browser.First("[class='errorUnderline']")
-                    .CheckIfInnerText(s => s.Contains("NotAllowedValue"));
+                AssertUI.InnerText(browser.First("[class='errorUnderline']")
+                    , s => s.Contains("NotAllowedValue"));
             });
         }
 
-        [TestMethod]
+        [Fact]
         public void Error_MissingRequiredProperty()
         {
-            RunInAllBrowsers(browser =>
-            {
+            RunInAllBrowsers(browser => {
                 browser.NavigateToUrl(SamplesRouteUrls.Errors_MissingRequiredProperty);
-                browser.First("p.summary").CheckIfInnerText(s => s.Contains("must be set"));
-                browser.First("[class='source-errorLine']").CheckIfInnerText(s => s.Contains("dot:CheckBox"));
+                AssertUI.InnerText(browser.First("p.summary"), s => s.Contains("must be set"));
+                AssertUI.InnerText(browser.First("[class='source-errorLine']"), s => s.Contains("dot:CheckBox"));
             });
         }
 
-        [TestMethod]
+        [Fact]
         public void Error_MissingRequiredProperty2()
         {
-            RunInAllBrowsers(browser =>
-            {
+            RunInAllBrowsers(browser => {
                 browser.NavigateToUrl(SamplesRouteUrls.Errors_MissingRequiredProperty2);
 
-                browser.First("p.summary").CheckIfInnerText(s => s.Contains("is missing required properties"));
-                browser.First("[class='errorUnderline']").CheckIfInnerText(s => s.Contains("dot:GridViewTextColumn "));
+                AssertUI.InnerText(browser.First("p.summary"), s => s.Contains("is missing required properties"));
+                AssertUI.InnerText(browser.First("[class='errorUnderline']"), s => s.Contains("dot:GridViewTextColumn "));
             });
         }
 
-        [TestMethod]
+        [Fact]
         public void Error_BindingInvalidProperty()
         {
-            RunInAllBrowsers(browser =>
-            {
+            RunInAllBrowsers(browser => {
                 browser.NavigateToUrl(SamplesRouteUrls.Errors_BindingInvalidProperty);
 
-                browser.First(".exceptionMessage").CheckIfInnerText(s => s.Contains("Could not resolve identifier"));
-                browser.First("[class='source-errorLine']").CheckIfInnerText(s => s.Contains("InvalidPropertyName"));
-                browser.First("[class='errorUnderline']").CheckIfInnerText(s => s.Contains("InvalidPropertyName"));
+                AssertUI.InnerText(browser.First(".exceptionMessage"), s => s.Contains("Could not resolve identifier"));
+                AssertUI.InnerText(browser.First("[class='source-errorLine']"), s => s.Contains("InvalidPropertyName"));
+                AssertUI.InnerText(browser.First("[class='errorUnderline']"), s => s.Contains("InvalidPropertyName"));
             });
         }
 
-        [TestMethod]
+        [Fact]
         public void Error_BindingInvalidCommand()
         {
-            RunInAllBrowsers(browser =>
-            {
+            RunInAllBrowsers(browser => {
                 browser.NavigateToUrl(SamplesRouteUrls.Errors_BindingInvalidCommand);
 
-                browser.First(".exceptionMessage").CheckIfInnerText(s => s.Contains("Could not resolve identifier") && s.Contains("NonExistingCommand"));
-                browser.First("[class='source-errorLine']").CheckIfInnerText(s => s.Contains("NonExistingCommand"));
-                browser.First("[class='errorUnderline']").CheckIfInnerText(s => s.Contains("NonExistingCommand"));
+                AssertUI.InnerText(browser.First(".exceptionMessage"), s => s.Contains("Could not resolve identifier") && s.Contains("NonExistingCommand"));
+                AssertUI.InnerText(browser.First("[class='source-errorLine']"), s => s.Contains("NonExistingCommand"));
+                AssertUI.InnerText(browser.First("[class='errorUnderline']"), s => s.Contains("NonExistingCommand"));
             });
         }
 
-        [TestMethod]
+        [Fact]
         public void Error_MalformedBinding()
         {
-            RunInAllBrowsers(browser =>
-            {
+            RunInAllBrowsers(browser => {
                 browser.NavigateToUrl(SamplesRouteUrls.Errors_MalformedBinding);
 
-                browser.First(".exceptionMessage").CheckIfInnerText(s => s.Contains("Unexpected token"));
-                browser.First("[class='errorUnderline']").CheckIfInnerText(s => s.Contains("!"));
+                AssertUI.InnerText(browser.First(".exceptionMessage"), s => s.Contains("Unexpected token"));
+                AssertUI.InnerText(browser.First("[class='errorUnderline']"), s => s.Contains("!"));
             });
         }
 
-        [TestMethod]
+        [Fact]
         public void Error_EmptyBinding()
         {
-            RunInAllBrowsers(browser =>
-            {
+            RunInAllBrowsers(browser => {
                 browser.NavigateToUrl(SamplesRouteUrls.Errors_EmptyBinding);
 
-                browser.First(".exceptionMessage").CheckIfInnerText(s => s.Contains("Identifier name was expected"));
-                browser.First(".errorUnderline").CheckIfInnerText(s => s.Contains("{{value: }}"));
+                AssertUI.InnerText(browser.First(".exceptionMessage"), s => s.Contains("Identifier name was expected"));
+                AssertUI.InnerText(browser.First(".errorUnderline"), s => s.Contains("{{value: }}"));
             });
         }
 
-        [TestMethod]
+        [Fact]
         public void Error_MasterPageRequiresDifferentViewModel()
         {
-            RunInAllBrowsers(browser =>
-            {
+            RunInAllBrowsers(browser => {
                 browser.NavigateToUrl(SamplesRouteUrls.Errors_MasterPageRequiresDifferentViewModel);
 
                 //TODO:  !!! In error page, viewModel directive should by underlined !!!
-                browser.First("p.summary").CheckIfInnerText(s => s.Contains("Master page requires viewModel"));
-                //browser.First("[class='errorUnderline']").CheckIfInnerText(s => s.Contains("DotVVM.Samples.BasicSamples.ViewModels.EmptyViewModel, DotVVM.Samples.Common"));
+                AssertUI.InnerText(browser.First("p.summary"), s => s.Contains("Master page requires viewModel"));
+                //browser.First("[class='errorUnderline']"),s => s.Contains("DotVVM.Samples.BasicSamples.ViewModels.EmptyViewModel, DotVVM.Samples.Common"));
             });
         }
 
-        [TestMethod]
+        [Fact]
         public void Error_ControlUsageValidation()
         {
-            RunInAllBrowsers(browser =>
-            {
+            RunInAllBrowsers(browser => {
                 browser.NavigateToUrl(SamplesRouteUrls.Errors_ControlUsageValidation);
-                browser.First("p.summary").CheckIfInnerText(s => s.Contains("Text property and inner content") && s.Contains("cannot be set at the same time"));
-                browser.First(".errorUnderline").CheckIfInnerText(s => s.Contains("Click=\"{command: 5}\" Text=\"Text property\""));
+                AssertUI.InnerText(browser.First("p.summary"), s => s.Contains("Text property and inner content") && s.Contains("cannot be set at the same time"));
+                AssertUI.InnerText(browser.First(".errorUnderline"), s => s.Contains("Click=\"{command: 5}\" Text=\"Text property\""));
             });
         }
 
-        [TestMethod]
+        [Fact]
         public void Error_EncryptedPropertyInValueBinding()
         {
-            RunInAllBrowsers(browser =>
-            {
+            RunInAllBrowsers(browser => {
                 browser.NavigateToUrl(SamplesRouteUrls.Errors_EncryptedPropertyInValueBinding);
-                browser.First(".exceptionMessage").CheckIfInnerText(s => s.Contains("is encrypted and cannot be used in JS"));
-                browser.First(".errorUnderline").CheckIfInnerText(s => s.Contains("{{value: SomeProperty}}"));
+                AssertUI.InnerText(browser.First(".exceptionMessage"), s => s.Contains("is encrypted and cannot be used in JS"));
+                AssertUI.InnerText(browser.First(".errorUnderline"), s => s.Contains("{{value: SomeProperty}}"));
             });
         }
 
-        [TestMethod]
+        [Fact]
         public void Error_FieldInValueBinding()
         {
-            RunInAllBrowsers(browser =>
-            {
+            RunInAllBrowsers(browser => {
                 browser.NavigateToUrl(SamplesRouteUrls.Errors_FieldInValueBinding);
-                browser.First(".exceptionMessage").CheckIfInnerText(s => s.Contains("Can not translate field"));
-                browser.First(".errorUnderline").CheckIfInnerText(s => s.Contains("{{value: SomeField}}"));
+                AssertUI.InnerText(browser.First(".exceptionMessage"), s => s.Contains("Can not translate field"));
+                AssertUI.InnerText(browser.First(".errorUnderline"), s => s.Contains("{{value: SomeField}}"));
             });
         }
 
 
-        [TestMethod]
+        [Fact]
         [SampleReference(nameof(SamplesRouteUrls.ControlSamples_Button_InputTypeButton_HtmlContentInside))]
         public void Error_InputTypeButton_HtmlContentInside()
         {
-            RunInAllBrowsers(browser =>
-            {
+            RunInAllBrowsers(browser => {
                 browser.NavigateToUrl(SamplesRouteUrls.ControlSamples_Button_InputTypeButton_HtmlContentInside);
-                browser.First("p.summary").CheckIfInnerText(s => s.Contains("control cannot have inner HTML"));
+                AssertUI.InnerText(browser.First("p.summary"), s => s.Contains("control cannot have inner HTML"));
             });
         }
 
-        [TestMethod]
+        [Fact]
         public void Error_MarkupControlInvalidViewModel()
         {
-            RunInAllBrowsers(browser =>
-            {
+            RunInAllBrowsers(browser => {
                 browser.NavigateToUrl(SamplesRouteUrls.Errors_MarkupControlInvalidViewModel);
-                browser.First("p.summary")
-                    .CheckIfInnerText(
+                AssertUI.InnerText(browser.First("p.summary")
+                    ,
                         s =>
                             s.Contains("DotVVM.Framework.Compilation.DotvvmCompilationException") &&
                             s.Contains("requires a DataContext of type")
@@ -253,38 +238,36 @@ namespace DotVVM.Samples.Tests
         }
 
 
-        [TestMethod]
+        [Fact]
         [SampleReference(nameof(SamplesRouteUrls.Errors_FieldInValueBinding))]
         public void Error_ExceptionWindow_LabelClickableTest()
         {
-            RunInAllBrowsers(browser =>
-            {
+            RunInAllBrowsers(browser => {
                 browser.NavigateToUrl(SamplesRouteUrls.Errors_FieldInValueBinding);
 
                 //click Exception
                 browser.First("label[for=menu_radio_stack_trace]").Click();
-                browser.First("#container_stack_trace").CheckIfIsDisplayed();
+                AssertUI.IsDisplayed(browser.First("#container_stack_trace"));
 
                 //click Cookies
                 browser.First("label[for=menu_radio_cookies]").Click();
-                browser.First("#container_cookies").CheckIfIsDisplayed();
+                AssertUI.IsDisplayed(browser.First("#container_cookies"));
 
                 //click Request Headers
                 browser.First("label[for=menu_radio_reqHeaders]").Click();
-                browser.First("#container_reqHeaders").CheckIfIsDisplayed();
+                AssertUI.IsDisplayed(browser.First("#container_reqHeaders"));
 
                 //click DotVVM Markup
                 browser.First("label[for=menu_radio_dothtml]").Click();
-                browser.First("#container_dothtml").CheckIfIsDisplayed();
+                AssertUI.IsDisplayed(browser.First("#container_dothtml"));
             });
         }
 
-        [TestMethod]
+        [Fact]
         [SampleReference(nameof(SamplesRouteUrls.Errors_FieldInValueBinding))]
         public void Error_ExceptionWindow_DotNetReferenceSourceRedirect()
         {
-            RunInAllBrowsers(browser =>
-            {
+            RunInAllBrowsers(browser => {
                 browser.NavigateToUrl(SamplesRouteUrls.Errors_FieldInValueBinding);
                 browser.First("label[for=menu_radio_stack_trace]").Click();
 
@@ -308,12 +291,11 @@ namespace DotVVM.Samples.Tests
             });
         }
 
-        [TestMethod, Ignore]
+        [Fact, Ignore]
         [SampleReference(nameof(SamplesRouteUrls.Errors_FieldInValueBinding))]
         public void Error_ExceptionWindow_GitHubRedirect()
         {
-            RunInAllBrowsers(browser =>
-            {
+            RunInAllBrowsers(browser => {
                 browser.NavigateToUrl(SamplesRouteUrls.Errors_FieldInValueBinding);
                 //open exception tab
                 browser.First("label[for=menu_radio_exception]").Click();
@@ -334,5 +316,8 @@ namespace DotVVM.Samples.Tests
             });
         }
 
+        public ErrorsTests(ITestOutputHelper output) : base(output)
+        {
+        }
     }
 }
---
+++ src/DotVVM.Samples.Tests.New/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+ public static class Extensions
+    {
+        public static bool Contains(this string text, string value, StringComparison comparison)
+        {
+
+            return text.IndexOf(value, comparison) > -1;
+        }
+    }
+}
