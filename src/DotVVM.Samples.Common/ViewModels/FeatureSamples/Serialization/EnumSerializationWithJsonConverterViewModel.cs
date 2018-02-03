dd8ca35005ad2dd9da78206fc2a8a2a7e97f96f5
Tomáš Herceg <tomas.herceg@riganti.cz>
2018-02-03 21:12:21 +0100

    Failing test case with JsonConverer attribute on enum property added

---
+++ src/DotVVM.Samples.Common/ViewModels/FeatureSamples/Serialization/EnumSerializationWithJsonConverterViewModel.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DotVVM.Framework.ViewModel;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace DotVVM.Samples.Common.ViewModels.FeatureSamples.Serialization
+{
+    public class EnumSerializationWithJsonConverterViewModel : DotvvmViewModelBase
+    {
+
+        // NSwag generates this attribute. It should be ignored by DotVVM.
+        [JsonConverter(typeof(StringEnumConverter))]
+        public EnumWithJsonConverter EnumValue { get; set; } = EnumWithJsonConverter.One;
+
+    }
+
+    public enum EnumWithJsonConverter
+    {
+        One,
+        Two,
+        Three
+    }
+}
+
