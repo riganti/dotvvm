adb0a19734dd670db5f883d36e646ff5020f0d37
Ladislav Šesták <ladislav.sestak@riganti.cz>
2018-02-05 20:21:08 +0100

    Copy Artifacts script - added nupkg

--- src/Tools/CopyArtifacts.ps1
+++ src/Tools/CopyArtifacts.ps1
@@ -1,8 +1,9 @@
 ﻿param([String]$searchFolder, [String]$artifactsFolder)
 
 
-$files  = Get-ChildItem   "*.pdb"  -Recurse | where { !$_.Directory.FullName -notlike "\obj\" }
+$files  = Get-ChildItem   "*.pdb"  -Recurse | where { !$_.Directory.FullName -notlike "\obj\" } | where { $_.Name -like "*dotvvm*" }   |   where { $_.Directory.FullName -notlike "\packages\" }   |  where { $_.Name -notlike "*Riganti.selenium*" }
 $files2  = Get-ChildItem   "*.dll"  -Recurse | where { $_.Directory.FullName -notlike "\obj\" }  | where { $_.Name -like "*dotvvm*" }   |   where { $_.Directory.FullName -notlike "\packages\" }   |  where { $_.Name -notlike "*Riganti.selenium*" }
+$files3  = Get-ChildItem   "*.nupkg"  -Recurse | where { $_.Directory.FullName -notlike "\obj\" }  | where { $_.Name -like "*dotvvm*" }   |   where { $_.Directory.FullName -notlike "\packages\" }   |  where { $_.Name -notlike "*Riganti.selenium*" }
 
 
 foreach($file in $files){
@@ -12,6 +13,11 @@ foreach($file in $files){
 }
 
 foreach($file in $files2){
+
+ Copy-Item $file -Destination $artifactsFolder
+
+}
+foreach($file in $files3){
 
  Copy-Item $file -Destination $artifactsFolder
 
