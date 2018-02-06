d57521ca9d321777e4525f004d2b472feaddf1a0
Ladislav Šesták <ladislav.sestak@riganti.cz>
2018-02-06 01:11:06 +0100

    Added test: InnerText set at span in <dot:content>

---
+++ src/DotVVM.Samples.Common/ViewModels/ControlSamples/ContentPlaceHolder/DoubleContentPlaceHolderMasterPageViewModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DotVVM.Framework.ViewModel;
+
+namespace DotVVM.Samples.Common.ViewModels.ControlSamples.ContentPlaceHolder
+{
+    public class DoubleContentPlaceHolderMasterPageViewModel : DotvvmViewModelBase
+    {
+        
+    }
+}
+
--- src/DotVVM.Testing.Abstractions/SamplesRouteUrls.designer.cs
+++ src/DotVVM.Testing.Abstractions/SamplesRouteUrls.designer.cs
@@ -49,6 +49,7 @@ namespace DotVVM.Testing.Abstractions
         public static string ControlSamples_ComboBox_ComboBoxDelaySync3 => "ControlSamples/ComboBox/ComboBoxDelaySync3";
         public static string ControlSamples_ContentPlaceHolder_ContentPlaceHolderPage => "ControlSamples/ContentPlaceHolder/ContentPlaceHolderPage";
         public static string ControlSamples_ContentPlaceHolder_ContentPlaceHolderPage_ContentTest => "ControlSamples/ContentPlaceHolder/ContentPlaceHolderPage_ContentTest";
+        public static string ControlSamples_ContentPlaceHolder_DoubleContentPlaceHolderPage_ContentTest => "ControlSamples/ContentPlaceHolder/DoubleContentPlaceHolderPage_ContentTest";
         public static string ControlSamples_DataPager_DataPager => "ControlSamples/DataPager/DataPager";
         public static string ControlSamples_EnabledProperty_EnabledProperty => "ControlSamples/EnabledProperty/EnabledProperty";
         public static string ControlSamples_EnvironmentView_EnvironmentViewTest => "ControlSamples/EnvironmentView/EnvironmentViewTest";
