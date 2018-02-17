eb1ecc61b3dea03a3713a6f67632aa841449b17f
Adam Jež <adamjez@outlook.cz>
2018-02-17 11:00:48 +0100

    Ported Api.SwashbuckleOwin tests

--- src/DotVVM.Framework.Api.Swashbuckle.Owin.Tests/SwaggerFileGenerationTests.cs
+++ src/DotVVM.Framework.Api.Swashbuckle.Owin.Tests/SwaggerFileGenerationTests.cs
@@ -6,6 +6,7 @@ using System.Web.Http.Description;
 using DotVVM.Core.Common;
 using DotVVM.Framework.Api.Swashbuckle.Owin.Filters;
 using DotVVM.Framework.ViewModel;
+using DotVVM.Samples.BasicSamples.Api.Common.Model;
 using DotVVM.Samples.BasicSamples.Api.Owin.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
@@ -35,8 +36,12 @@ namespace DotVVM.Framework.Api.Swashbuckle.Owin.Tests
             var versions = new Dictionary<string, Info> {
                 { "v1", new Info() { version = "v1", title = "Test API" } }
             };
-            var options = new SwaggerGeneratorOptions(operationFilters: new IOperationFilter[]
-            {
+
+            var apiOptions = new DotvvmApiOptions() {
+                KnownTypes = { typeof(Company<string>) }
+            };
+
+            var options = new SwaggerGeneratorOptions(operationFilters: new IOperationFilter[] {
                 // default filters used by Swashbuckle
                 new HandleFromUriParams(),
                 new ApplySwaggerOperationAttributes(),
@@ -45,23 +50,59 @@ namespace DotVVM.Framework.Api.Swashbuckle.Owin.Tests
 
                 // our custom filters
                 new AddAsObjectAnnotationOperationFilter(new DefaultPropertySerialization()),
-            }, schemaFilters: new[] { new HandleDotvvmNameSchemaFilter(new DotvvmApiOptions(), new DefaultPropertySerialization()) });
+            }, schemaFilters: new[] { new HandleDotvvmNameSchemaFilter(apiOptions, new DefaultPropertySerialization())
+            }, documentFilters: new[] { new HandleKnownTypesDocumentFilter(apiOptions) });
+
             var generator = new SwaggerGenerator(apiExplorer, settings, versions, options);
             document = generator.GetSwagger("http://localhost:61453/", "v1");
         }
 
         [TestMethod]
-        public void Swashbuckle_Owin_ReturnsDataSetAnnotation_NotAdded()
+        public void Swashbuckle_Owin_NameAndKnownTypeAnnotation_Added()
         {
-            var operation = document.paths["/api/companies"].get;
-            Assert.IsFalse(operation.vendorExtensions.ContainsKey("x-returns-knownType"));
+            var definition = document.definitions["IPagingOptions"];
+
+            Assert.AreEqual("DotVVM.Framework.Controls.IPagingOptions", definition.vendorExtensions[ApiConstants.DotvvmKnownTypeKey]);
+            Assert.AreEqual("PageIndex", definition.properties["PageIndex"].vendorExtensions[ApiConstants.DotvvmNameKey]);
+            Assert.AreEqual("PageSize", definition.properties["PageSize"].vendorExtensions[ApiConstants.DotvvmNameKey]);
+        }
+
+
+        [TestMethod]
+        public void Swashbuckle_Owin_GenericNameAndKnownTypeAnnotation_Added()
+        {
+            var definition = document.definitions["GridViewDataSet[Company[String]]"];
+
+            Assert.AreEqual("DotVVM.Framework.Controls.GridViewDataSet<Company[String]>",
+                definition.vendorExtensions[ApiConstants.DotvvmKnownTypeKey]);
+
+            Assert.AreEqual("IsRefreshRequired", definition.properties["IsRefreshRequired"].vendorExtensions[ApiConstants.DotvvmNameKey]);
+            Assert.AreEqual("Items", definition.properties["Items"].vendorExtensions[ApiConstants.DotvvmNameKey]);
         }
 
         [TestMethod]
-        public void Swashbuckle_Owin_ReturnsDataSetAnnotation_Added()
+        public void Swashbuckle_Owin_GenericKnownTypeAnnotation_FullNameParameter()
         {
-            var operation = document.paths["/api/companies/sorted"].get;
-            Assert.AreEqual("true", operation.vendorExtensions["x-returns-knownType"].ToString());
+            var definition = document.definitions["Company[String]"];
+
+            Assert.AreEqual("DotVVM.Samples.BasicSamples.Api.Common.Model.Company<System.String>",
+                definition.vendorExtensions[ApiConstants.DotvvmKnownTypeKey]);
+        }
+
+        [TestMethod]
+        public void Swashbuckle_Owin_MixedGenericKnownTypeAnnotation_Exist()
+        {
+            var definition = document.definitions["GridViewDataSet[Company[Boolean]]"];
+
+            Assert.AreEqual("DotVVM.Framework.Controls.GridViewDataSet<Company[Boolean]>", definition.vendorExtensions[ApiConstants.DotvvmKnownTypeKey]);
+        }
+
+        [TestMethod]
+        public void Swashbuckle_Owin_KnownTypeAnnotation_NotAdded()
+        {
+            var definition = document.definitions["Order"];
+
+            Assert.IsFalse(definition.vendorExtensions.ContainsKey(ApiConstants.DotvvmKnownTypeKey));
         }
 
         [TestMethod]
@@ -69,7 +110,7 @@ namespace DotVVM.Framework.Api.Swashbuckle.Owin.Tests
         {
             var operation1 = document.paths["/api/companies/sorted"].get;
             var param1 = operation1.parameters.Single(p => p.name == "sortingOptions.SortDescending");
-            Assert.AreEqual("DotVVM.Framework.Controls.ISortingOptions, DotVVM.Core", param1.vendorExtensions["x-dotvvm-wrapperType"]);
+            Assert.AreEqual("DotVVM.Framework.Controls.ISortingOptions, DotVVM.Core", param1.vendorExtensions[ApiConstants.DotvvmWrapperTypeKey]);
         }
 
         [TestMethod]
@@ -78,8 +119,8 @@ namespace DotVVM.Framework.Api.Swashbuckle.Owin.Tests
             var operation = document.paths["/api/companies/sortedandpaged"].get;
             var param1 = operation.parameters.Single(p => p.name == "sortingOptions.SortDescending");
             var param2 = operation.parameters.Single(p => p.name == "pagingOptions.PageSize");
-            Assert.AreEqual("DotVVM.Framework.Controls.ISortingOptions, DotVVM.Core", param1.vendorExtensions["x-dotvvm-wrapperType"]);
-            Assert.AreEqual("DotVVM.Framework.Controls.IPagingOptions, DotVVM.Core", param2.vendorExtensions["x-dotvvm-wrapperType"]);
+            Assert.AreEqual("DotVVM.Framework.Controls.ISortingOptions, DotVVM.Core", param1.vendorExtensions[ApiConstants.DotvvmWrapperTypeKey]);
+            Assert.AreEqual("DotVVM.Framework.Controls.IPagingOptions, DotVVM.Core", param2.vendorExtensions[ApiConstants.DotvvmWrapperTypeKey]);
         }
 
         [TestMethod]
@@ -87,7 +128,7 @@ namespace DotVVM.Framework.Api.Swashbuckle.Owin.Tests
         {
             var operation = document.paths["/api/orders/{orderId}"].get;
             var param = operation.parameters.Single(p => p.name == "orderId");
-            Assert.IsFalse(param.vendorExtensions.ContainsKey("x-dotvvm-wrapperType"));
+            Assert.IsFalse(param.vendorExtensions.ContainsKey(ApiConstants.DotvvmWrapperTypeKey));
         }
     }
 }
