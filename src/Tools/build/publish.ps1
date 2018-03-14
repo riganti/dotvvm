abe75f6e5d45b66a5591d5ccce90d079b173cfc4
Ladislav Šesták <ladislav.sestak@riganti.cz>
2018-03-14 21:27:24 +0100

    Publish script - publish only symbols version of nupkg

--- src/Tools/build/publish.ps1
+++ src/Tools/build/publish.ps1
@@ -68,16 +68,15 @@ function BuildPackages() {
 			& dotnet restore --source $nugetRestoreAltSource --source https://nuget.org/api/v2/ | Out-Host
 		}
 		
-		& dotnet pack -c $configuration | Out-Host
-		& dotnet pack -c Debug --include-symbols | Out-Host
+		& dotnet pack -c $configuration --include-symbols | Out-Host
 		cd ..
 	}
 }
 
 function PushPackages() {
 	foreach ($package in $packages) {
-		& .\Tools\nuget.exe push .\$($package.Directory)\bin\$configuration\$($package.Package).$version.nupkg -source $server -apiKey $apiKey | Out-Host
-		& .\Tools\nuget.exe push .\$($package.Directory)\bin\Debug\$($package.Package).$version.symbols.nupkg -source $internalServer -apiKey $apiKeyInternal | Out-Host
+		& .\Tools\nuget.exe push .\$($package.Directory)\bin\$configuration\$($package.Package).$version.symbols.nupkg -source $server -apiKey $apiKey | Out-Host
+		#& .\Tools\nuget.exe push .\$($package.Directory)\bin\$configuration\$($package.Package).$version.symbols.nupkg -source $internalServer -apiKey $apiKeyInternal | Out-Host
 	}
 }
 
