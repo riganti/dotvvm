1940bd68cd898dd31f945fcd9169e169349632d1
Mrnda <michal.mrnustik@outlook.com>
2018-04-14 13:57:57 +0200

    Add support for string contains

--- src/DotVVM.TypeScript.Compiler/Compiler.cs
+++ src/DotVVM.TypeScript.Compiler/Compiler.cs
@@ -77,6 +77,7 @@ namespace DotVVM.TypeScript.Compiler
             _methodTranslatorRegistry.RegisterTranslator(typeof(List<>).GetMethod("Remove"), new ListRemoveMethodTranslator(_factory));
             _methodTranslatorRegistry.RegisterTranslator(typeof(List<>).GetMethod("Add"), new ListAddMethodTranslator(_factory));
             _methodTranslatorRegistry.RegisterTranslator(typeof(List<>).GetMethod("Clear"), new ListClearMethodTranslator(_factory));
+            _methodTranslatorRegistry.RegisterTranslator(typeof(string).GetMethod("Contains"), new StringContainsMethodTranslator(_factory));
         }
 
         public void RegisterTranslators(CompilerContext compilerContext)
---
+++ src/DotVVM.TypeScript.Compiler/Translators/Builtin/StringContainsMethodTranslator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DotVVM.TypeScript.Compiler.Ast;
+using DotVVM.TypeScript.Compiler.Ast.Factories;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace DotVVM.TypeScript.Compiler.Translators.Builtin
+{
+    class StringContainsMethodTranslator : IMethodCallTranslator
+    {
+        private readonly ISyntaxFactory _factory;
+
+        public StringContainsMethodTranslator(ISyntaxFactory factory)
+        {
+            _factory = factory;
+        }
+
+        public ISyntaxNode Translate(IInvocationOperation operation, List<IExpressionSyntax> arguments, IReferenceSyntax reference, ISyntaxNode parent)
+        {
+            var format = $"({reference.ToDisplayString()}.indexOf({arguments.Single()}) !== -1)";
+            return _factory.CreateRawSyntaxNode(format, parent);
+        }
+    }
+}
