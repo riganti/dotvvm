f9da94234fdf5f5ea2c992510c6ab06f126418e8
Mrnda <michal.mrnustik@outlook.com>
2018-05-11 18:46:36 +0200

    Move TsObjectCreationExpressionSyntax to its proper folder

--- src/DotVVM.TypeScript.Compiler/Ast/IObjectCreationExpressionSyntax.cs
+++ src/DotVVM.TypeScript.Compiler/Ast/IObjectCreationExpressionSyntax.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using DotVVM.TypeScript.Compiler.Ast.TypeScript;
-using DotVVM.TypeScript.Compiler.Ast.Visitors;
 
 namespace DotVVM.TypeScript.Compiler.Ast
 {
@@ -10,30 +8,5 @@ namespace DotVVM.TypeScript.Compiler.Ast
     {
         ITypeSyntax ObjectType { get; }
         IList<IExpressionSyntax> Arguments { get; }
-    }
-
-    class TsObjectCreationExpressionSyntax : TsExpressionSyntax, IObjectCreationExpressionSyntax
-    {
-        public ITypeSyntax ObjectType { get; }
-
-        public IList<IExpressionSyntax> Arguments { get; }
-
-        public TsObjectCreationExpressionSyntax(ISyntaxNode parent, IList<IExpressionSyntax> argumentsSyntax, ITypeSyntax objectType) : base(parent)
-        {
-            Arguments = argumentsSyntax;
-            ObjectType = objectType;
-        }
-
-        public override void AcceptVisitor(INodeVisitor visitor)
-        {
-            visitor.VisitObjectCreationExpresion(this);
-        }
-
-        public override IEnumerable<ISyntaxNode> DescendantNodes()
-        {
-            throw new NotImplementedException();
-        }
-
-
     }
 }
---
+++ src/DotVVM.TypeScript.Compiler/Ast/TypeScript/TsObjectCreationExpressionSyntax.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DotVVM.TypeScript.Compiler.Ast.Visitors;
+
+namespace DotVVM.TypeScript.Compiler.Ast.TypeScript
+{
+    class TsObjectCreationExpressionSyntax : TsExpressionSyntax, IObjectCreationExpressionSyntax
+    {
+        public ITypeSyntax ObjectType { get; }
+
+        public IList<IExpressionSyntax> Arguments { get; }
+
+        public TsObjectCreationExpressionSyntax(ISyntaxNode parent, IList<IExpressionSyntax> argumentsSyntax, ITypeSyntax objectType) : base(parent)
+        {
+            Arguments = argumentsSyntax;
+            ObjectType = objectType;
+        }
+
+        public override void AcceptVisitor(INodeVisitor visitor)
+        {
+            visitor.VisitObjectCreationExpresion(this);
+        }
+
+        public override IEnumerable<ISyntaxNode> DescendantNodes()
+        {
+            return Enumerable.Empty<ISyntaxNode>();
+        }
+
+
+    }
+}
