fe91e751554a335d7673834386d61e2b65033faf
Mrnda <michal.mrnustik@outlook.com>
2018-05-11 18:47:01 +0200

    Remove unused translator interface

--- src/DotVVM.TypeScript.Compiler/Translators/IOperationTranslator.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.CodeAnalysis;
-
-namespace DotVVM.TypeScript.Compiler.Translators
-{
-    public interface IOperationTranslator<in TInput> : ITranslator<TInput> where TInput : IOperation
-    {
-    }
-}
