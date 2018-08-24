709172bfb097e5771950a4f4d3a9b912c104037a
Adam Jež <adamjez@outlook.cz>
2018-08-24 16:14:21 +0200

    Removed unnecessary code

--- DotVVM.Diagnostics.StatusPage.Sample/Startup.cs
+++ DotVVM.Diagnostics.StatusPage.Sample/Startup.cs
@@ -1,20 +1,10 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.Logging;
-using DotVVM.Framework.Hosting;
 
 namespace DotVVM.Diagnostics.StatusPage.Sample
 {
     public class Startup
     {
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDataProtection();
@@ -23,21 +13,10 @@ namespace DotVVM.Diagnostics.StatusPage.Sample
             services.AddDotVVM<DotvvmStartup>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app)
         {
-            loggerFactory.AddConsole();
-
-            // use DotVVM
-            var dotvvmConfiguration = app.UseDotVVM<DotvvmStartup>(env.ContentRootPath);
-            
-            // use static files
-            app.UseStaticFiles(new StaticFileOptions
-            {
-                FileProvider = new PhysicalFileProvider(env.WebRootPath)
-            });
+            app.UseDotVVM<DotvvmStartup>();
+            app.UseStaticFiles();
         }
-
-        public const string AuthenticationScheme = "Cookie";
     }
 }
