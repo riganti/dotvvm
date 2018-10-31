b39b685fd6fb57a5f08ff6365880266781193d2a
exyi <exyi@outlook.cz>
2018-10-31 08:03:09 +0000

    Removed mess in <reference ...> paths

--- src/DotVVM.Framework.Tests.Javascript/tests/DotVVM.Events.Tests.d.ts
+++ src/DotVVM.Framework.Tests.Javascript/tests/DotVVM.Events.Tests.d.ts
@@ -1,4 +1,4 @@
-/// <reference path="../scripts/typings/jasmine/jasmine.d.ts" />
+/// <reference path="../Scripts/typings/jasmine/jasmine.d.ts" />
 /// <reference path="../../DotVVM.Framework/Resources/Scripts/typings/knockout/knockout.d.ts" />
 /// <reference path="../../DotVVM.Framework/Resources/Scripts/DotVVM.d.ts" />
 declare var dotvvm: DotVVM;
