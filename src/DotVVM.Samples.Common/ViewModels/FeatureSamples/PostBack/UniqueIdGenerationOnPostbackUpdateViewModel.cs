162aad4d52fdc84633a1707d6c2e0eab67c46823
Ladislav Šesták <sestakladislav.jr@gmail.com>
2019-02-15 02:35:04 +0100

    Added UniqueIdGenerationOnPostbackUpdate sample

---
+++ src/DotVVM.Samples.Common/ViewModels/FeatureSamples/PostBack/UniqueIdGenerationOnPostbackUpdateViewModel.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotVVM.Framework.ViewModel;
+
+namespace DotVVM.Samples.Common.ViewModels.FeatureSamples.PostBack
+{
+    public class UniqueIdGenerationOnPostbackUpdateViewModel : DotvvmViewModelBase
+    {
+        public override Task PreRender()
+        {
+            Data = new List<TestDataItem>() {
+                new TestDataItem() {
+                    Data = new List<TestDataItem>() {
+                        new TestDataItem(),
+                        new TestDataItem(),
+                        new TestDataItem(),
+                        new TestDataItem(),
+                        new TestDataItem(),
+                    }
+                },
+                new TestDataItem() {
+                    Data = new List<TestDataItem>() {
+                        new TestDataItem(){
+                            Data = new List<TestDataItem>() {
+                                new TestDataItem(),
+                                new TestDataItem(),
+                                new TestDataItem(),
+                                new TestDataItem(),
+                                new TestDataItem(),
+                                new TestDataItem(),
+                                new TestDataItem(),
+                            },
+                        },
+                        new TestDataItem(){
+                            Data = new List<TestDataItem>() {
+                                new TestDataItem(){
+                                    Data = new List<TestDataItem>() {
+                                        new TestDataItem(){
+                                            Data = new List<TestDataItem>() {
+                                                new TestDataItem(){
+                                                    Data = new List<TestDataItem>() {
+                                                        new TestDataItem(){
+                                                            Data = new List<TestDataItem>() {
+                                                                new TestDataItem(),
+                                                                new TestDataItem(),
+                                                                new TestDataItem(),
+                                                                new TestDataItem(),
+                                                                new TestDataItem(),
+                                                                new TestDataItem(),
+                                                            },
+                                                        },
+                                                        new TestDataItem(),
+                                                        new TestDataItem(),
+                                                        new TestDataItem(),
+                                                        new TestDataItem(),
+                                                        new TestDataItem(),
+                                                    },
+                                                },
+                                                new TestDataItem(),
+                                                new TestDataItem(),
+                                                new TestDataItem(),
+                                                new TestDataItem(),
+                                                new TestDataItem(),
+                                            },
+                                        },
+                                        new TestDataItem(),
+                                        new TestDataItem(),
+                                        new TestDataItem(),
+                                        new TestDataItem(),
+                                        new TestDataItem(),
+                                    },
+                                },
+                                new TestDataItem(),
+                                new TestDataItem(),
+                                new TestDataItem(),
+                                new TestDataItem(),
+                                new TestDataItem(),
+                            },
+                        },new TestDataItem(){
+                            Data = new List<TestDataItem>() {
+                                new TestDataItem(),
+                                new TestDataItem(),
+                                new TestDataItem(),
+                                new TestDataItem(),
+                                new TestDataItem(),
+                                new TestDataItem(),
+                                new TestDataItem(),
+                                new TestDataItem(),
+                            },
+                        },new TestDataItem(){
+                            Data = new List<TestDataItem>() {
+                                new TestDataItem(),
+                                new TestDataItem(),
+                                new TestDataItem(),
+                                new TestDataItem(),
+                                new TestDataItem(),
+                                new TestDataItem(),
+                                new TestDataItem(),
+                            },
+                        },
+                    }
+                }
+            };
+
+            return base.PreRender();
+        }
+
+        public List<TestDataItem> Data { get; set; }
+    }
+
+    public class TestDataItem
+    {
+        public DateTime Now { get; set; } = DateTime.Now;
+        public List<TestDataItem> Data { get; set; }
+        public string Text { get; set; }
+
+        public void Command()
+        {
+            Text = "Command executed";
+        }
+    }
+}
+
