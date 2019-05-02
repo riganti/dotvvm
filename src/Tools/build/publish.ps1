89180f835925da46b76d898c0e972e428117d591
Ladislav Šesták <ladislav.sestak@riganti.cz>
2019-05-02 19:47:52 +0200

    Added SeleniumHelpers to publish script

--- src/Tools/build/publish.ps1
+++ src/Tools/build/publish.ps1
@@ -118,6 +118,7 @@ $packages = @(
 	[pscustomobject]@{ Package = "DotVVM.Api.Swashbuckle.AspNetCore"; Directory = "DotVVM.Framework.Api.Swashbuckle.AspNetCore" },
 	[pscustomobject]@{ Package = "DotVVM.Api.Swashbuckle.Owin"; Directory = "DotVVM.Framework.Api.Swashbuckle.Owin" }
 	[pscustomobject]@{ Package = "DotVVM.Framework.Testing.SeleniumGenerator"; Directory = "DotVVM.Framework.Testing.SeleniumGenerator" }
+	[pscustomobject]@{ Package = "DotVVM.Framework.Testing.SeleniumHelpers"; Directory = "DotVVM.Framework.Testing.SeleniumHelpers" }
 )
 
 function PublishTemplates() {
