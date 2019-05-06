bdb95c69041245001d3dcf419216cbbd2d065bcf
exyi <exyi@outlook.cz>
2019-05-06 21:16:16 +0000

    Added few more test cases for UrlHelper.IsLocal method

--- src/DotVVM.Framework.Tests.Common/Routing/UrlHelperTests.cs
+++ src/DotVVM.Framework.Tests.Common/Routing/UrlHelperTests.cs
@@ -16,7 +16,14 @@ namespace DotVVM.Framework.Tests.Common.Routing
         [DataRow("//local", false)]
         [DataRow("~//local", false)]
         [DataRow("https://www.google.com", false)]
+        [DataRow("/https://www.google.com", true)]
         [DataRow("/\r\n/google.com", false)]
+        [DataRow("/\n/google.com", false)]
+        [DataRow("mailto:a", false)]
+        [DataRow("/mailto:a", true)]
+        [DataRow(@"\\www.google.com", false)] // Chrome replaces backslashes with forward slashes...
+        [DataRow(@"\/www.google.com", false)]
+        [DataRow(@"/\www.google.com", false)]
         public void UrlHelper_IsLocalUrl(string url, bool exepectedResult)
         {
             var result = UrlHelper.IsLocalUrl(url);
