440b51236160093695fbdcbb299590e3a8d69fad
Tomáš Herceg <herceg@vbnet.cz>
2019-06-16 18:35:34 +0200

    Service registration issue in tests fixed

--- src/DotVVM.Framework.Tests.Owin/DefaultViewModelSerializerTests.cs
+++ src/DotVVM.Framework.Tests.Owin/DefaultViewModelSerializerTests.cs
@@ -18,6 +18,8 @@ using Microsoft.Owin;
 using Microsoft.Owin.Security.DataProtection;
 using Newtonsoft.Json.Linq;
 using Microsoft.Owin.Infrastructure;
+using DotVVM.Framework.Runtime.Caching;
+using DotVVM.Framework.Hosting.Owin.Runtime.Caching;
 
 namespace DotVVM.Framework.Tests.Runtime
 {
@@ -37,6 +39,7 @@ namespace DotVVM.Framework.Tests.Runtime
                 services.AddTransient<IViewModelProtector, DefaultViewModelProtector>();
                 services.AddTransient<ICsrfProtector, DefaultCsrfProtector>();
                 services.AddSingleton<ICookieManager, ChunkingCookieManager>();
+                services.AddSingleton<IDotvvmCacheAdapter, OwinDotvvmCacheAdapter>();
             });
 			configuration.Security.SigningKey = Convert.FromBase64String("Uiq1FXs016lC6QaWIREB7H2P/sn4WrxkvFkqaIKpB27E7RPuMipsORgSgnT+zJmUu8zXNSJ4BdL73JEMRDiF6A1ScRNwGyDxDAVL3nkpNlGrSoLNM1xHnVzSbocLFDrdEiZD2e3uKujguycvWSNxYzjgMjXNsaqvCtMu/qRaEGc=");
 			configuration.Security.EncryptionKey = Convert.FromBase64String("jNS9I3ZcxzsUSPYJSwzCOm/DEyKFNlBmDGo9wQ6nxKg=");
