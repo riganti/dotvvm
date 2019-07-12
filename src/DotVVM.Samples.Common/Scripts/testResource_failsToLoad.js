f28a0a5917726b9f56e1a522a851158e6dabf961
Adam Štěpánek <adam.stepanek@riganti.cz>
2019-07-12 13:20:39 +0200

    Add a LocationFallback sample

---
+++ src/DotVVM.Samples.Common/Scripts/testResource_failsToLoad.js
@@ -0,0 +1,1 @@
+﻿window.isTestResourceLoaded = false;
---
+++ src/DotVVM.Samples.Common/Scripts/testResource_locationFallback.js
@@ -0,0 +1,7 @@
+﻿window.isTestResourceLoaded = true;
+
+let proof = document.createElement("h1");
+proof.innerText = "The \"testResource_locationFallback\" script has been loaded.";
+
+document.getElementsByTagName("body")[0]
+    .appendChild(proof);
