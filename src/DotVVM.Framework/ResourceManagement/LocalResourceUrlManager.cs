7c0fb93e4da542e1d8f826f00971196e6cc3351a
Adam Štěpánek <adam.stepanek@riganti.cz>
2019-07-24 12:16:05 +0200

    Add support for LocationFallback for ILinkResources

--- src/DotVVM.Framework/ResourceManagement/LocalResourceUrlManager.cs
+++ src/DotVVM.Framework/ResourceManagement/LocalResourceUrlManager.cs
@@ -2,6 +2,7 @@
 using DotVVM.Framework.Hosting;
 using DotVVM.Framework.Hosting.Middlewares;
 using DotVVM.Framework.Routing;
+using DotVVM.Framework.Utils;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
@@ -13,6 +14,10 @@ namespace DotVVM.Framework.ResourceManagement
 {
     public class LocalResourceUrlManager : ILocalResourceUrlManager
     {
+        private const string HashParameterName = "hash";
+        private const string NameParameterName = "name";
+        private const string LocationQueryName = "location";
+
         private readonly IResourceHashService hasher;
         private readonly RouteBase resourceRoute;
         private readonly DotvvmResourceRepository resources;
@@ -21,18 +26,40 @@ namespace DotVVM.Framework.ResourceManagement
 
         public LocalResourceUrlManager(DotvvmConfiguration configuration, IResourceHashService hasher)
         {
-            this.resourceRoute = new DotvvmRoute("dotvvmResource/{hash}/{name:regex(.*)}", null, null, null, configuration);
+            this.resourceRoute = new DotvvmRoute(
+                url: $"dotvvmResource/{{{HashParameterName}}}/{{{NameParameterName}:regex(.*)}}",
+                virtualPath: null,
+                defaultValues: null,
+                presenterFactory: null,
+                configuration: configuration);
             this.hasher = hasher;
             this.resources = configuration.Resources;
             this.alternateDirectories = configuration.Debug ? new ConcurrentDictionary<string, string>() : null;
             this.suppressVersionHash = configuration.Debug;
         }
 
-        public string GetResourceUrl(ILocalResourceLocation resource, IDotvvmRequestContext context, string name) =>
-            resourceRoute.BuildUrl(new Dictionary<string, object> {
-                ["hash"] = GetVersionHash(resource, context, name),
-                ["name"] = EncodeResourceName(name)
+        public string GetResourceUrl(ILocalResourceLocation resource,
+            IDotvvmRequestContext context,
+            string name)
+        {
+            var locationOwner = resources.FindResource(name);
+            var url = resourceRoute.BuildUrl(new Dictionary<string, object> {
+                [HashParameterName] = GetVersionHash(resource, context, name),
+                [NameParameterName] = EncodeResourceName(name)
             });
+            if (locationOwner is ILinkResource link)
+            {
+                var locations = link.GetLocations().ToArray();
+                var locationIndex = Array.IndexOf(locations, resource);
+                if (locationIndex != 0)
+                {
+                    url += UrlHelper.BuildUrlSuffix(null, new Dictionary<string, object> {
+                        [LocationQueryName] = locationIndex
+                    });
+                }
+            }
+            return url;
+        }
 
         protected virtual string EncodeResourceName(string name)
         {
@@ -52,16 +79,28 @@ namespace DotVVM.Framework.ResourceManagement
         public ILocalResourceLocation FindResource(string url, IDotvvmRequestContext context, out string mimeType)
         {
             mimeType = null;
-            if (DotvvmRoutingMiddleware.FindMatchingRoute(new[] { resourceRoute }, context, out var parameters) == null) return null;
-            var name = DecodeResourceName((string)parameters["name"]);
-            var hash = (string)parameters["hash"];
+            if (DotvvmRoutingMiddleware.FindMatchingRoute(new[] { resourceRoute }, context, out var parameters) == null)
+            {
+                return null;
+            }
+
+            var name = DecodeResourceName((string)parameters[NameParameterName]);
+            var hash = (string)parameters[HashParameterName];
+            int? locationIndex = null;
+            if (context.Query.TryGetValue(LocationQueryName, out var locationString))
+            {
+                locationIndex = (int)ReflectionUtils.ConvertValue(locationString, typeof(int));
+            }
             if (resources.FindResource(name) is IResource resource)
             {
-                var location = FindLocation(resource, out mimeType);
+                var location = FindLocation(resource, locationIndex, out mimeType);
                 if (GetVersionHash(location, context, name) == hash) // check if the resource matches so that nobody can gues the url by chance
                 {
                     if (alternateDirectories != null)
+                    {
                         alternateDirectories.GetOrAdd(hash, _ => (location as IDebugFileLocalLocation)?.GetFilePath(context));
+                    }
+
                     return location;
                 }
             }
@@ -77,20 +116,36 @@ namespace DotVVM.Framework.ResourceManagement
                 if (directory != null)
                 {
                     var sourceFile = Path.Combine(directory, name);
-                    if (File.Exists(sourceFile)) return new FileResourceLocation(sourceFile);
+                    if (File.Exists(sourceFile))
+                    {
+                        return new FileResourceLocation(sourceFile);
+                    }
                 }
             }
             return null;
         }
 
-        protected ILocalResourceLocation FindLocation(IResource resource, out string mimeType)
+        protected ILocalResourceLocation FindLocation(
+            IResource resource,
+            int? locationIndex,
+            out string mimeType)
         {
-            var linkResource = resource as ILinkResource;
-            mimeType = linkResource?.MimeType;
-            return linkResource
-                   ?.GetLocations()
-                   ?.OfType<ILocalResourceLocation>()
-                   ?.FirstOrDefault();
+            if (!(resource is ILinkResource link))
+            {
+                mimeType = null;
+                return null;
+            }
+
+            mimeType = link.MimeType;
+            var locations = link.GetLocations().ToArray();
+            if (locationIndex == null)
+            {
+                return locations
+                    .OfType<ILocalResourceLocation>()
+                    .FirstOrDefault();
+            }
+            // unless the order of locations changes, the location has to be local
+            return (ILocalResourceLocation)locations.ElementAtOrDefault(locationIndex.Value);
         }
     }
 }
