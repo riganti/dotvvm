3427cee3354281693bc727f75f3b0edd81cf2a2e
Adam Štěpánek <adam.stepanek@riganti.cz>
2019-08-06 10:03:19 +0200

    Add a sample for #716

---
+++ src/DotVVM.Samples.Common/Views/ComplexSamples/ButtonInMarkupControl/ButtonWrapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotVVM.Framework.Binding;
+using DotVVM.Framework.Binding.Expressions;
+using DotVVM.Framework.Controls;
+
+namespace DotVVM.Samples.BasicSamples.Views.ComplexSamples.ButtonInMarkupControl
+{
+    public class ButtonWrapper : DotvvmMarkupControl
+    {
+        public bool Enabled
+        {
+            get { return (bool)GetValue(EnabledProperty); }
+            set { SetValue(EnabledProperty, value); }
+        }
+        public static readonly DotvvmProperty EnabledProperty
+            = DotvvmProperty.Register<bool, ButtonWrapper>(c => c.Enabled, true);
+
+        public Command ClickCommand
+        {
+            get { return (Command)GetValue(ClickCommandProperty); }
+            set { SetValue(ClickCommandProperty, value); }
+        }
+        public static readonly DotvvmProperty ClickCommandProperty
+            = DotvvmProperty.Register<Command, ButtonWrapper>(c => c.ClickCommand, null);
+    }
+}
