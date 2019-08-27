e8c5b3fb2188053cd017fb41ed578df5e68898ca
Tomáš Herceg <herceg@vbnet.cz>
2019-08-27 21:19:41 +0200

    Server-side viewmodel caching options extracted to a separate class

--- src/DotVVM.Framework/ViewModel/Serialization/InMemoryViewModelServerStore.cs
+++ src/DotVVM.Framework/ViewModel/Serialization/InMemoryViewModelServerStore.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Threading.Tasks;
 using DotVVM.Framework.Runtime.Caching;
+using Microsoft.Extensions.Options;
 
 namespace DotVVM.Framework.ViewModel.Serialization
 {
     public class InMemoryViewModelServerStore : IViewModelServerStore
     {
         private readonly IDotvvmCacheAdapter cacheAdapter;
+        private readonly IOptions<ViewModelServerCacheConfiguration> cacheConfigurationOptions;
 
-        public TimeSpan CacheLifetime { get; set; } = TimeSpan.FromMinutes(5);
-
-        public InMemoryViewModelServerStore(IDotvvmCacheAdapter cacheAdapter)
+        public InMemoryViewModelServerStore(IDotvvmCacheAdapter cacheAdapter, IOptions<ViewModelServerCacheConfiguration> cacheConfigurationOptions)
         {
             this.cacheAdapter = cacheAdapter;
+            this.cacheConfigurationOptions = cacheConfigurationOptions;
         }
 
         public byte[] Retrieve(string hash)
@@ -22,7 +23,7 @@ namespace DotVVM.Framework.ViewModel.Serialization
 
         public void Store(string hash, byte[] cacheData)
         {
-            cacheAdapter.GetOrAdd(BuildKey(hash), k => new DotvvmCachedItem<byte[]>(cacheData, slidingExpiration: CacheLifetime));
+            cacheAdapter.GetOrAdd(BuildKey(hash), k => new DotvvmCachedItem<byte[]>(cacheData, DotvvmCacheItemPriority.Low, slidingExpiration: cacheConfigurationOptions.Value.CacheLifetime));
         }
 
         private static string BuildKey(string hash)
---
+++ src/DotVVM.Framework/ViewModel/Serialization/ViewModelServerCacheConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotVVM.Framework.ViewModel.Serialization
+{
+    public class ViewModelServerCacheConfiguration
+    {
+
+        public TimeSpan CacheLifetime { get; set; } = TimeSpan.FromMinutes(5);
+
+    }
+}
