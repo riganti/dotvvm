87a5ded35203c959b77ffc416566318f172da6e9
Ladislav Šesták <ladislav.sestak@riganti.cz>
2019-09-17 19:08:40 +0200

    Changed addresses in RedirectService samples

--- src/DotVVM.Samples.Common/ViewModels/FeatureSamples/Redirect/RedirectService.cs
+++ src/DotVVM.Samples.Common/ViewModels/FeatureSamples/Redirect/RedirectService.cs
@@ -16,21 +16,21 @@ namespace DotVVM.Samples.BasicSamples.ViewModels.FeatureSamples.Redirect
         [AllowStaticCommand]
         public void RedirectTest()
         {
-            Context.RedirectToUrl("~/FeatureSamples/Redirect/Redirect?time=" + DateTime.Now.Ticks);
+            Context.RedirectToUrl("~/FeatureSamples/Redirect/Redirect_StaticCommand?time=" + DateTime.Now.Ticks);
 
             throw new Exception("This exception should not occur because Redirect interrupts the request execution!");
         }
         [AllowStaticCommand]
         public void RedirectObjectQueryString()
         {
-            Context.RedirectToRoute("FeatureSamples_Redirect_Redirect", urlSuffix: "?param=temp1#test1", query: new { time = DateTime.Now.Ticks });
+            Context.RedirectToRoute("FeatureSamples_Redirect_Redirect_StaticCommand", urlSuffix: "?param=temp1#test1", query: new { time = DateTime.Now.Ticks });
 
             throw new Exception("This exception should not occur because Redirect interrupts the request execution!");
         }
         [AllowStaticCommand]
         public void RedirectDictionaryQueryString()
         {
-            Context.RedirectToRoute("FeatureSamples_Redirect_Redirect", urlSuffix: "#test2", query: new Dictionary<string, string> { { "time", DateTime.Now.Ticks.ToString() }, { "param", "temp2" } });
+            Context.RedirectToRoute("FeatureSamples_Redirect_Redirect_StaticCommand", urlSuffix: "#test2", query: new Dictionary<string, string> { { "time", DateTime.Now.Ticks.ToString() }, { "param", "temp2" } });
 
             throw new Exception("This exception should not occur because Redirect interrupts the request execution!");
         }
