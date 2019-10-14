2700760efecf261ba6e26d68f5ff90e8db90fbf6
Michal Tichý <michal.tichy@riganti.cz>
2019-10-14 22:22:31 +0200

    modified LocalizationPresenter test not to use shared DotVVM config

--- src/DotVVM.Framework.Tests.Common/Routing/LocalizablePresenterTests.cs
+++ src/DotVVM.Framework.Tests.Common/Routing/LocalizablePresenterTests.cs
@@ -15,23 +15,25 @@ namespace DotVVM.Framework.Tests.Routing
     [TestClass]
     public class LocalizablePresenterTests
     {
-        DotvvmConfiguration configuration = DotvvmTestHelper.DefaultConfig;
 
         [TestMethod]
         public void LocalizablePresenter_RedirectsOnInvalidLanguageCode()
         {
-            var config = DotvvmTestHelper.DefaultConfig;
+            var config = DotvvmConfiguration.CreateDefault();
             var presenterFactory = LocalizablePresenter.BasedOnParameter("Lang");
 
             config.RouteTable.Add("Test", "test/Lang", "test", new { Lang = "en" }, presenterFactory);
-            var localizablePresenter = presenterFactory(configuration.ServiceProvider);
-            var context = DotvvmTestHelper.CreateContext(configuration);
+
+            var context = DotvvmTestHelper.CreateContext(config);
             context.Parameters["Lang"] = "cz";
             context.Route = config.RouteTable.First();
+
             var httpRequest = new TestHttpContext();
             httpRequest.Request = new TestHttpRequest(httpRequest) { PathBase = "" };
             httpRequest.Request.Headers.Add(HostingConstants.SpaContentPlaceHolderHeaderName, new string[0]);
+
             context.HttpContext = httpRequest;
+            var localizablePresenter = presenterFactory(config.ServiceProvider);
 
             Assert.ThrowsException<DotvvmInterruptRequestExecutionException>(() =>
                 localizablePresenter.ProcessRequest(context));
