34e6c744c99d988ddf59efda72c29e82bee3aef3
Adam Štěpánek <tengateman@gmail.com>
2019-10-27 14:51:57 +0100

    Add DotVVM.Samples.SeleniumGenerator.Owin

---
+++ src/DotVVM.Samples.SeleniumGenerator.Owin/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Web.Hosting;
+using Microsoft.Owin;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using Owin;
+using DotVVM.Framework;
+using DotVVM.Framework.Configuration;
+using DotVVM.Framework.Routing;
+using DotVVM.Framework.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: OwinStartup(typeof(DotVVM.Samples.SeleniumGenerator.Owin.Startup))]
+namespace DotVVM.Samples.SeleniumGenerator.Owin
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var applicationPhysicalPath = HostingEnvironment.ApplicationPhysicalPath;
+
+            ConfigureAuth(app);
+
+
+            var dotvvmConfiguration = app.UseDotVVM<DotvvmStartup>(applicationPhysicalPath, debug: IsInDebugMode());
+
+            dotvvmConfiguration.AssertConfigurationIsValid();
+            // use static files
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileSystem = new PhysicalFileSystem(applicationPhysicalPath)
+            });
+        }
+        public void ConfigureAuth(IAppBuilder app)
+        {
+        }
+
+		private bool IsInDebugMode()
+        {
+#if !DEBUG
+			return false;
+#endif
+            return true;
+        }
+    }
+}
