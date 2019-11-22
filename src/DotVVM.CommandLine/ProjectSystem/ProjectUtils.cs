1d393dcd86304b1b700d5b2ed011d115592a7652
Adam Štěpánek <tengateman@gmail.com>
2019-11-22 19:17:38 +0100

    Implement ProjectUitls.ResolveMetadata

--- src/DotVVM.CommandLine/ProjectSystem/ProjectUtils.cs
+++ src/DotVVM.CommandLine/ProjectSystem/ProjectUtils.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using DotVVM.Utils.ProjectService;
 using DotVVM.Utils.ProjectService.Lookup;
 using Microsoft.Build.Evaluation;
@@ -12,52 +13,146 @@ namespace DotVVM.CommandLine.ProjectSystem
 {
     public static class ProjectUtils
     {
-        public const string AssemblyNameProperty = "AssemblyName";
-        public const string CollectPackageReferencesTarget = "CollectPackageReferences";
-        public const string ProjectReferenceItemType = "ProjectReference";
-        public const string ReferenceItemType = "Reference";
-
         public static IResolvedProjectMetadata ResolveMetadata(string projectDirectory)
         {
+            // load the project with MSBuild
             var csproj = new DirectoryInfo(projectDirectory).GetFiles("*.csproj").Single();
-            var project = new Project(csproj.FullName);
+            var project = new Project(csproj.FullName);var names = Enum.GetNames(typeof(TargetFramework));
             var projectInstance = BuildManager.DefaultBuildManager.GetProjectInstanceForBuild(project);
 
-            var dependencies = new List<ProjectDependency>();
-            dependencies.AddRange(projectInstance.GetItems(ProjectReferenceItemType)
-                .Select(r => new ProjectDependency {
-                    IsProjectReference = true,
-                    ProjectPath = r.EvaluatedInclude,
-                    Version = r.GetMetadata("Version")?.EvaluatedValue
-                }));
-            dependencies.AddRange(projectInstance.GetItems(ReferenceItemType)
-                .Select(r => new ProjectDependency {
-                    Name = r.EvaluatedInclude,
-                    Version = r.GetMetadata("Version")?.EvaluatedValue
-                }));
-            var packagesRequest = new BuildRequestData(projectInstance, new[] { CollectPackageReferencesTarget });
-            var packagesResult = BuildManager.DefaultBuildManager.Build(null, packagesRequest);
-            if (packagesResult.OverallResult == BuildResultCode.Success)
-            {
-                dependencies.AddRange(packagesResult.ResultsByTarget[CollectPackageReferencesTarget].Items
-                    .Select(i => new ProjectDependency {
-                        Name = i.ItemSpec,
-                        Version = i.GetMetadata("Version")
-                    }));
-            }
-            var assemblyName = projectInstance.GetProperty(AssemblyNameProperty)?.EvaluatedValue;
+            var csprojVersion = GetCsprojVersion(projectInstance);
+            var targetFramework = GetTargetFramework(projectInstance, csprojVersion);
+
+            var assemblyName = projectInstance.GetPropertyValue("AssemblyName");
+            bool shouldRunDotvvmCompiler = ShouldRunDotvvmCompiler(projectInstance, csprojVersion);
+            var assemblyPath = ProjectOutputAssemblyProvider
+                .GetAssemblyPath(csproj, assemblyName, targetFramework);
+            var dotvvmDependencies = GetDotvvmDependencies(projectInstance);
+            var packageDirectory = GetNuGetPackageDirectory(projectInstance, csprojVersion);
+            var dotvvmPackage = dotvvmDependencies.SingleOrDefault(d =>
+                d.Name.Equals("DotVVM", StringComparison.OrdinalIgnoreCase) && !d.IsProjectReference);
+            var dotvvmPackageDirectory = dotvvmPackage != null
+                 ? Path.Combine(packageDirectory, dotvvmPackage.Name, dotvvmPackage.Version)
+                 : "";
+
             return new ResolvedProjectMetadata {
-                AssemblyName = assemblyName,
-                AssemblyPath = null, // TODO
+                CsprojVersion = csprojVersion,
+                TargetFramework = targetFramework,
                 CsprojFullName = csproj.FullName,
-                CsprojVersion = default, // TODO: Maybe use UsingMicrosoftNETSdk or NETCoreSdkVersion
-                DotvvmPackageNugetFolders = null, // TODO
-                DotvvmProjectDependencies = dependencies,
-                PackageNugetFolders = null, // TODO
-                ProjectRootDirectory = projectDirectory, // TODO
-                RunDotvvmCompiler = false, // TODO
-                TargetFramework = default // TODO
+                AssemblyName = assemblyName,
+                RunDotvvmCompiler = shouldRunDotvvmCompiler,
+                AssemblyPath = assemblyPath,
+                ProjectRootDirectory = projectDirectory,
+                DotvvmProjectDependencies = dotvvmDependencies,
+                PackageNugetFolders = new List<string> { packageDirectory },
+                DotvvmPackageNugetFolders = new List<string> { dotvvmPackageDirectory }
             };
+        }
+
+        private static CsprojVersion GetCsprojVersion(ProjectInstance project)
+        {
+            return string.IsNullOrEmpty(project.GetPropertyValue("UsingMicrosoftNETSdk"))
+                ? CsprojVersion.OlderProjectSystem
+                : CsprojVersion.DotNetSdk;
+        }
+
+        private static TargetFramework GetTargetFramework(ProjectInstance project, CsprojVersion csprojVersion)
+        {
+            string targetName = "GetTargetFrameworks";
+            var targetFrameworks = BuildManager.DefaultBuildManager.Build(null,
+                new BuildRequestData(project, new[] { targetName }))[targetName].Items.Single()
+                .GetMetadata("TargetFrameworks");
+            if (string.IsNullOrEmpty(targetFrameworks))
+                return csprojVersion == CsprojVersion.DotNetSdk
+                    ? TargetFramework.NetStandard
+                    : TargetFramework.NetFramework;
+            var names = Enum.GetNames(typeof(TargetFramework));
+            return targetFrameworks.Split(';')
+                .Select(s => s.Replace(".", "").Trim())
+                .Select(s => names.FirstOrDefault(b => b.Equals(s, StringComparison.OrdinalIgnoreCase)))
+                .Where(n => n is object)
+                .Select(n => Enum.TryParse<TargetFramework>(n, out var t) ? t : TargetFramework.Unknown)
+                .Aggregate((l, r) => l | r);
+        }
+
+        private static bool ShouldRunDotvvmCompiler(ProjectInstance project, CsprojVersion csprojVersion)
+        {
+            switch (csprojVersion)
+            {
+                case CsprojVersion.DotNetSdk:
+                    // TODO: When is .net core project compatible?
+                    return false;
+                case CsprojVersion.OlderProjectSystem:
+                    return project.GetPropertyValue("ProjectTypeGuids")
+                        .Split(';')
+                        .Contains(Constants.DotvvmProjectGuid);
+                default:
+                    return false;
+            }
+        }
+
+        private static List<ProjectDependency> GetDotvvmDependencies(ProjectInstance project)
+        {
+            bool IsDotvvmReference(ProjectItemInstance item)
+            {
+                return item.EvaluatedInclude
+                    .IndexOf("DotVVM", StringComparison.OrdinalIgnoreCase) != -1;
+            }
+
+            var dotvvmDependencies = new List<ProjectDependency>();
+            var packages = project.GetItems("PackageReference")
+                .Where(p => IsDotvvmReference(p))
+                .Select(p => new ProjectDependency {
+                    Name = p.GetMetadataValue("Include"),
+                    Version = p.GetMetadataValue("Version")
+                });
+            dotvvmDependencies.AddRange(dotvvmDependencies);
+            var projects = project.GetItems("ProjectReference")
+                .Where(p => IsDotvvmReference(p))
+                .Select(p => new ProjectDependency {
+                    IsProjectReference = true,
+                    ProjectPath = p.EvaluatedInclude,
+                    Name = GetDotvvmProjectReferenceName(p.GetMetadataValue("Name"))
+                });
+            dotvvmDependencies.AddRange(projects);
+            var references = project.GetItems("Reference")
+                .Where(p => IsDotvvmReference(p))
+                .Select(p => {
+                    var assemblyName = new AssemblyName(p.EvaluatedInclude);
+                    return new ProjectDependency {
+                        Name = assemblyName.Name,
+                        Version = assemblyName.Version.ToString()
+                    };
+                });
+            dotvvmDependencies.AddRange(references);
+            return dotvvmDependencies;
+        }
+
+        private static string GetDotvvmProjectReferenceName(string name)
+        {
+            switch (name) {
+                case "DotVVM.Framework":
+                    return "DotVVM";
+                case "DotVVM.Framework.Hosting.Owin":
+                    return "DotVVM.Owin";
+                case "DotVVM.Framework.Hosting.AspNetCore":
+                    return "DotVVM.AspNetCore";
+                default:
+                    return name;
+            }
+        }
+
+        private static string GetNuGetPackageDirectory(ProjectInstance project, CsprojVersion csprojVersion)
+        {
+            switch(csprojVersion)
+            {
+                case CsprojVersion.OlderProjectSystem:
+                    return Path.Combine(project.Directory, "..\\packages");
+                case CsprojVersion.DotNetSdk:
+                    return project.GetPropertyValue("NuGetPackageRoot");
+                default:
+                    return string.Empty;
+            }
         }
     }
 }
