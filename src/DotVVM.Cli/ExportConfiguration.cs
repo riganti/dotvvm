840da609483cc8a49c6512a4539c658ad9823a94
Adam Štěpánek <adam.stepanek@riganti.cz>
2019-11-30 22:39:25 +0100

    Add CLI stub

---
+++ src/DotVVM.Cli/ExportConfiguration.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DotVVM.Cli
+{
+    public static class ExportConfiguration
+    {
+        public static void Invoke(string target)
+        {
+            Console.WriteLine(target);
+        }
+    }
+}
