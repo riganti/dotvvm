11de945e38650ac4dd0c7d2a92a2971186611b1a
exyi <exyi@outlook.cz>
2020-01-24 22:02:40 +0000

    JS: little cleanup of SSR foreach

--- src/DotVVM.Framework/Resources/Scripts/binding-handlers/SSR-foreach.ts
+++ src/DotVVM.Framework/Resources/Scripts/binding-handlers/SSR-foreach.ts
@@ -1,5 +1,6 @@
 import makeUpdatableChildrenContextHandler from './makeUpdatableChildrenContext'
-import foreachCollectionSymbol from './foreachCollectionSymbol'
+
+const foreachCollectionSymbol = "$foreachCollectionSymbol"
 
 ko.virtualElements.allowedBindings["dotvvm-SSR-foreach"] = true;
 ko.virtualElements.allowedBindings["dotvvm-SSR-item"] = true;
@@ -8,9 +9,32 @@ type SeenUpdateElement = HTMLElement & { seenUpdate?: number };
 
 export default {
     "dotvvm-SSR-foreach": {
-        init: makeUpdatableChildrenContextHandler( // TODO: this is only usage - inline it here
-            (bindingContext, rawValue) => bindingContext.extend({ [foreachCollectionSymbol]: rawValue.data }),
-            v => v.data != null)
+        init(element: Node, valueAccessor: () => any, allBindings?: KnockoutAllBindingsAccessor, viewModel?: any, bindingContext?: KnockoutBindingContext) {
+            if (!bindingContext) {
+                throw new Error();
+            }
+
+            let savedNodes: Node[] | undefined;
+            ko.computed(() => {
+                const rawValue = valueAccessor().data;
+
+                // save a copy of the inner nodes on the initial update, but only if we have dependencies.
+                if (!savedNodes && ko.computedContext.getDependenciesCount()) {
+                    savedNodes = ko.utils.cloneNodes(ko.virtualElements.childNodes(element), true /* shouldCleanNodes */);
+                }
+
+                if (rawValue != null) {
+                    if (savedNodes) {
+                        ko.virtualElements.setDomNodeChildren(element, ko.utils.cloneNodes(savedNodes));
+                    }
+                    ko.applyBindingsToDescendants(bindingContext.extend({ [foreachCollectionSymbol]: rawValue }), element);
+                } else {
+                    ko.virtualElements.emptyNode(element);
+                }
+
+            }, null, { disposeWhenNodeIsRemoved: element });
+            return { controlsDescendantBindings: true } // do not apply binding again
+        }
     },
     "dotvvm-SSR-item": {
         init<T>(element: SeenUpdateElement, valueAccessor: () => T, allBindings?: any, viewModel?: any, bindingContext?: KnockoutBindingContext) {
--- src/DotVVM.Framework/Resources/Scripts/binding-handlers/foreachCollectionSymbol.ts
+++
@@ -1,1 +0,0 @@
-export default "$foreachCollectionSymbol" // TODO: where should this be?
