72c3f6606dbedd2c44e91746e350ac2997991399
Michal Tichý <michal.tichy@riganti.cz>
2020-03-14 12:13:57 +0100

    Added doc comments to IRenderAdapter

--- src/DotVVM.Framework/Controls/IRenderAdapter.cs
+++ src/DotVVM.Framework/Controls/IRenderAdapter.cs
@@ -6,9 +6,24 @@ namespace DotVVM.Framework.Controls
 {
     public interface IRenderAdapter
     {
+        /// <summary>
+        /// Alternative implementation for AddAttributesToRender
+        /// </summary>
         Action<IDotvvmControl, IHtmlWriter, IDotvvmRequestContext>? AddAttributesToRender { get; }
+
+        /// <summary>
+        /// Alternative implementation for RenderBeginTag
+        /// </summary>
         Action<IDotvvmControl, IHtmlWriter, IDotvvmRequestContext>? RenderBeginTag { get; }
+
+        /// <summary>
+        /// Alternative implementation for RenderContents
+        /// </summary>
         Action<IDotvvmControl, IHtmlWriter, IDotvvmRequestContext>? RenderContents { get; }
+
+        /// <summary>
+        /// Alternative implementation for RenderEndTag
+        /// </summary>
         Action<IDotvvmControl, IHtmlWriter, IDotvvmRequestContext>? RenderEndTag { get; }
     }
 }
