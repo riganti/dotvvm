b339404e1af0caa15fc5c0a3b9b1200903bbee0b
Tomáš Herceg <tomas.herceg@riganti.cz>
2020-03-22 17:12:02 +0100

    Added repro for postback queue race condition

---
+++ src/DotVVM.Samples.Common/ViewModels/FeatureSamples/PostbackConcurrency/StressTestViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using DotVVM.Framework.ViewModel;
+
+namespace DotVVM.Samples.Common.ViewModels.FeatureSamples.PostbackConcurrency
+{
+    public class StressTestViewModel : DotvvmViewModelBase
+    {
+
+        public int Value { get; set; }
+
+        [Bind(Direction.ServerToClientFirstRequest)]
+        public int RejectedCount { get; set; }
+
+        [Bind(Direction.ServerToClientFirstRequest)]
+        public int BeforePostback { get; set; }
+
+        [Bind(Direction.ServerToClientFirstRequest)]
+        public int AfterPostback { get; set; }
+
+        public void Increment()
+        {
+            Value++;
+            Thread.Sleep(0);
+        }
+
+    }
+}
