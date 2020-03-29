28ceca4bdb58bb4d981e0c47060b0e7f3e5771a4
Tomáš Herceg <tomas.herceg@riganti.cz>
2020-03-29 14:15:32 +0200

    Samples for redirect and race condition issues added

---
+++ src/DotVVM.Samples.Common/ViewModels/FeatureSamples/PostbackConcurrency/RedirectPostbackQueueViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotVVM.Samples.Common.ViewModels.FeatureSamples.PostbackConcurrency
+{
+    public class RedirectPostbackQueueViewModel : RedirectPostbackQueueMasterViewModel
+    {
+
+        public int Value { get; set; }
+
+        public override async Task Init()
+        {
+            if (Context.Query.ContainsKey("time"))
+            {
+                await Task.Delay(5000);
+            }
+
+            await base.Init();
+        }
+
+        public void Increment()
+        {
+            Value++;
+        }
+
+        public void IncrementWithWait()
+        {
+            Thread.Sleep(5000);
+            Value++;
+        }
+
+        public void Redirect()
+        {
+            Thread.Sleep(2000);
+            Context.RedirectToRoute(Context.Route.RouteName, query: new { time = Environment.TickCount });
+        }
+
+    }
+}
