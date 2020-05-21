8c3060620de0552819a1c0eb76e1f81395671af1
Andrej Čižmárik <andrej.cizmarik@riganti.cz>
2020-05-21 11:21:23 +0200

    Added control sample for ValidationSummary's HideWhenValid property

---
+++ src/DotVVM.Samples.Common/ViewModels/ControlSamples/ValidationSummary/HideWhenValidViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DotVVM.Samples.Common.ViewModels.ControlSamples.ValidationSummary
+{
+    public class HideWhenValidViewModel
+    {
+        [Required]
+        public string Text { get; set; }
+
+        public void Validate()
+        {
+
+        }
+    }
+}
