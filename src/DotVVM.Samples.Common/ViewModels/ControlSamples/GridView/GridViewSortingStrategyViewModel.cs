564fda578571874e2f72a294226fb9751ceb7a14
Andrej Čižmárik <andrej.cizmarik@riganti.cz>
2020-05-26 13:12:53 +0200

    Added sample for custom sorting strategy

---
+++ src/DotVVM.Samples.Common/ViewModels/ControlSamples/GridView/GridViewSortingStrategyViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotVVM.Framework.Controls;
+using DotVVM.Framework.ViewModel;
+
+namespace DotVVM.Samples.Common.ViewModels.ControlSamples.GridView
+{
+    public class GridViewSortingStrategyViewModel : DotvvmViewModelBase
+    {
+        private static IQueryable<CountryData> GetData()
+        {
+            return new[]
+            {
+                new CountryData() { Name = "Czech Republic", Population = 10_708_981, Area = 77_240 },
+                new CountryData() { Name = "Slovakia", Population = 5_459_642, Area = 48_088 },
+                new CountryData() { Name = "Germany", Population = 83_783_942, Area = 348_560 },
+                new CountryData() { Name = "Austria", Population = 9_006_398, Area = 82_409 },
+                new CountryData() { Name = "Poland", Population = 37_846_611, Area = 306_230 },
+            }.AsQueryable();
+        }
+
+        public GridViewDataSet<CountryData> CountriesDataSet { get; set; } = new GridViewDataSet<CountryData>()
+        {
+            SortingOptions = new SortingOptions()
+            {
+                SortingStrategy = new SortingStrategy()
+                {
+                    Strategy = (dataSet, expr) =>
+                    {
+                        var options = dataSet.SortingOptions;
+
+                        if (expr == "Name")
+                        {
+                            options.SortExpression = expr;
+                            options.SortDescending = false;
+                        }
+                        else if (options.SortExpression == expr)
+                        {
+                            options.SortDescending ^= true;
+                        }
+                        else
+                        {
+                            options.SortExpression = expr;
+                            options.SortDescending = false;
+                        }
+
+                        (dataSet as IPageableGridViewDataSet)?.GoToFirstPage();
+                    }
+                }
+            }
+        };
+
+        public override Task PreRender()
+        {
+            CountriesDataSet.LoadFromQueryable(GetData());
+
+            return base.PreRender();
+        }
+    }
+
+    public class CountryData
+    {
+        public string Name { get; set; }
+        public int Population { get; set; }
+        public int Area { get; set; }
+    }
+}
