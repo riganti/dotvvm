70f30633e0de8889277989394a8ce9c7a808c34a
exyi <exyi@outlook.cz>
2020-06-29 13:47:07 +0000

    Serialization: tuple with nested viewmodel test

--- src/DotVVM.Framework.Tests.Common/ViewModel/SerializerTests.cs
+++ src/DotVVM.Framework.Tests.Common/ViewModel/SerializerTests.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using DotVVM.Framework.ViewModel;
 
 namespace DotVVM.Framework.Tests.Common.ViewModel
 {
@@ -53,7 +54,16 @@ namespace DotVVM.Framework.Tests.Common.ViewModel
                 P3 = {
                     new KeyValuePair<int, int>(3, 4),
                     new KeyValuePair<int, int>(5, 6)
-                }
+                },
+                P4 = (
+                    6,
+                    new TestViewModelWithBind {
+                        P1 = "X",
+                        P2 = "Y",
+                        ServerToClient = "Z",
+                        ClientToServer = "Z"
+                    }
+                )
             };
             var obj2 = new TestViewModelWithTuples();
             Populate(obj2, Serialize(obj));
@@ -61,6 +71,11 @@ namespace DotVVM.Framework.Tests.Common.ViewModel
             Assert.AreEqual(obj.P1, obj2.P1);
             Assert.AreEqual(obj.P2, obj2.P2);
             Assert.IsTrue(obj.P3.SequenceEqual(obj2.P3));
+            Assert.AreEqual(obj.P4.a, obj2.P4.a);
+            Assert.AreEqual(obj.P4.b.P1, obj2.P4.b.P1);
+            Assert.AreEqual(obj.P4.b.P2, obj2.P4.b.P2);
+            Assert.AreEqual("default", obj2.P4.b.ServerToClient);
+            Assert.AreEqual("default", obj2.P4.b.ClientToServer);
         }
     }
 
@@ -69,5 +84,18 @@ namespace DotVVM.Framework.Tests.Common.ViewModel
         public Tuple<int, int, int, int> P1 { get; set; }
         public (int a, int b, int c, int d) P2 { get; set; } = (1, 2, 3, 4);
         public List<KeyValuePair<int, int>> P3 { get; set; } = new List<KeyValuePair<int, int>>();
+        public (int a, TestViewModelWithBind b) P4 { get; set; } = (1, new TestViewModelWithBind());
+    }
+
+    public class TestViewModelWithBind
+    {
+        [Bind(Name = "property ONE")]
+        public string P1 { get; set; } = "value 1";
+        [JsonProperty("property TWO")]
+        public string P2 { get; set; } = "value 2";
+        [Bind(Direction.ClientToServer)]
+        public string ClientToServer { get; set; } = "default";
+        [Bind(Direction.ServerToClient)]
+        public string ServerToClient { get; set; } = "default";
     }
 }
