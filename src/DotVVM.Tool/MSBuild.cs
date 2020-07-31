3ec466b05c27a4deb4f0dd452ce9c623ac034ee2
Adam Štěpánek <adam.stepanek@riganti.cz>
2020-07-31 16:49:28 +0200

    Find MSBuild somewhat properly

--- src/DotVVM.Tool/MSBuild.cs
+++ src/DotVVM.Tool/MSBuild.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Immutable;
+using System.Diagnostics;
 using System.IO;
 using Microsoft.VisualStudio.Setup.Configuration;
 
@@ -8,43 +11,66 @@ namespace DotVVM.Tool
         public const string DefaultTargetFramework = "netcoreapp3.1";
         public const string VSRelativePath = "./MSBuild/Current/Bin/MSBuild.exe";
 
-        public string TargetFramework { get; } = DefaultTargetFramework;
         public string Path { get; } = string.Empty;
+        public ImmutableArray<string> PrefixedArgs { get; } = ImmutableArray.Create<string>();
 
-        public static string FindSDKPath()
+        public MSBuild(string path, ImmutableArray<string> prefixedArgs)
         {
-            return new MSBuild();
+            Path = path;
+            PrefixedArgs = prefixedArgs;
         }
 
-        public static string? FindVSPath()
+        public static MSBuild? CreateFromSdk()
         {
-            var query = new SetupConfiguration();
-            var query2 = (ISetupConfiguration2)query;
-            var @enum = query2.EnumAllInstances();
-            var instances = new ISetupInstance[1];
-            int fetchedCount;
-            do
+            return new MSBuild(
+                path: "dotnet",
+                prefixedArgs: ImmutableArray.Create("msbuild"));
+        }
+
+        public static MSBuild? CreateFromVS()
+        {
+            try
             {
-                @enum.Next(1, instances, out fetchedCount);
-                if (fetchedCount > 0)
+                var query = new SetupConfiguration();
+                var query2 = (ISetupConfiguration2)query;
+                var @enum = query2.EnumAllInstances();
+                var instances = new ISetupInstance[1];
+                int fetchedCount;
+                do
                 {
-                    var instance2 = (ISetupInstance2)instances[0];
-                    var path = instance2.GetInstallationPath();
-                    var exePath = System.IO.Path.Combine(path, VSRelativePath);
-                    if (File.Exists(exePath))
+                    @enum.Next(1, instances, out fetchedCount);
+                    if (fetchedCount > 0)
                     {
-                        return exePath;
+                        var instance2 = (ISetupInstance2)instances[0];
+                        var path = instance2.GetInstallationPath();
+                        var exePath = System.IO.Path.Combine(path, VSRelativePath);
+                        if (File.Exists(exePath))
+                        {
+                            return new MSBuild(exePath, ImmutableArray.Create<string>());
+                        }
                     }
                 }
+                while (fetchedCount > 0);
+                return null;
             }
-            while (fetchedCount > 0);
+            catch(PlatformNotSupportedException)
+            {
+                return null;
+            }
+        }
+
+        public static MSBuild? Create(FileInfo project)
+        {
+            var vs = CreateFromVS();
+            Console.WriteLine($"VS: {vs}");
+            var sdk = CreateFromSdk();
+            Console.WriteLine($"SDK: {sdk}");
             return null;
         }
 
-        public static MSBuild Create(FileInfo project)
+        public override string ToString()
         {
-            var vs = CreateFromVS();
-            return new MSBuild();
+            return $"({Path} {string.Join(' ', PrefixedArgs)})";
         }
     }
 }
