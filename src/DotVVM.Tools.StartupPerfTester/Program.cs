ed32ed55df05394e01a610a1a8fb125747fd0cba
Adam Štěpánek <adam.stepanek@riganti.cz>
2020-08-14 09:04:41 +0200

    Add the 'dotvvm-startup-perf' command name

--- src/DotVVM.Tools.StartupPerfTester/Program.cs
+++ src/DotVVM.Tools.StartupPerfTester/Program.cs
@@ -2,16 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.CommandLine;
 using System.CommandLine.Invocation;
-using System.Diagnostics;
-using System.Net.Http;
-using System.Net.Sockets;
-using System.Threading;
-using Process = System.Diagnostics.Process;
 
 namespace DotVVM.Tools.StartupPerfTester
 {
@@ -19,7 +11,7 @@ namespace DotVVM.Tools.StartupPerfTester
     {
         static void Main(string[] args)
         {
-            var rootCommand = new RootCommand
+            var rootCommand = new RootCommand("Measures the performance of DotVVM startup")
             {
                 new Argument<FileInfo>(
                     "project",
@@ -27,17 +19,17 @@ namespace DotVVM.Tools.StartupPerfTester
                 ) { Arity = ArgumentArity.ExactlyOne },
                 new Option<TestTarget>(
                     new [] { "-t", "--type" },
-                    "Type of the project - use 'owin' or 'aspnetcore'"
+                    "Type of the project"
                 ) { IsRequired = true },
                 new Option<int>(
                     new [] { "-r", "--repeat" },
                     () => 1,
-                    "How many times the operation should be repeated."
+                    "How many times the operation should be repeated"
                 ),
                 new Option<string>(
                     new [] { "-u", "--url" },
                     () => "",
-                    "Relative URL in the app that should be tested."
+                    "Relative URL in the app that should be tested"
                 ),
                 new Option<bool>(
                     new [] { "-v", "--verbose" },
@@ -47,9 +39,10 @@ namespace DotVVM.Tools.StartupPerfTester
                 new Option<int>(
                     new[] { "--timeout" },
                     () => 10,
-                    "Timeout for the HTTP interface to start listening."
+                    "Timeout for the HTTP interface to start listening"
                 )
-            }; 
+            };
+            rootCommand.Name = "dotvvm-startup-perf";
             rootCommand.Handler = CommandHandler.Create<FileInfo, TestTarget, int, string, bool, int>((project, type, repeat, url, verbose, timeout) =>
             {
                 new StartupPerformanceTest(project, type, repeat, url, verbose, timeout).HandleCommand();
