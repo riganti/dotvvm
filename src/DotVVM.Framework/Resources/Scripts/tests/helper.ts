6b34b9d984c49ce377d23729130453bda24ec468
exyi <exyi@outlook.cz>
2020-08-28 21:10:25 +0000

    add postback tests

--- src/DotVVM.Framework/Resources/Scripts/tests/helper.ts
+++ src/DotVVM.Framework/Resources/Scripts/tests/helper.ts
@@ -1,3 +1,6 @@
+import dotvvm from '../dotvvm-root'
+import { keys } from '../utils/objects'
+
 export function initDotvvm(viewModel: any, culture: string = "en-US") {
     const input = window.document.createElement("input")
     input.value = JSON.stringify(viewModel)
@@ -6,4 +9,23 @@ export function initDotvvm(viewModel: any, culture: string = "en-US") {
 
     dotvvm.init(culture)
 
+}
+
+export function watchEvents() {
+    const handlers: any = {}
+    for (const event of keys(dotvvm.events)) {
+        if ("subscribe" in (dotvvm.events as any)[event]) {
+            function h(args: any) {
+                console.debug("Event " + event, args)
+            }
+            (dotvvm.events as any)[event].subscribe(h)
+            handlers[event] = h
+        }
+    }
+
+    return () => {
+        for (const event in keys(handlers)) {
+            (dotvvm.events as any)[event].unsubscribe(handlers[event])
+        }
+    }
 }
