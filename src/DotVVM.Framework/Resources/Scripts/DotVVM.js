e81ccf22bedecd7dedbe00b267016ffe8ebe647c
Riganti Team City <rigantiteamcity>
2020-09-24 19:16:56 +0200

    NuGet package version 2.5.0-preview05-90660-event

--- src/DotVVM.Framework/Resources/Scripts/DotVVM.js
+++ src/DotVVM.Framework/Resources/Scripts/DotVVM.js
@@ -23,11 +23,10 @@ var __assign = (this && this.__assign) || function () {
     return __assign.apply(this, arguments);
 };
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
@@ -220,22 +219,22 @@ var DotvvmAfterPostBackEventArgs = /** @class */ (function () {
     }
     Object.defineProperty(DotvvmAfterPostBackEventArgs.prototype, "postbackClientId", {
         get: function () { return this.postbackOptions.postbackId; },
-        enumerable: false,
+        enumerable: true,
         configurable: true
     });
     Object.defineProperty(DotvvmAfterPostBackEventArgs.prototype, "viewModelName", {
         get: function () { return this.postbackOptions.viewModelName; },
-        enumerable: false,
+        enumerable: true,
         configurable: true
     });
     Object.defineProperty(DotvvmAfterPostBackEventArgs.prototype, "viewModel", {
         get: function () { return this.postbackOptions.viewModel; },
-        enumerable: false,
+        enumerable: true,
         configurable: true
     });
     Object.defineProperty(DotvvmAfterPostBackEventArgs.prototype, "sender", {
         get: function () { return this.postbackOptions.sender; },
-        enumerable: false,
+        enumerable: true,
         configurable: true
     });
     return DotvvmAfterPostBackEventArgs;
@@ -250,7 +249,7 @@ var DotvvmAfterPostBackWithRedirectEventArgs = /** @class */ (function (_super) 
     }
     Object.defineProperty(DotvvmAfterPostBackWithRedirectEventArgs.prototype, "redirectPromise", {
         get: function () { return this._redirectPromise; },
-        enumerable: false,
+        enumerable: true,
         configurable: true
     });
     return DotvvmAfterPostBackWithRedirectEventArgs;
@@ -661,7 +660,7 @@ var DotvvmSerialization = /** @class */ (function () {
         var deserialized = this.deserialize(ko.unwrap(value), unwrappedTarget[prop], deserializeAll);
         if (value instanceof Date) {
             // if we get Date value from API, it was converted to string, but we should note that it was date to convert it back
-            unwrappedTarget[prop + "$options"] = __assign(__assign({}, unwrappedTarget[prop + "$options"]), { isDate: true });
+            unwrappedTarget[prop + "$options"] = __assign({}, unwrappedTarget[prop + "$options"], { isDate: true });
         }
         // update the property
         if (ko.isObservable(deserialized)) { //deserialized is observable <=> its input target is observable
@@ -683,7 +682,7 @@ var DotvvmSerialization = /** @class */ (function () {
         }
     };
     DotvvmSerialization.prototype.copyPropertyMetadata = function (unwrappedTarget, prop, viewModel) {
-        unwrappedTarget[prop] = __assign(__assign({}, unwrappedTarget[prop]), viewModel[prop]);
+        unwrappedTarget[prop] = __assign({}, unwrappedTarget[prop], viewModel[prop]);
         var originalName = prop.substring(0, prop.length - "$options".length);
         if (typeof unwrappedTarget[originalName] === "undefined") {
             unwrappedTarget[originalName] = ko.observable();
@@ -1287,11 +1286,11 @@ var DotVVM = /** @class */ (function () {
                                     }
                                 }
                                 var result = responseObj.result;
-                                dotvvm.events.staticCommandMethodInvoked.trigger(__assign(__assign({}, data), { result: result, xhr: response }));
+                                dotvvm.events.staticCommandMethodInvoked.trigger(__assign({}, data, { result: result, xhr: response }));
                                 callback(result);
                             }
                             catch (error) {
-                                dotvvm.events.staticCommandMethodFailed.trigger(__assign(__assign({}, data), { xhr: response, error: error }));
+                                dotvvm.events.staticCommandMethodFailed.trigger(__assign({}, data, { xhr: response, error: error }));
                                 errorCallback({ xhr: response, error: error });
                             }
                             finally {
@@ -1311,7 +1310,7 @@ var DotVVM = /** @class */ (function () {
                             _this.events.error.trigger(new DotvvmErrorEventArgs(sender, _this.viewModels[viewModelName].viewModel, viewModelName, xhr, null));
                             console.warn("StaticCommand postback failed: " + xhr.status + " - " + xhr.statusText, xhr);
                             errorCallback({ xhr: xhr });
-                            dotvvm.events.staticCommandMethodFailed.trigger(__assign(__assign({}, data), { xhr: xhr }));
+                            dotvvm.events.staticCommandMethodFailed.trigger(__assign({}, data, { xhr: xhr }));
                         }, function (xhr) {
                             xhr.setRequestHeader("X-PostbackType", "StaticCommand");
                         });
