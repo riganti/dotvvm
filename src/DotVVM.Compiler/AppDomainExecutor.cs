3f4d201be3879e8145c872901d3962a8c9b66dcf
Adam Štěpánek <adam.stepanek@riganti.cz>
2020-10-08 14:36:55 +0200

    Add AppDomainExecutor

---
+++ src/DotVVM.Compiler/AppDomainExecutor.cs
@@ -0,0 +1,26 @@
+﻿#if NET461
+using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace DotVVM.Compiler
+{
+    [Serializable]
+    public class AppDomainExecutor : MarshalByRefObject
+    {
+        public void ExecuteCompile(
+            FileInfo assembly,
+            DirectoryInfo? projectDir,
+            string? rootNamespace)
+        {
+            var logger = CommandLineExtensions.Factory.CreateLogger("DotVVM.Compiler");
+            var projectAssembly = Assembly.LoadFrom(assembly.FullName);
+            Program.Compile(projectAssembly, projectDir, rootNamespace, logger);
+        }
+    }
+}
+#endif
--- src/DotVVM.Compiler/Program.cs
+++ src/DotVVM.Compiler/Program.cs
@@ -5,7 +5,6 @@ using System.CommandLine.Invocation;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.Loader;
 using DotVVM.CommandLine;
 using DotVVM.Framework.Binding;
 using DotVVM.Framework.Compilation;
@@ -32,24 +31,64 @@ namespace DotVVM.Compiler
             ILogger logger)
         {
 #if NETCOREAPP3_1
-            var dependencyResolver = new AssemblyDependencyResolver(assembly.FullName);
-            AssemblyLoadContext.Default.Resolving += (c, n) =>
+            var dependencyResolver = new System.Runtime.Loader.AssemblyDependencyResolver(assembly.FullName);
+            System.Runtime.Loader.AssemblyLoadContext.Default.Resolving += (c, n) =>
             {
                 var path = dependencyResolver.ResolveAssemblyToPath(n);
                 return path is object
                     ? c.LoadFromAssemblyPath(path)
                     : null;
             };
+            var projectAssembly = System.Runtime.Loader.AssemblyLoadContext.Default
+                .LoadFromAssemblyPath(assembly.FullName);
+            Compile(projectAssembly, projectDir, rootNamespace, logger);
+
+#elif NETCOREAPP2_1
+            // NB: This currently *almost* works for .NET Core 2.1. It tries to load a "DotVVM.Framework.resources"
+            //     assembly, which does not exist.
+            var builder = new McMaster.NETCore.Plugins.Loader.AssemblyLoadContextBuilder();
+            builder.SetMainAssemblyPath(assembly.FullName);
+            builder.SetDefaultContext(new NullLoadContext());
+            var baseDir = Path.GetDirectoryName(assembly.FullName);
+            var assemblyFileName = Path.GetFileNameWithoutExtension(assembly.FullName);
+            var depsJsonFile = Path.Combine(baseDir, assemblyFileName + ".deps.json");
+            if (File.Exists(depsJsonFile))
+            {
+                McMaster.NETCore.Plugins.Loader.DependencyContextExtensions.AddDependencyContext(builder, depsJsonFile);
+            }
+
+            var pluginRuntimeConfigFile = Path.Combine(baseDir, assemblyFileName + ".runtimeconfig.json");
+            McMaster.NETCore.Plugins.Loader.RuntimeConfigExtensions.TryAddAdditionalProbingPathFromRuntimeConfig(
+                builder: builder,
+                runtimeConfigPath: pluginRuntimeConfigFile,
+                includeDevConfig: true,
+                error: out _);
+            var loader = builder.Build();
+            loader.LoadFromAssemblyPath(assembly.FullName);
+            AssemblyLoadContext.Default.Resolving += (c, n) =>
+            {
+                var sideAssembly = loader.LoadFromAssemblyName(n);
+                return sideAssembly is object && sideAssembly != typeof(NullLoadContext).Assembly
+                    ? c.LoadFromAssemblyPath(sideAssembly.Location)
+                    : null;
+            };
             var projectAssembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(assembly.FullName);
             Compile(projectAssembly, projectDir, rootNamespace, logger);
-#elif NETCOREAPP2_1
-            var plugin = McMaster.NETCore.Plugins.PluginLoader.CreateFromAssemblyFile(
-                assembly.FullName,
-                new Type[] { typeof(ILogger) });
-            var projectAssembly = plugin.LoadDefaultAssembly();
-            var thisAssembly = plugin.LoadAssemblyFromPath(typeof(Program).Assembly.Location);
-            thisAssembly.GetType(typeof(Program).FullName).GetMethod(nameof(Compile))
-                .Invoke(null, new object?[] { projectAssembly, projectDir, rootNamespace, logger });
+
+#elif NET461
+            var setup = new AppDomainSetup { ApplicationBase = Path.GetDirectoryName(assembly.FullName) };
+            var configPath = assembly.FullName + ".config";
+            if (File.Exists(configPath))
+            {
+                setup.ConfigurationFile = configPath;
+            }
+            var domain = AppDomain.CreateDomain("DotVVM.Compiler.AppDomain", null, setup);
+            var executor = (AppDomainExecutor)domain.CreateInstanceFromAndUnwrap(
+                assemblyName: typeof(AppDomainExecutor).Assembly.Location,
+                typeName: typeof(AppDomainExecutor).FullName);
+            executor.ExecuteCompile(assembly, projectDir, rootNamespace);
+#else
+#error Fix TargetFrameworks.
 #endif
         }
 
