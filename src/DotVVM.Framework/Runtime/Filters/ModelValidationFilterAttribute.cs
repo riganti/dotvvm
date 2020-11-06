e085c486ad181a6792795d8cd9a153575728742f
Andrej Čižmárik <andrej.cizmarik@riganti.cz>
2020-11-06 14:47:51 +0100

    Adjusted validationPath generation for changed dataContext

--- src/DotVVM.Framework/Runtime/Filters/ModelValidationFilterAttribute.cs
+++ src/DotVVM.Framework/Runtime/Filters/ModelValidationFilterAttribute.cs
@@ -1,4 +1,5 @@
 #nullable enable
+using System.Linq;
 using System.Threading.Tasks;
 using DotVVM.Framework.Hosting;
 using DotVVM.Framework.Utils;
@@ -18,11 +19,30 @@ namespace DotVVM.Framework.Runtime.Filters
             if (!string.IsNullOrEmpty(context.ModelState.ValidationTargetPath))
             {
                 var validator = context.Services.GetRequiredService<IViewModelValidator>();
-                context.ModelState.Errors.AddRange(validator.ValidateViewModel(context.ModelState.ValidationTarget, context.ModelState.ValidationTargetPath));
+                context.ModelState.Errors.AddRange(validator.ValidateViewModel(context.ModelState.ValidationTarget, GetFullValidationPath(context)));
                 context.FailOnInvalidModelState();
             }
 
             return TaskUtils.GetCompletedTask();
+        }
+
+        private string GetFullValidationPath(IDotvvmRequestContext context)
+        {
+            if (context.ModelState.ValidationTargetPath == "/")
+                return context.ModelState.ValidationTargetPath;
+
+            var data = context.ReceivedViewModelJson;
+            if (data == null)
+                return context.ModelState.ValidationTargetPath;
+
+            var currentPathSegments = data["currentPath"].Values<string>();
+            if (currentPathSegments.FirstOrDefault() == null)
+                return context.ModelState.ValidationTargetPath;
+
+            var currentPath = string.Join("/", data["currentPath"].Values<string>());
+            var validationTargetPathPrefix = currentPath.Substring(currentPath.IndexOf(".") + 1);
+            var validationTargetPath = context.ModelState.ValidationTargetPath;
+            return $"{validationTargetPathPrefix}/{validationTargetPath}";
         }
     } 
 }
