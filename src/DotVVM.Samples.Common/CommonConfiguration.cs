cb4ae2b1caa97fccfc3534ce2896d720d3bed2bf
Tomáš Herceg <tomas.herceg@riganti.cz>
2020-12-18 11:43:46 +0100

    Added Gogole Maps sample

--- src/DotVVM.Samples.Common/CommonConfiguration.cs
+++ src/DotVVM.Samples.Common/CommonConfiguration.cs
@@ -83,7 +83,11 @@ namespace DotVVM.Samples.Common
             });
 
             resources.Register("FeatureSamples_Resources_TestPageModule", new ScriptModuleResource(new FileResourceLocation("~/Scripts/testPageModule.js")));
-
+            resources.Register("FeatureSamples_Resources_GoogleMapSample", new ScriptModuleResource(new FileResourceLocation("~/Scripts/googleMapSample.js"))
+            {
+                Dependencies = new[] { "googleMap" }
+            });
+            resources.Register("googleMap", new ScriptResource(new UrlResourceLocation("https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap")));
 
             resources.Register("testJsModule", new ScriptModuleResource(new InlineResourceLocation("export const commands = { myCommand() { console.info(\"Hello from page module\") } }")));
 
---
+++ src/DotVVM.Samples.Common/Scripts/googleMapSample.js
@@ -0,0 +1,6 @@
+﻿export function init(context) {
+    context.state.map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
+    });
+}
