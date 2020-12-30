a0f37a55a7ef12a1bd70e1037971678c5ef3ba94
Ladislav Šesták <quigamdev@users.noreply.github.com>
2020-12-30 18:14:47 +0100

    Update src/DotVVM.Framework/Resources/Scripts/global-declarations.ts
    Co-authored-by: Stanislav Lukeš <exyi@outlook.cz>

--- src/DotVVM.Framework/Resources/Scripts/global-declarations.ts
+++ src/DotVVM.Framework/Resources/Scripts/global-declarations.ts
@@ -145,7 +145,7 @@ interface DotvvmPostbackHandlerCollection {
 
 type DotvvmStaticCommandResponse = {
     result: any;
-    customData?: any;
+    customData: { [key: string]: any };
 } | {
     action: "redirect";
     url: string;
