e0c4f132e7251ded225fdc69ea562601360e1855
Tomáš Herceg <tomas.herceg@riganti.cz>
2021-01-17 18:01:49 +0100

    Fixed timing issues in UI tests

--- src/DotVVM.Samples.Tests/Control/RoleViewTests.cs
+++ src/DotVVM.Samples.Tests/Control/RoleViewTests.cs
@@ -20,20 +20,22 @@ namespace DotVVM.Samples.Tests.Control
 
                 void AssertInnerTextEquals(string selector, string text)
                 {
-                    AssertUI.InnerTextEquals(
-                        browser.FindElements(selector).ThrowIfDifferentCountThan(1).First(),
-                        text);
+                    browser.WaitFor(() => {
+                        AssertUI.InnerTextEquals(
+                            browser.FindElements(selector).ThrowIfDifferentCountThan(1).First(),
+                            text);
+                    }, 5000);
                 }
 
                 // make sure we are signed out (first should show IfNotMember, second should be hidden)
-                browser.First("input[value='Sign Out']").Click().Wait();
+                browser.First("input[value='Sign Out']").Click();
 
                 AssertInnerTextEquals(".result1", "I am not a member!");
                 AssertUI.IsNotDisplayed(browser, ".result2");
 
                 // sign in as admin (both should show IsMember content)
                 browser.First("input[type=checkbox][value=admin]").Click();
-                browser.First("input[value='Sign In']").Click().Wait();
+                browser.First("input[value='Sign In']").Click();
 
                 AssertInnerTextEquals(".result1", "I am a member!");
                 AssertInnerTextEquals(".result2", "I am a member!");
@@ -41,27 +43,27 @@ namespace DotVVM.Samples.Tests.Control
                 // sign in as moderator and headhunter (both should show IsMember content)
                 browser.First("input[type=checkbox][value=moderator]").Click();
                 browser.First("input[type=checkbox][value=headhunter]").Click();
-                browser.First("input[value='Sign In']").Click().Wait();
+                browser.First("input[value='Sign In']").Click();
 
                 AssertInnerTextEquals(".result1", "I am a member!");
                 AssertInnerTextEquals(".result2", "I am a member!");
 
                 // sign in as headhunter only (both should be visible but show that user is not a member)
                 browser.First("input[type=checkbox][value=headhunter]").Click();
-                browser.First("input[value='Sign In']").Click().Wait();
+                browser.First("input[value='Sign In']").Click();
 
                 AssertInnerTextEquals(".result1", "I am not a member!");
                 AssertInnerTextEquals(".result2", "I am not a member!");
 
                 // sign in as tester only (both should show IsMember content)
                 browser.First("input[type=checkbox][value=tester]").Click();
-                browser.First("input[value='Sign In']").Click().Wait();
+                browser.First("input[value='Sign In']").Click();
 
                 AssertInnerTextEquals(".result1", "I am a member!");
                 AssertInnerTextEquals(".result2", "I am a member!");
 
                 // sign out (first should show IfNotMember, second should be hidden)
-                browser.First("input[value='Sign Out']").Click().Wait();
+                browser.First("input[value='Sign Out']").Click();
 
                 AssertInnerTextEquals(".result1", "I am not a member!");
                 AssertUI.IsNotDisplayed(browser, ".result2");
