822beb021a493216e36cb0c6fa606de70d182ff1
Tomáš Herceg <tomas.herceg@riganti.cz>
2021-02-26 12:16:04 +0100

    Uncommented tests that were accidentally committed

--- src/DotVVM.Framework/Resources/Scripts/tests/stateManagement.test.ts
+++ src/DotVVM.Framework/Resources/Scripts/tests/stateManagement.test.ts
@@ -335,86 +335,86 @@ test("Serialized computed updates on changes", () => {
     expect(lastValue).toBe("b")
 })
 
-// test("Stress test - simple increments", async () => {
-//     jest.setTimeout(120_000);
+test("Stress test - simple increments", async () => {
+    jest.setTimeout(120_000);
 
-//     // watchEvents()
+    // watchEvents()
 
-//     await fc.assert(fc.asyncProperty(
-//         fc.integer(1, 100),
-//         fc.scheduler(),
-//         async (steps, scheduler) => {
-//             vm.Int(0)
-//             expect(vm.Int()).toBe(0)
-//             s.doUpdateNow()
-//             expect(s.state.Int).toBe(0);
+    await fc.assert(fc.asyncProperty(
+        fc.integer(1, 100),
+        fc.scheduler(),
+        async (steps, scheduler) => {
+            vm.Int(0)
+            expect(vm.Int()).toBe(0)
+            s.doUpdateNow()
+            expect(s.state.Int).toBe(0);
 
-//             await waitForEnd([
-//                 (async () => {
-//                     for (let i = 0; i < steps / 8 + 1; i++) {
-//                         await scheduler.schedule(Promise.resolve(), `Sync ${i}`)
-//                         s.doUpdateNow()
-//                     }
-//                 })(),
-//                 (async () => {
-//                     for (let i = 0; i < steps; i++) {
-//                         await scheduler.schedule(Promise.resolve(), `Inc ${i} + 1`)
-//                         expect(vm.Int()).toBe(i)
-//                         vm.Int(vm.Int() + 1)
-//                         expect(vm.Int()).toBe(i + 1)
-//                     }
-//                 })()],
-//                 scheduler,
-//                 () => {
-//                     expect(vm.Int()).toBe(s.state.Int)
-//                 })
-//         }
-//     ), { timeout: 2000 })
-// })
+            await waitForEnd([
+                (async () => {
+                    for (let i = 0; i < steps / 8 + 1; i++) {
+                        await scheduler.schedule(Promise.resolve(), `Sync ${i}`)
+                        s.doUpdateNow()
+                    }
+                })(),
+                (async () => {
+                    for (let i = 0; i < steps; i++) {
+                        await scheduler.schedule(Promise.resolve(), `Inc ${i} + 1`)
+                        expect(vm.Int()).toBe(i)
+                        vm.Int(vm.Int() + 1)
+                        expect(vm.Int()).toBe(i + 1)
+                    }
+                })()],
+                scheduler,
+                () => {
+                    expect(vm.Int()).toBe(s.state.Int)
+                })
+        }
+    ), { timeout: 2000 })
+})
 
-// test("Stress test - simple increments with postbacks in background", async () => {
-//     jest.setTimeout(120_000);
+test("Stress test - simple increments with postbacks in background", async () => {
+    jest.setTimeout(120_000);
 
-//     // watchEvents()
+    // watchEvents()
 
-//     await fc.assert(fc.asyncProperty(
-//         fc.integer(1, 100),
-//         fc.integer(1, 100),
-//         fc.scheduler(),
-//         async (steps, postbacks, scheduler) => {
-//             vm.Int(0)
-//             expect(vm.Int()).toBe(0)
-//             s.doUpdateNow()
-//             expect(s.state.Int).toBe(0);
+    await fc.assert(fc.asyncProperty(
+        fc.integer(1, 100),
+        fc.integer(1, 100),
+        fc.scheduler(),
+        async (steps, postbacks, scheduler) => {
+            vm.Int(0)
+            expect(vm.Int()).toBe(0)
+            s.doUpdateNow()
+            expect(s.state.Int).toBe(0);
 
-//             await waitForEnd([
-//                 (async () => {
-//                     for (let i = 0; i < steps / 8 + 1; i++) {
-//                         await scheduler.schedule(Promise.resolve(), `Sync ${i}`)
-//                         s.doUpdateNow()
-//                     }
-//                 })(),
-//                 (async () => {
-//                     for (let i = 0; i < postbacks; i++) {
-//                         await scheduler.schedule(Promise.resolve(), `Postback ${i}`)
-//                         s.setState(JSON.parse(JSON.stringify(s.state)));
-//                     }
-//                 })(),
-//                 (async () => {
-//                     for (let i = 0; i < steps; i++) {
-//                         await scheduler.schedule(Promise.resolve(), `Inc ${i} + 1`)
-//                         expect(vm.Int()).toBe(i)
-//                         vm.Int(vm.Int() + 1)
-//                         expect(vm.Int()).toBe(i + 1)
-//                     }
-//                 })()],
-//                 scheduler,
-//                 () => {
-//                     expect(vm.Int()).toBe(s.state.Int)
-//                 })
-//         }
-//     ), { timeout: 2000 })
-// })
+            await waitForEnd([
+                (async () => {
+                    for (let i = 0; i < steps / 8 + 1; i++) {
+                        await scheduler.schedule(Promise.resolve(), `Sync ${i}`)
+                        s.doUpdateNow()
+                    }
+                })(),
+                (async () => {
+                    for (let i = 0; i < postbacks; i++) {
+                        await scheduler.schedule(Promise.resolve(), `Postback ${i}`)
+                        s.setState(JSON.parse(JSON.stringify(s.state)));
+                    }
+                })(),
+                (async () => {
+                    for (let i = 0; i < steps; i++) {
+                        await scheduler.schedule(Promise.resolve(), `Inc ${i} + 1`)
+                        expect(vm.Int()).toBe(i)
+                        vm.Int(vm.Int() + 1)
+                        expect(vm.Int()).toBe(i + 1)
+                    }
+                })()],
+                scheduler,
+                () => {
+                    expect(vm.Int()).toBe(s.state.Int)
+                })
+        }
+    ), { timeout: 2000 })
+})
 
 test("lastSetError flag", () => {
 
