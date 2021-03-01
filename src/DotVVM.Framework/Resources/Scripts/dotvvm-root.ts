b5f27c789bb77c70e64439b9e1d0f1d6e608495e
Ladislav Šesták <ladislav.sestak@riganti.cz>
2021-03-02 00:10:20 +0100

    Fixed typescript definition of events for SPA

--- src/DotVVM.Framework/Resources/Scripts/dotvvm-root.ts
+++ src/DotVVM.Framework/Resources/Scripts/dotvvm-root.ts
@@ -74,7 +74,7 @@ const dotvvmExports = {
     isPostbackRunning,
     events: (compileConstants.isSpa ?
              { ...events, ...spaEvents } :
-             events),
+             events) as (typeof events | (typeof spaEvents & typeof events)),
     viewModels: {
         root: {
             get viewModel() { return getViewModel() }
@@ -101,7 +101,7 @@ if (compileConstants.isSpa) {
 }
 
 declare global {
-    const dotvvm: typeof dotvvmExports;
+    const dotvvm: typeof dotvvmExports & {isSpaReady?: typeof isSpaReady, handleSpaNavigation?: typeof handleSpaNavigation};
 
     interface Window {
         dotvvm: typeof dotvvmExports
