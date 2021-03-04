eddddccf9404f0163579045f73f4a45d231b198c
Andrej Čižmárik <andrej.cizmarik@riganti.cz>
2021-03-04 18:36:23 +0100

    ValidationTarget is no longer set to whole viewmodel when target property is null

--- src/Framework/Framework/Runtime/Commands/CommandResolver.cs
+++ src/Framework/Framework/Runtime/Commands/CommandResolver.cs
@@ -25,7 +25,9 @@ namespace DotVVM.Framework.Runtime.Commands
                 eventValidator.ValidateCommand(path, commandId, viewRootControl, validationTargetPath) :
                 eventValidator.ValidateControlCommand(path, commandId, viewRootControl, targetControl, validationTargetPath);
 
-            context.ModelState.ValidationTarget = findResult.Control!.GetValue(DotVVM.Framework.Controls.Validation.TargetProperty) ?? context.ViewModel;
+            context.ModelState.ValidationTarget = findResult.Control!.GetValue(DotVVM.Framework.Controls.Validation.TargetProperty);
+            if (context.ModelState.ValidationTargetPath == "/" && context.ModelState.ValidationTarget == null)
+                context.ModelState.ValidationTarget = context.ViewModel;
 
             return new ActionInfo(
                 findResult.Binding,
