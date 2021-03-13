fb943ecf8d0331a90dd2360925af4c07bd70c232
Ladislav Šesták <sestakladislav.jr@gmail.com>
2021-03-13 11:36:25 +0100

    Added script for javascript tests CI pipeline

---
+++ src/Tools/PerformJsTests.ps1
@@ -0,0 +1,21 @@
+﻿param([String]$outputDictionary)
+
+Set-Location $PSScriptRoot
+Set-Location ..\DotVVM.Framework
+npm install
+
+npm install yarn -g
+yarn add jest-junit -g
+
+Set-Location $PSScriptRoot
+Set-Location ..\DotVVM.Framework\Resources\Scripts\tests
+
+yarn jest --ci --reporters="jest-junit"
+
+Set-Location $PSScriptRoot
+Set-Location ..\DotVVM.Framework
+
+Copy-Item .\junit.xml $outputDictionary\js-tests-results.xml
+Remove-Item .\junit.xml
+
+Set-Location $PSScriptRoot
