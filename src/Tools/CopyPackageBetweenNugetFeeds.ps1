05febe6ad83082991d23b6bf1f3f30560bdabf8c
Michal Tichý <email@tichymichal.net>
2021-03-15 14:28:21 +0100

    added support for snupkg durring copu

--- src/Tools/CopyPackageBetweenNugetFeeds.ps1
+++ src/Tools/CopyPackageBetweenNugetFeeds.ps1
@@ -1,22 +1,26 @@
 Param(
     [string]$version,
     [string]$server, 
-    [string]$internalServer, 
+    [string]$internalServer,
+    [string]$internalSnupkgServer,
     [string]$apiKey,
-    [string]$packageId
+    [string]$packageId,
 )
 
     $packageId = $package.Package
     $webClient = New-Object System.Net.WebClient
     $url = "$internalServer/package/" + $packageId + "/" + $version
+    $snupkgUrl = "$internalServer/package/" + $packageId + "/" + $version
     $nupkgFile = Join-Path $PSScriptRoot ($packageId + "." + $version + ".nupkg")
+    $snupkgurl = "$internalSnupkgServer/snupkg/" + $nupkgFile
+
     $snupkgFile = Join-Path $PSScriptRoot ($packageId + "." + $version + ".snupkg")
 
     Write-Host "Downloading from $url"
     $webClient.DownloadFile($url, $nupkgFile)
     try {
-        $webClient.DownloadFile($url, $snupkgFile)
-    }catch {echo "No snupkg package found!"}
+        $webClient.DownloadFile($snupkgUrl, $snupkgFile)
+    }catch { Write-Host "No snupkg package found!"}
     Write-Host "Package downloaded from '$internalServer'."
 
     Write-Host "Uploading package..."
@@ -25,4 +29,4 @@ Param(
     Write-Host "Package uploaded to $server."
 
     Remove-Item $nupkgFile    
-    try {Remove-Item $snupkgFile}catch {}
+    try {Remove-Item $snupkgFile}catch{ Write-Host "Unable to delete snupkg!"}
