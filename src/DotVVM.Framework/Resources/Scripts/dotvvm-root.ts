8d1f13635d780d725cb4eaa2c6dd09e370a88b12
Tomáš Herceg <tomas.herceg@riganti.cz>
2021-04-02 16:53:10 +0200

    FileUpload control doesn't need an iframe

--- src/DotVVM.Framework/Resources/Scripts/dotvvm-root.ts
+++ src/DotVVM.Framework/Resources/Scripts/dotvvm-root.ts
@@ -54,8 +54,7 @@ const dotvvmExports = {
     },
     fileUpload: {
         reportProgress: fileUpload.reportProgress,
-        showUploadDialog: fileUpload.showUploadDialog,
-        createUploadId: fileUpload.createUploadId
+        showUploadDialog: fileUpload.showUploadDialog
     },
     api: {
         invoke: api.invoke,
