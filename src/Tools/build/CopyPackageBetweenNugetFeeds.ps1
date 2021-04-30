70249232dc511e6f12f4d03bde1372f8697e8571
Tomáš Herceg <tomas.herceg@riganti.cz>
2021-04-30 20:19:18 +0200

    Publish script update

--- src/Tools/build/CopyPackageBetweenNugetFeeds.ps1
+++ src/Tools/build/CopyPackageBetweenNugetFeeds.ps1
@@ -5,18 +5,18 @@ Param(
     [string]$apiKey,
     [string]$packageId
 )
-    Write-Host "Copping package: $packageId.$version"   
-    $webClient = New-Object System.Net.WebClient
-        
-    $url = "$internalServer/packages/$packageId/$version/$packageId.$version.nupkg"
-    $nupkgFile = Join-Path $PSScriptRoot ($packageId + "." + $version + ".nupkg")
 
-    Write-Host "Downloading from $url"
-    $webClient.DownloadFile($url, $nupkgFile)
-    Write-Host "Package downloaded from '$internalServer'."
-
-    Write-Host "Uploading package..."
-    & .\Tools\nuget.exe push $nupkgFile -source $server -apiKey $apiKey
-    Write-Host "Package uploaded to $server."
-
-    Remove-Item $nupkgFile
+# get package
+& .\tools\nuget.exe install $packageId -OutputDirectory .\tools\packages -version $version -DirectDownload -NoCache -DependencyVersion Ignore -source $internalServer
+$nupkgFile = dir -s ./tools/packages/$packageId.$version.nupkg | Select -First 1
+Write-Host "Downloaded package located on '$nupkgFile'"
+	
+if ($nupkgFile) {
+	# upload 
+	Write-Host "Uploading package..."
+	& .\tools\nuget.exe push $nupkgFile -source $server -apiKey $apiKey
+	Write-Host "Package uploaded to $server."
+}
+if ( Test-Path -Path ./tools/packages ) {
+	Remove-Item -Recurse -Force ./tools/packages
+}
