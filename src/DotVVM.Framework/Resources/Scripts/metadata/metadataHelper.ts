92fee9ac5cfc3856d3ab5cb417d3211064a68fbd
Andrej Čižmárik <andrej.cizmarik@riganti.cz>
2021-05-04 12:49:54 +0200

    Added helper functions for working with type metadata

---
+++ src/DotVVM.Framework/Resources/Scripts/metadata/metadataHelper.ts
@@ -0,0 +1,40 @@
+﻿import { getTypeInfo } from "./typeMap";
+
+export function getTypeId(viewModel: any): string {
+    if (typeof viewModel !== "object") {
+        throw Error("Expected object but received: " + typeof viewModel);
+    }
+
+    return ko.unwrap(viewModel.$type);
+}
+
+export function getTypeMetadata(viewModel: any): TypeMetadata {
+    let typeId = getTypeId(viewModel);
+    return getTypeInfo(typeId);
+}
+
+export function getEnumMetadata(enumMetadataId: string): EnumTypeMetadata {
+    let metadata = getTypeInfo(enumMetadataId);
+    if (metadata.type !== "enum") {
+        throw new Error("Expected enum, but received object");
+    }
+
+    return metadata as EnumTypeMetadata;
+}
+
+export function getEnumValue(identifier: string | number, enumMetadataId: string): number | null {
+    let metadata = getEnumMetadata(enumMetadataId);
+    if (typeof identifier === "string") {
+        return metadata.values[identifier];
+    }
+    else {
+        // Ensure this number is not already defined
+        for (const key in metadata.values) {
+            if (metadata.values[key] === identifier) {
+                return null;
+            }
+        }
+
+        return identifier;
+    }
+}
