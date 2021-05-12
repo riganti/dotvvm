8a324820a5220a25069710a15096b5b110e69b67
Milan Mikuš <milan.mikus@riganti.cz>
2021-05-12 22:45:33 +0200

    Postback abort simple sample

---
+++ src/DotVVM.Samples.Common/ViewModels/FeatureSamples/PostbackAbortSignal/LoadAbortViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotVVM.Framework.ViewModel;
+
+namespace DotVVM.Samples.Common.ViewModels.FeatureSamples.PostbackAbortSignal
+{
+    public class LoadAbortViewModel : DotvvmViewModelBase
+    {
+        [AllowStaticCommand]
+        public static async Task<string[]> LoadData()
+        {
+            await Task.Delay(2000);
+            return new[] { "strawberry", "lemon", "mango" };
+        }
+
+        public string[] Data { get; set; } = new string[] {};
+    }
+}
+
---
+++ src/DotVVM.Samples.Common/Views/FeatureSamples/PostbackAbortSignal/Loader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DotVVM.Framework.Binding;
+using DotVVM.Framework.Binding.Expressions;
+using DotVVM.Framework.Compilation.Javascript;
+using DotVVM.Framework.Controls;
+using DotVVM.Framework.Hosting;
+
+namespace DotVVM.Samples.Common.Views.FeatureSamples.PostbackAbortSignal
+{
+    public class Loader : HtmlGenericControl
+    {
+        public Command Load
+        {
+            get { return (Command)GetValue(LoadProperty); }
+            set { SetValue(LoadProperty, value); }
+        }
+        public static readonly DotvvmProperty LoadProperty
+            = DotvvmProperty.Register<Command, Loader>(c => c.Load, null);
+
+        public Loader() : base("input")
+        {
+
+        }
+
+        protected override void AddAttributesToRender(IHtmlWriter writer, IDotvvmRequestContext context)
+        {
+            base.AddAttributesToRender(writer, context);
+
+            var loadBinding = GetCommandBinding(LoadProperty);
+            if (loadBinding != null)
+            {
+                writer.AddAttribute("onclick", "window.abortController=new AbortController(); "+ KnockoutHelper.GenerateClientPostBackScript(
+                    nameof(Load),
+                    loadBinding,
+                    this,
+                    new PostbackScriptOptions(abortSignal: new CodeParameterAssignment("window.abortController.signal", OperatorPrecedence.Max))), true, ";");
+            }
+
+            writer.AddAttribute("value", "Load data");
+            writer.AddAttribute("type", "button");
+        }
+    }
+}
