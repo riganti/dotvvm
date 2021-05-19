8b8f647bd062ac879bcfdc728d5acdd483d95b60
Tomáš Herceg <tomas.herceg@riganti.cz>
2021-05-19 12:40:15 +0200

    Fixed polyfills for IE11

--- src/DotVVM.Framework/Resources/Scripts/Polyfills/utils.js
+++ src/DotVVM.Framework/Resources/Scripts/Polyfills/utils.js
@@ -3,3 +3,28 @@ if (!Math.trunc) {
         return v < 0 ? Math.ceil(v) : Math.floor(v);
     };
 }
+if (!String.prototype.includes) {
+    String.prototype.includes = function (search, start) {
+        'use strict';
+
+        if (search instanceof RegExp) {
+            throw TypeError('first argument must not be a RegExp');
+        }
+        if (start === undefined) { start = 0; }
+        return this.indexOf(search, start) !== -1;
+    };
+}
+if (!String.prototype.endsWith) {
+    String.prototype.endsWith = function (search, this_len) {
+        if (this_len === undefined || this_len > this.length) {
+            this_len = this.length;
+        }
+        return this.substring(this_len - search.length, this_len) === search;
+    };
+}
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(search, rawPos) {
+        var pos = rawPos > 0 ? rawPos | 0 : 0;
+        return this.substring(pos, pos + search.length) === search;
+    }
+}
