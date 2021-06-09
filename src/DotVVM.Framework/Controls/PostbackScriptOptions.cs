754d49f0a82d37817a447bb7725637cbc0acda25
Standa Lukeš <exyi@outlook.cz>
2021-06-09 18:48:21 +0000

    Remove binary breaking change in PostbackScriptOptions

--- src/DotVVM.Framework/Controls/PostbackScriptOptions.cs
+++ src/DotVVM.Framework/Controls/PostbackScriptOptions.cs
@@ -32,5 +32,24 @@ namespace DotVVM.Framework.Controls
             this.AllowPostbackHandlers = allowPostbackHandlers;
             AbortSignal = abortSignal;
         }
+
+        // TODO remove this overload with next major version
+        // it's left here for binary compatibility
+        public PostbackScriptOptions(bool useWindowSetTimeout = false,
+            bool? returnValue = false,
+            bool isOnChange = false,
+            string elementAccessor = "this",
+            CodeParameterAssignment? koContext = null,
+            CodeParameterAssignment? commandArgs = null,
+            bool allowPostbackHandlers = true)
+        {
+            this.UseWindowSetTimeout = useWindowSetTimeout;
+            this.ReturnValue = returnValue;
+            this.IsOnChange = isOnChange;
+            this.ElementAccessor = new CodeParameterAssignment(elementAccessor, OperatorPrecedence.Max);
+            this.KoContext = koContext;
+            this.CommandArgs = commandArgs;
+            this.AllowPostbackHandlers = allowPostbackHandlers;
+        }
     }
 }
