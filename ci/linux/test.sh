ee0092a4d9c2443c1c54ef5a84431c8a64b97053
Adam Štěpánek <adam.stepanek@riganti.cz>
2021-07-16 13:24:44 +0200

    Remove CI test scripts

--- ci/linux/test.sh
+++
@@ -1,1 +0,0 @@
-docker run --rm -it --mount "type=bind,src=${PWD},dst=/s" docker-reg.riganti.cz/docker-agent:latest-linux bash
--- ci/windows/test.ps1
+++
@@ -1,1 +0,0 @@
-docker run --rm -it --mount "type=bind,src=${PWD},dst=C:\s" docker-reg.riganti.cz/docker-agent:latest-win powershell
