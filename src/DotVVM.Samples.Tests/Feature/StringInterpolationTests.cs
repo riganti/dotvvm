7959a9b3ac653fa11eb59d2eccd8771029242582
František Siladi <frantisek.siladi@riganti.cz>
2021-07-22 13:56:52 +0200

    add string interpolation test

---
+++ src/DotVVM.Samples.Tests/Feature/StringInterpolationTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotVVM.Samples.Tests.Base;
+using DotVVM.Testing.Abstractions;
+using Riganti.Selenium.Core;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace DotVVM.Samples.Tests.Feature
+{
+    public class StringInterpolationTests : AppSeleniumTest
+    {
+        [Fact]
+        public void Feature_StringInterpolation_SpecialCharacterTest()
+        {
+            RunInAllBrowsers(browser => {
+                browser.NavigateToUrl(SamplesRouteUrls.FeatureSamples_StringInterpolation);
+            });
+
+
+        }
+        public StringInterpolationTests(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+}
