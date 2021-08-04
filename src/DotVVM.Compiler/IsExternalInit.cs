abcc2895601790ff630625f67b271f821d2fb54f
Adam Štěpánek <adam.stepanek@riganti.cz>
2021-08-04 12:01:50 +0200

    Add attributes to IsExternalInit in the Compiler

--- src/DotVVM.Compiler/IsExternalInit.cs
+++ src/DotVVM.Compiler/IsExternalInit.cs
@@ -1,7 +1,12 @@
 #if !NET5_0_OR_GREATER
 
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
 namespace System.Runtime.CompilerServices
 {
+    [DebuggerNonUserCode]
+    [ExcludeFromCodeCoverage]
     internal static class IsExternalInit {}
 }
 
