a1f530d35df11949b7de7a4721e8af20014016e0
Adam Štěpánek <adam.stepanek@riganti.cz>
2021-08-27 14:26:30 +0200

    Make internal css better
    ...again

--- src/DotVVM.Framework/Diagnostics/CompilationPageViewModel.cs
+++ src/DotVVM.Framework/Diagnostics/CompilationPageViewModel.cs
@@ -1,23 +1,14 @@
 ﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Threading.Tasks;
 using DotVVM.Framework.Compilation;
-using DotVVM.Framework.Compilation.Parser;
-using DotVVM.Framework.Configuration;
-using DotVVM.Framework.Controls.Infrastructure;
-using DotVVM.Framework.Hosting;
-using DotVVM.Framework.Routing;
 using DotVVM.Framework.ViewModel;
 
 namespace DotVVM.Framework.Diagnostics
 {
-    public class CompilationPageViewModel
-        : DotvvmViewModelBase
+    public class CompilationPageViewModel : DotvvmViewModelBase
     {
-        private readonly DotvvmConfiguration config;
         private readonly IDotvvmViewCompilationService viewCompilationService;
 
         public ImmutableArray<DotHtmlFileInfo> Routes => viewCompilationService.GetRoutes();
@@ -25,24 +16,24 @@ namespace DotVVM.Framework.Diagnostics
         public ImmutableArray<DotHtmlFileInfo> Controls => viewCompilationService.GetControls();
         public string ApplicationPath { get; set; }
         public bool CompileAfterLoad { get; set; }
+        public int ActiveTab { get; set; } = 0;
 
-        public CompilationPageViewModel(DotvvmConfiguration config, IDotvvmViewCompilationService viewCompilationService)
+        public CompilationPageViewModel(IDotvvmViewCompilationService viewCompilationService)
         {
-            this.config = config;
             this.viewCompilationService = viewCompilationService;
         }
 
         public override async Task Init()
         {
             ApplicationPath = Context.Configuration.ApplicationPhysicalPath;
-            CompileAfterLoad = config.Development.CompilationPage.ShouldCompileAllOnLoad;
+            CompileAfterLoad = Context.Configuration.Development.CompilationPage.ShouldCompileAllOnLoad;
 
             await base.Init();
         }
 
         public async Task CompileAll()
         {
-            await viewCompilationService.CompileAll(true,true);
+            await viewCompilationService.CompileAll(true, true);
         }
 
         public void BuildView(DotHtmlFileInfo file)
