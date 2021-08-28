6b4b5b825b15f2e2e8db409b594db7b109486f37
Ladislav Šesták <ladislav.sestak@riganti.cz>
2021-08-28 11:41:44 +0100

    Moved CI scripts from /tools to /ci/scripts && removed dotnetSummer.md and package-lock.json from root of repo

---
+++ ci/scripts/CopyBetweenNugetFeeds.ps1
@@ -0,0 +1,84 @@
+Param(
+    [string]$version,
+    [string]$server, 
+    [string]$internalServer,    
+    [string]$internalSnupkgServer,
+    [string]$apiKey
+)
+
+
+### Configuration
+$packages = @(
+    [pscustomobject]@{ Package = "DotVVM.Core"; Directory = "DotVVM.Core"; Type = "standard" },
+    [pscustomobject]@{ Package = "DotVVM"; Directory = "DotVVM.Framework" ; Type = "standard" },
+    [pscustomobject]@{ Package = "DotVVM.Owin"; Directory = "DotVVM.Framework.Hosting.Owin"; Type = "standard" },
+    [pscustomobject]@{ Package = "DotVVM.AspNetCore"; Directory = "DotVVM.Framework.Hosting.AspNetCore" ; Type = "standard" },
+    [pscustomobject]@{ Package = "DotVVM.Testing"; Directory = "DotVVM.Framework.Testing" ; Type = "standard" },
+    [pscustomobject]@{ Package = "DotVVM.CommandLine"; Directory = "DotVVM.CommandLine"; Type = "tool" },
+    [pscustomobject]@{ Package = "DotVVM.Templates"; Directory = "Templates" ; Type = "template" },
+    [pscustomobject]@{ Package = "DotVVM.Api.Swashbuckle.AspNetCore"; Directory = "DotVVM.Framework.Api.Swashbuckle.AspNetCore"; Type = "standard" },
+    [pscustomobject]@{ Package = "DotVVM.Api.Swashbuckle.Owin"; Directory = "DotVVM.Framework.Api.Swashbuckle.Owin"; Type = "standard" }
+)
+
+$webClient = New-Object System.Net.WebClient
+## Standard packages
+foreach ($package in $packages) {
+
+    $packageId = $package.Package    
+
+    Write-Host "Downloading $packageId ($version)"
+
+    # standard package
+    if ($package.Type -eq "standard") {
+        & .\tools\nuget.exe install $packageId -OutputDirectory .\tools\packages -version $version -DirectDownload -NoCache -DependencyVersion Ignore -source $internalServer | Out-Host
+        $nupkgFile = dir -s ./tools/packages/$packageId.$version.nupkg | Select -First 1
+        Write-Host "Downloaded package located on '$nupkgFile'"
+    }
+    # standard package
+    if ($package.Type -eq "tool") {
+        ## dotnet tools
+        dotnet tool install $packageId --tool-path ./tools/packages --version $version --add-source $internalServer | Out-Host
+        $nupkgFile = dir -s ./tools/packages/*/$packageId.$version.nupkg | Select -First 1
+        Write-Host "Downloaded tool located on '$nupkgFile'"
+    }
+    # dotnet templates
+    if ($package.Type -eq "template") {
+        dotnet new --install "$packageId::$version" --force --nuget-source $internalServer | Out-Host
+        $nupkgFile = dir -s $env:USERPROFILE/.templateengine/dotnetcli -filter $packageId.$version.nupkg | Select -First 1
+        Write-Host "Downloaded template located on '$nupkgFile'"
+    }
+    
+    if ($nupkgFile) {
+        # upload 
+        Write-Host "Uploading package..."
+        & .\tools\nuget.exe push $nupkgFile -source $server -apiKey $apiKey | Out-Host
+        Write-Host "Package uploaded to $server."
+    }
+    if (Test-Path -Path ./tools/packages) {
+        Remove-Item -Recurse -Force ./tools/packages
+    }
+
+    # snupkg management
+    
+        $snupkgUrl = "file://$internalSnupkgServer/snupkg/"+$packageId + "." + $version + ".snupkg"
+    $snupkgFile = Join-Path $PSScriptRoot ($packageId + "." + $version + ".snupkg")
+
+    try {
+      $webClient.DownloadFile($snupkgUrl, $snupkgFile)
+      $snupkgDownloaded = $true;
+    } catch {
+      Write-Host "No snupkg package found!"
+      $snupkgDownloaded = $false;
+    }        
+    
+    if ($snupkgDownloaded -eq $true){
+        Write-Host "Uploading snupkg package..."        
+        & .\Tools\nuget.exe push $snupkgFile -source $server -apiKey $apiKey | Out-Host
+		Write-Host "Uploaded snupkg package." 
+        try {
+			Remove-Item $snupkgFile
+		} catch {            
+            Write-Host "Unable to cleanup snupkg..."
+        }
+    }
+}
