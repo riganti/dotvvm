bb9f2bd84745f18485524120c79fa8dc771f2c1a
Adam Štěpánek <adam.stepanek@riganti.cz>
2021-09-03 12:30:42 +0200

    Upgrade target frameworks to 'netstandard2.1;net471'

--- src/DotVVM.Samples.BasicSamples.Api.Common/Utilities/DateTimeConverter.cs
+++ src/DotVVM.Samples.BasicSamples.Api.Common/Utilities/DateTimeConverter.cs
@@ -5,7 +5,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
-namespace DotVVM.Samples.BasicSamples.Api.AspNetCoreLatest.Utilities
+namespace DotVVM.Samples.BasicSamples.Api.Common.Utilities
 {
     public class DateTimeConverter : JsonConverter<DateTime>
     {
