f601780cc03deb83e29ac31f6eb952843ff4f7f5
Ladislav Šesták <ladislav.sestak@riganti.cz>
2021-10-16 15:37:31 +0200

    Deprecated owin authorize attribute in favor of Authorize() extension method

--- src/Framework/Hosting.Owin/Runtime/Filters/AuthorizeAttribute.cs
+++ src/Framework/Hosting.Owin/Runtime/Filters/AuthorizeAttribute.cs
@@ -13,6 +13,7 @@ namespace DotVVM.Framework.Runtime.Filters
     /// <summary>
     /// Specifies that the class or method requires the specified authorization.
     /// </summary>
+    [Obsolete("Please use context.Authorize(...). You can place it into ViewModel.Init or into your command method, for example. If you are using GlobalFilters, use AuthorizeActionFilter.")]
     public class AuthorizeAttribute : ActionFilterAttribute
     {
         /// <summary>
