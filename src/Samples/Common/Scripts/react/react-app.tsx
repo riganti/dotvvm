ecceab985cb182f90f5bd7d3327f5a7638cebd4a
Ladislav Šesták <ladislav.sestak@riganti.cz>
2021-10-31 14:02:04 +0100

    Moved JsComponent.React to separate web project folder

--- src/Samples/Common/Scripts/react/react-app.tsx
+++ src/Samples/Common/Scripts/react/react-app.tsx
@@ -1,12 +1,13 @@
 ﻿/// <reference path="../../../../Framework/Framework/obj/typescript-types/dotvvm.d.ts" />
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { KnockoutTemplateReactComponent, registerReactControl} from 'dotvvm.jscomponent.react';
 import { LineChart, XAxis, Tooltip, CartesianGrid, Line, Dot } from 'recharts';
 import type { StateManager } from 'state-manager';
 
 // react component
 function RechartComponent(props: any) {
-    const onMouse = props.onMouse ?? (() => {})
+    const onMouse = props.onMouse ?? (() => { })
     return (
         <LineChart
             width={400}
@@ -19,10 +20,10 @@ function RechartComponent(props: any) {
             {
                 Object.keys(props.data[0]).slice(1).map((s, i) =>
                     <Line type="monotone"
-                          dataKey={s}
-                          stroke={"#" + (i * 4).toString() + "87908"}
-                          yAxisId={i}
-                          onMouseEnter={(_) => onMouse(s)} />)
+                        dataKey={s}
+                        stroke={"#" + (i * 4).toString() + "87908"}
+                        yAxisId={i}
+                        onMouseEnter={(_) => onMouse(s)} />)
             }
         </LineChart>
     );
@@ -35,74 +36,6 @@ function TemplateSelector(props) {
             getChildContext={c => c.extend({ $kokos: 1 })} />
     </div>
 }
-
-type KnockoutTemplateReactComponent_Props = {
-    wrapperTag: string
-    templateName: string
-    getChildContext?: (context: KnockoutBindingContext) => KnockoutBindingContext
-    viewModel?: any
-}
-
-class KnockoutTemplateReactComponent extends React.Component<KnockoutTemplateReactComponent_Props> {
-    static defaultProps = {
-        wrapperTag: "div"
-    }
-    wrapRef: React.RefObject<HTMLElement> = React.createRef()
-    templateName = ko.observable()
-    viewModelStateManager?: StateManager<any>
-
-    // TODO: how to dispose the template?
-    // componentWillUnmount() {
-    // }
-    componentDidMount() {
-        setTimeout(() => this.initializeTemplate(), 5)
-    }
-    initializeTemplate() {
-        const e = this.wrapRef.current
-        let context: KnockoutBindingContext = ko.contextFor(e)
-        if (this.props.getChildContext) {
-            context = this.props.getChildContext(context)
-        }
-        else if (this.props.viewModel !== undefined) {
-            const updateEvent = new dotvvm.DotvvmEvent("templateInReact.newState")
-            this.viewModelStateManager = new dotvvm.StateManager(this.props.viewModel, updateEvent)
-            context = context.createChildContext(this.viewModelStateManager.stateObservable)
-        }
-        this.updateStuff()
-        ko.renderTemplate(this.templateName, context, {}, e)
-    }
-    componentDidUpdate() {
-        this.updateStuff()
-    }
-    updateStuff() {
-        if (this.templateName() !== this.props.templateName)
-            this.templateName(this.props.templateName)
-        if (this.viewModelStateManager) {
-            this.viewModelStateManager.setState(this.props.viewModel)
-        }
-    }
-    render() {
-        
-        return React.createElement(this.props.wrapperTag, { ref: this.wrapRef })
-    }
-}
-
-const registerReactControl = (ReactControl, defaultProps = {}) => ({
-    create: (elm, props, commands, templates) => {
-        const initialProps = { ...defaultProps, ...commands, ...templates }
-        let currentProps = { ...initialProps, ...props };
-        ReactDOM.render(<ReactControl {...currentProps} />, elm);
-        return {
-            updateProps(updatedProps) {
-                currentProps = { ...currentProps, ...updatedProps }
-                ReactDOM.render(<ReactControl {...currentProps} />, elm);
-            },
-            dispose() {
-                ReactDOM.unmountComponentAtNode(elm)
-            }
-        }
-    }
-});
 
 // DotVVM Context importer 
 export default (context) => ({
