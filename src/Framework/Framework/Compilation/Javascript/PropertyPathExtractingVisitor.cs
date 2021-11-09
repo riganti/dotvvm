0fe2fcc2b0a8566b26ac31213fb726f9aaffe43e
Andrej Čižmárik <andrej.cizmarik@riganti.cz>
2021-11-09 14:02:02 +0100

    `PropertyPathValidator` now throws whenever property paths contain invalid expressions

--- src/Framework/Framework/Compilation/Javascript/PropertyPathExtractingVisitor.cs
+++ src/Framework/Framework/Compilation/Javascript/PropertyPathExtractingVisitor.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using DotVVM.Framework.Compilation.Javascript.Ast;
 
@@ -7,6 +8,21 @@ namespace DotVVM.Framework.Compilation.Javascript
     internal class PropertyPathExtractingVisitor : JsNodeVisitor
     {
         private Stack<string> stack = new Stack<string>();
+        private string wrongExpressionErrorMessage = "Provided path expression is invalid. Make sure it contains only property identifiers, member accesses and indexers with numeric literals.";
+
+        public override void VisitArrowFunctionExpression(JsArrowFunctionExpression jsFunctionExpression) => throw new ArgumentException(wrongExpressionErrorMessage);
+        public override void VisitAssignmentExpression(JsAssignmentExpression assignmentExpression) => throw new ArgumentException(wrongExpressionErrorMessage);
+        public override void VisitBinaryExpression(JsBinaryExpression binaryExpression) => throw new ArgumentException(wrongExpressionErrorMessage);
+        public override void VisitBlockStatement(JsBlockStatement jsBlockStatement) => throw new ArgumentException(wrongExpressionErrorMessage);
+        public override void VisitConditionalExpression(JsConditionalExpression conditionalExpression) => throw new ArgumentException(wrongExpressionErrorMessage);
+        public override void VisitFunctionExpression(JsFunctionExpression jsFunctionExpression) => throw new ArgumentException(wrongExpressionErrorMessage);
+        public override void VisitIfStatement(JsIfStatement jsIfStatement) => throw new ArgumentException(wrongExpressionErrorMessage);
+        public override void VisitInvocationExpression(JsInvocationExpression invocationExpression) => throw new ArgumentException(wrongExpressionErrorMessage);
+        public override void VisitNewExpression(JsNewExpression newExpression) => throw new ArgumentException(wrongExpressionErrorMessage);
+        public override void VisitParenthesizedExpression(JsParenthesizedExpression parenthesizedExpression) => throw new ArgumentException(wrongExpressionErrorMessage);
+        public override void VisitReturnStatement(JsReturnStatement jsReturnStatement) => throw new ArgumentException(wrongExpressionErrorMessage);
+        public override void VisitUnaryExpression(JsUnaryExpression unaryExpression) => throw new ArgumentException(wrongExpressionErrorMessage);
+        public override void VisitVariableDefStatement(JsVariableDefStatement variableDefStatement) => throw new ArgumentException(wrongExpressionErrorMessage);
 
         public override void VisitMemberAccessExpression(JsMemberAccessExpression memberAccessExpression)
         {
@@ -31,7 +47,7 @@ namespace DotVVM.Framework.Compilation.Javascript
                     sb.Append(stack.Pop());
             }
 
-            return (sb.Length > 0) ? sb.ToString() : "/";
+            return sb.ToString();
         }
     }
 }
