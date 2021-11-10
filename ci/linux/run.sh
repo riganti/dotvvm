857ea37cca42489e1d558668c324483f294daf28
Ladislav Šesták <ladislav.sestak@riganti.cz>
2021-11-10 19:46:10 +0000

    Added global installation of rollup on linux build agent

--- ci/linux/run.sh
+++ ci/linux/run.sh
@@ -241,9 +241,12 @@ fi
 if [ $UI_TESTS -eq 1 ]; then
     clean_uitest
 
+    ensure_named_command "npm install rollup (global)" \
+        "cd \"$ROOT/src/Samples/Common\" \
+             npm install -g rollup"
+
     ensure_named_command "npm build" \
         "cd \"$ROOT/src/Samples/Common\" \
-            && npm install rollup \
             && npm ci --cache \"$ROOT/.npm\" --prefer-offline \
             && rollup -c"
 
