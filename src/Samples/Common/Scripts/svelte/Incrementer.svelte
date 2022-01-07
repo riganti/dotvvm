b88e84a118bf0dc043b332639c99dab537458674
Standa Lukeš <exyi@outlook.cz>
2022-01-07 20:06:28 +0000

    Prototype of JsComponent-Svelte integration

---
+++ src/Samples/Common/Scripts/svelte/Incrementer.svelte
@@ -0,0 +1,13 @@
+
+<script lang="ts">
+	export let i = 0
+</script>
+
+<div>
+	Svelte: {i}
+	<button type="button" on:click={_ => i++}> + </button>
+	<button type="button" on:click={_ => i--}> - </button>
+</div>
+<style>
+	
+</style>
