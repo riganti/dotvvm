8664a416f0974cf20c4b0d0876dec7a4b03fa813
Milan Mikuš <milan.mikus@riganti.cz>
2022-02-02 17:36:56 +0100

    Array tests added.

--- src/Tests/Runtime/ControlTree/DefaultControlTreeResolver/MarkupDeclaredPropertyTests.cs
+++ src/Tests/Runtime/ControlTree/DefaultControlTreeResolver/MarkupDeclaredPropertyTests.cs
@@ -6,7 +6,7 @@ using System;
 using System.Collections.Generic;
 
 namespace DotVVM.Framework.Tests.Runtime.ControlTree
-{ 
+{
     [TestClass]
     public class MarkupDeclaredPropertyTests : DefaultControlTreeResolverTestsBase
     {
@@ -31,6 +31,7 @@ fileName: "control.dotcontrol");
         [DataRow("10", "long", typeof(long), 10)]
         [DataRow("3.14", "double", typeof(double), 3.14)]
         [DataRow("'a'", "char", typeof(char), 'a')]
+        [DataRow("\"t\"+\"e\"+$\"{\"s\"}t{1+1}\"+DotVVM.Framework.Tests.Runtime.ControlTree.TestConstants.Test", "string", typeof(string), "test2test")]
         public void ResolvedTree_MarkupDeclaredProperty_CorrectConstantInitialValue(string initializer, string typeName, Type propertyType, object? testedValue)
         {
             var root = ParseSource(@$"@viewModel object
@@ -60,6 +61,40 @@ fileName: "control.dotcontrol");
             Assert.AreEqual("ItemId", declarationDirective.NameSyntax.Name);
 
             Assert.AreEqual(new Guid("645f970d-2879-4fff-a19d-ba7b4c4a4853"), declarationDirective.InitialValue);
+        }
+
+        [TestMethod]
+        public void ResolvedTree_MarkupDeclaredProperty_ArrayInitializer_Array()
+        {
+            var root = ParseSource(@$"@viewModel object
+@property int[] Items = [1, 1+1, 9/3, 9%5, 2*3-1, DotVVM.Framework.Tests.Runtime.ControlTree.TestConstants.Six]
+",
+fileName: "control.dotcontrol");
+            var declarationDirective = EnsureSingleResolvedDeclarationDirective(root);
+
+            Assert.IsFalse(declarationDirective.DothtmlNode.HasNodeErrors);
+            Assert.AreEqual(typeof(int[]).FullName, declarationDirective.PropertyType.FullName);
+            Assert.AreEqual("Items", declarationDirective.NameSyntax.Name);
+            
+            Assert.IsInstanceOfType(declarationDirective.InitialValue, typeof(int[]));
+            CollectionAssert.AreEqual(new int[] { 1, 2, 3, 4, 5, 6 }, (int[])declarationDirective.InitialValue);
+        }
+
+        [TestMethod]
+        public void ResolvedTree_MarkupDeclaredProperty_ArrayInitializer_List()
+        {
+            var root = ParseSource(@$"@viewModel object
+@property System.Collections.Generic.List<int> Items = [1, 2, 3, 4, 5, DotVVM.Framework.Tests.Runtime.ControlTree.TestConstants.Six]
+",
+fileName: "control.dotcontrol");
+            var declarationDirective = EnsureSingleResolvedDeclarationDirective(root);
+
+            Assert.IsFalse(declarationDirective.DothtmlNode.HasNodeErrors);
+            Assert.AreEqual(typeof(List<int>).FullName, declarationDirective.PropertyType.FullName);
+            Assert.AreEqual("Items", declarationDirective.NameSyntax.Name);
+
+            Assert.IsInstanceOfType(declarationDirective.InitialValue, typeof(List<int>));
+            CollectionAssert.AreEqual(new List<int> { 1, 2, 3, 4, 5 }, (List<int>)declarationDirective.InitialValue);
         }
 
         [TestMethod]
@@ -97,5 +132,11 @@ fileName: "control.dotcontrol");
                    .Content[2].CastTo<ResolvedControl>()
                    .Properties.First().Value.CastTo<ResolvedPropertyBinding>()
                    .Binding;
+    }
+
+    public class TestConstants
+    {
+        public const string Test = "test";
+        public const int Six = 6;
     }
 }
