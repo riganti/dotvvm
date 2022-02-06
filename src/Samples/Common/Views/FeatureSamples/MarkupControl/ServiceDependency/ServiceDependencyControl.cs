4a73b94483255c8bc7589d9206a4af1a7250634f
Ladislav Šesták <ladislav.sestak@riganti.cz>
2022-02-06 15:48:34 +0100

    Fixed DotvvmViewCompilationService when control has scoped dependency

---
+++ src/Samples/Common/Views/FeatureSamples/MarkupControl/ServiceDependency/ServiceDependencyControl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DotVVM.Framework.Controls;
+using DotVVM.Samples.BasicSamples.ViewModels.FeatureSamples.Redirect;
+
+namespace DotVVM.Samples.Common.Views.FeatureSamples.MarkupControl.ServiceDependency
+{
+    public class ServiceDependencyControl : DotvvmMarkupControl
+    {
+        public ServiceDependencyControl(ScopedTestService scopedService, SingletonTestService singletonService)
+        {
+        }
+    }
+}
+
