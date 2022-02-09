56c8478bd8ba423fc30093ac58fef0e75acbb14f
Andrej Čižmárik <andrej.cizmarik@riganti.cz>
2022-02-09 15:25:19 +0100

    Added documentation for the new validation API

--- src/Framework/Framework/ViewModel/Validation/DotvvmRequestContextValidationExtensions.cs
+++ src/Framework/Framework/ViewModel/Validation/DotvvmRequestContextValidationExtensions.cs
@@ -8,6 +8,10 @@ namespace DotVVM.Framework.ViewModel.Validation
 {
     public static class DotvvmRequestContextValidationExtensions
     {
+        /// <summary>
+        /// Adds a new validation error with the given message and attaches it to the root viewmodel.
+        /// </summary>
+        /// <param name="message">Validation error message</param>
         public static ViewModelValidationError AddModelError(this IDotvvmRequestContext context, string message)
         {
             var error = new ViewModelValidationError(message)
@@ -19,6 +23,13 @@ namespace DotVVM.Framework.ViewModel.Validation
             return error;
         }
 
+        /// <summary>
+        /// Adds a new raw validation error. This method is intended only for advanced use-cases and it partially bypasses the validation framework. 
+        /// Users of this method must provide an absolute validation path from the root viewmodel. Individual path segments need to be delimited using the '/' character. 
+        /// Example 1) /Customer/Id. Example 2) /Items/0/Price.
+        /// </summary>
+        /// <param name="absolutePath">Absolute validation path from the root viewmodel</param>
+        /// <param name="message">Validation error message</param>
         public static ViewModelValidationError AddRawModelError(this IDotvvmRequestContext context, string absolutePath, string message)
         {
             EnsurePathIsRooted(absolutePath);
@@ -31,6 +42,13 @@ namespace DotVVM.Framework.ViewModel.Validation
             return error;
         }
 
+        /// <summary>
+        /// Adds a new validation error with the given message and attaches it to the property determined by the provided expression. 
+        /// The target property must be reachable from the root viewmodel, otherwise the error won't be attached.
+        /// </summary>
+        /// <param name="vm">Viewmodel or one of its descendants (reachable objects)</param>
+        /// <param name="expression">Expression that determines the target property from the provided object</param>
+        /// <param name="message">Validation error message</param>
         public static ViewModelValidationError AddModelError<T, TProp>(this IDotvvmRequestContext context, T vm, Expression<Func<T, TProp>> expression, string message)
         {
             var lambdaExpression = (LambdaExpression)expression;
