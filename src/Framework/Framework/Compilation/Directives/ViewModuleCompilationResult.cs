a526eb1f2fe52bc4e0ec167a64de6417d223402a
Mylan719 <Mylan719@gmail.com>
2022-04-29 18:19:19 +0200

    Shortened record.

--- src/Framework/Framework/Compilation/Directives/ViewModuleCompilationResult.cs
+++ src/Framework/Framework/Compilation/Directives/ViewModuleCompilationResult.cs
@@ -3,16 +3,5 @@ using DotVVM.Framework.Binding;
 
 namespace DotVVM.Framework.Compilation.Directives
 {
-    public record ViewModuleCompilationResult
-    {
-        public JsExtensionParameter ExtensionParameter { get; }
-        public ViewModuleReferenceInfo Reference { get; }
-
-        public ViewModuleCompilationResult(JsExtensionParameter extensionParameter, ViewModuleReferenceInfo viewModuleReferenceInfo)
-        {
-            ExtensionParameter = extensionParameter;
-            Reference = viewModuleReferenceInfo;
-        }
-    }
-
+    public record ViewModuleCompilationResult(JsExtensionParameter ExtensionParameter, ViewModuleReferenceInfo ViewModuleReferenceInfo);
 }
