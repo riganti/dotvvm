2b1ada34945a35fcc0b7b1c0a6e976be2a5584d5
Tomáš Herceg <tomas.herceg@riganti.cz>
2022-05-04 13:49:23 +0200

    Removed unnecessary lines in CI publish script for npm package

--- ci/scripts/npm/build-dotvvm-types.ps1
+++ ci/scripts/npm/build-dotvvm-types.ps1
@@ -3,9 +3,6 @@ param([Parameter(Mandatory=$true)] $version)
 $dir = pwd
 push-location ../../../src/Framework/Framework
 
-$packageJson = Get-Content package.json | ConvertFrom-Json
-$typescriptVersion = $packageJson.devDependencies.typescript
-
 npm run tsc-types
 copy ./obj/typescript-types/dotvvm.d.ts "$dir/dotvvm-types/index.d.ts"
 
