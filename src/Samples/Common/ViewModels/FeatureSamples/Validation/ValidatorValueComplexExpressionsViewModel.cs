7749188914e4c97ed5e0df966c81ebbe1ce13d69
Andrej Čižmárik <andrej.cizmarik@riganti.cz>
2022-05-17 17:15:13 +0200

    Added sample and test for complex expression in `Validator.Value`

---
+++ src/Samples/Common/ViewModels/FeatureSamples/Validation/ValidatorValueComplexExpressionsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using DotVVM.Framework.ViewModel;
+
+namespace DotVVM.Samples.Common.ViewModels.FeatureSamples.Validation
+{
+    public class ValidatorValueComplexExpressionsViewModel : DotvvmViewModelBase
+    {
+        [Required]
+        public DateTime DateTime { get; set; }
+    }
+}
+
