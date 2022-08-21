e768cb0cf4ffa862890b8c96bf613f107265523c
Tomáš Herceg <tomas.herceg@riganti.cz>
2022-08-21 11:50:52 +0200

    Removed unnecessary TS declarations, and DotVVM customizations of Knockout added to the declaration file

--- src/Framework/Framework/Resources/Scripts/typings/knockout/knockout.dotvvm.d.ts
+++ src/Framework/Framework/Resources/Scripts/typings/knockout/knockout.dotvvm.d.ts
@@ -7,3 +7,21 @@ interface KnockoutDelaySync {
     resume(): void;
     run(action: () => void): void;
 }
+
+interface KnockoutUtils {
+    cloneNodes<T extends Node>(nodesArray: T[], shouldCleanNodes?: boolean): T[];
+}
+
+interface KnockoutObservableStatic {
+    <T>(value?: T | null, validator?: (newValue: T[]) => void): KnockoutObservable<T>;
+}
+interface KnockoutObservableArrayStatic {
+    <T>(value?: T[] | null, validator?: (newValue: T[]) => void): KnockoutObservableArray<T>;
+}
+
+interface KnockoutObservable<T> {
+    readonly state?: T;
+    patchState?(patch: any): void;
+    setState?(newState: any): void;
+    readonly updater?: UpdateDispatcher<T>;
+}
