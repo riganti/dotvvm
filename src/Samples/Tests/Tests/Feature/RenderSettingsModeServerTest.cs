245bd80a09e80c141b59644c372a9152bfa54295
Tomáš Herceg <tomas.herceg@riganti.cz>
2022-08-24 10:10:21 +0200

    Fixed bug in RenderSettings.Mode=Server when a collection is set to null value

--- src/Samples/Tests/Tests/Feature/RenderSettingsModeServerTest.cs
+++ src/Samples/Tests/Tests/Feature/RenderSettingsModeServerTest.cs
@@ -27,6 +27,67 @@ namespace DotVVM.Samples.Tests.Feature
             });
         }
 
+        [Fact]
+        public void Feature_RenderSettingsModeServer_RepeaterCollectionExchange()
+        {
+            RunInAllBrowsers(browser => {
+                browser.NavigateToUrl(SamplesRouteUrls.FeatureSamples_RenderSettingsModeServer_RepeaterCollectionExchange);
+
+                var repeater = browser.Single(".repeater");
+                var gridView = browser.Single(".gridview");
+                var comboBox = browser.Single(".combobox");
+
+                repeater.FindElements("li").ThrowIfDifferentCountThan(2);
+                gridView.FindElements("tbody tr").ThrowIfDifferentCountThan(2);
+                comboBox.FindElements("option").ThrowIfDifferentCountThan(2);
+
+                // use null
+                browser.ElementAt("input[type=checkbox]", 0).Click();
+                repeater.FindElements("li").ThrowIfDifferentCountThan(0);
+                browser.FindElements(".gridview").ThrowIfDifferentCountThan(0);
+                comboBox.FindElements("option").ThrowIfDifferentCountThan(0);
+
+                // don't use null
+                browser.ElementAt("input[type=checkbox]", 0).Click();
+                gridView = browser.Single(".gridview");
+                repeater.FindElements("li").ThrowIfDifferentCountThan(2);
+                gridView.FindElements("tbody tr").ThrowIfDifferentCountThan(2);
+                comboBox.FindElements("option").ThrowIfDifferentCountThan(2);
+
+                // use alternative collection
+                browser.ElementAt("input[type=checkbox]", 1).Click();
+                repeater.FindElements("li").ThrowIfDifferentCountThan(2);
+                gridView.FindElements("tbody tr").ThrowIfDifferentCountThan(2);
+                comboBox.FindElements("option").ThrowIfDifferentCountThan(2);
+                AssertUI.InnerTextEquals(repeater.First("li"), "alternative item 1");
+                AssertUI.InnerTextEquals(gridView.First("tbody tr"), "alternative item 1");
+                AssertUI.InnerTextEquals(comboBox.First("option"), "alternative item 1");
+            });
+        }
+
+        [Fact]
+        public void Feature_RenderSettingsModeServer_RepeaterCollectionSetToNull()
+        {
+            RunInAllBrowsers(browser => {
+                browser.NavigateToUrl(SamplesRouteUrls.FeatureSamples_RenderSettingsModeServer_RepeaterCollectionSetToNull);
+
+                var value = browser.Single(".value");
+                AssertUI.InnerTextEquals(value, "");
+                browser.FindElements("ul li").ThrowIfDifferentCountThan(2);
+
+                for (var i = 0; i < 2; i++)
+                {
+                    browser.ElementAt("input[type=button]", 0).Click();
+                    AssertUI.InnerTextEquals(value, "Null assigned to the collection");
+                    browser.FindElements("ul li").ThrowIfDifferentCountThan(0);
+
+                    browser.ElementAt("input[type=button]", 1).Click();
+                    AssertUI.InnerTextEquals(value, "Non-null assigned to the collection");
+                    browser.FindElements("ul li").ThrowIfDifferentCountThan(2);
+                }
+            });
+        }
+
         public RenderSettingsModeServerTest(ITestOutputHelper output) : base(output)
         {
         }
