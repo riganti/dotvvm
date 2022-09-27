1c15f5048242e70ff22d42ac1372f0e01d021131
Standa Lukeš <exyi@outlook.cz>
2022-09-27 22:22:09 +0200

    fixup! Fix enum coersions with int and empty strings

--- src/Framework/Framework/Resources/Scripts/utils/isNumber.ts
+++ src/Framework/Framework/Resources/Scripts/utils/isNumber.ts
@@ -1,7 +1,9 @@
 export function isNumber(value: any): boolean {
 	// soooo, this awesome language does not have a reasonable function to check if an object is number or a numeric string...
-	// Number(value) return NaN for anything which is not a number, except for empty/whitespace string which is converted to 0 🤦
-	// parseFloat works for empty string, but parseFloat("10dddd") returns 10, not NaN
+	// Number(value) returns NaN for anything which is not a number, except for empty/whitespace string which is converted to 0 🤦
+	// parseFloat correctly fails for empty string, but parseFloat("10dddd") returns 10, not NaN
+
+	// so value is number of Number(value) and parseFloat(value) return the same number
 	// if both return NaN, === is false, finally this "feature" is useful for something :D
 	return parseFloat(value) === Number(value);
 }
