a28ee96eea88285421b176b9cb0745c355fd48a3
Standa Lukeš <exyi@outlook.cz>
2022-11-08 17:32:10 +0100

    Attempt to fix API binding not refreshing
    Now it indeed does the refresh and it only does it once.
    
    However, there is still unsolved issue: With each refresh, the refreshOn function adds
    new observable subscription. This leads to decreasing
    performance during the page lifetime.
    For the same reason, we are leaking memory (although not much, hopefully)

--- src/Framework/Framework/Resources/Scripts/api/api.ts
+++ src/Framework/Framework/Resources/Scripts/api/api.ts
@@ -3,21 +3,17 @@ import { deserialize } from '../serialization/deserialize';
 import { logError, logWarning } from '../utils/logging';
 import { StateManager, unmapKnockoutObservables } from '../state-manager';
 import { DotvvmEvent } from '../events';
-import { callViewModuleCommand } from '../viewModules/viewModuleManager';
 
 type ApiComputed<T> =
     KnockoutObservable<T | null> & {
-        refreshValue: (throwOnError?: boolean) => PromiseLike<any> | undefined
+        refreshValue: () => PromiseLike<any>
     };
 
-type Result<T> =
-    { type: 'error', error: any } |
-    { type: 'result', result: T };
-
 type CachedValue = {
-    stateManager?: StateManager<any>
-    isLoading?: boolean
-    promise?: Result<PromiseLike<any>>
+    _stateManager?: StateManager<any>
+    _isLoading?: boolean
+    _promise?: PromiseLike<any>
+    _referenceCount: number
 }
 
 type Cache = { [k: string]: CachedValue }
@@ -35,79 +31,79 @@ export function invoke<T>(
     element: HTMLElement,
     sharingKeyProvider: (args: any[]) => string[]
 ): ApiComputed<T> {
+    const cache: Cache = element ? ((<any> element)["apiCachedValues"] ??= {}) : cachedValues;
+    const $type: TypeDefinition = { type: "dynamic" }
 
-    const args = ko.ignoreDependencies(argsProvider);
-    const callback = () => target[methodName].apply(target, args);
+    let args: any[];
+    let sharingKeyValue: string;
+    let cachedValue: CachedValue;
+    let stateManager = ko.observable() as KnockoutObservable<StateManager<any>>;
 
-    // the function gets re-evaluated when the observable changes - thus we need to cache the values
-    // GET requests can be cached globally, POST and other request must be cached on per-element scope
-    const sharingKeyValue = methodName + ":" + sharingKeyProvider(args);
-    const cache: Cache =
-        element ? ((<any> element)["apiCachedValues"] || ((<any> element)["apiCachedValues"] = {}))
-                : cachedValues;
-    const cachedValue = cache[sharingKeyValue] || (cache[sharingKeyValue] = {});
-
-    const isNew = cachedValue.stateManager == null
-    const $type = { type: "dynamic" }
-    if (cachedValue.stateManager == null)
-    {
-        const updateEvent = new DotvvmEvent("apiObject.newState")
-        cachedValue.stateManager = new StateManager<any>({ data: null, $type }, updateEvent)
-    }
-    const stateManager = cachedValue.stateManager
-
-    const load: () => Result<PromiseLike<any>> = () => {
-        try {
-            const result: PromiseLike<any> = Promise.resolve(ko.ignoreDependencies(callback));
-            return { type: 'result', result: result.then((val) => {
-                if (val) {
-                    const s = stateManager.setState({ data: unmapKnockoutObservables(deserialize(val)), $type });
-                    val = s.data
-                }
-                for (const t of notifyTriggers(args)) {
-                    eventHub.notify(t);
-                }
-                return val;
-            }, e => logWarning("rest-api", e)) };
-        } catch (e) {
-            logWarning("rest-api", e);
-            return { type: 'error', error: e };
+    function refreshArgs() {
+        args = ko.ignoreDependencies(argsProvider);
+        // the function gets re-evaluated when the observable changes - thus we need to cache the values
+        // GET requests can be cached globally, POST and other request must be cached on per-element scope
+        let oldKey = sharingKeyValue
+        sharingKeyValue = methodName + ":" + sharingKeyProvider(args)
+        const oldCached = cachedValue
+        cachedValue = cache[sharingKeyValue] ??= {_referenceCount: 0}
+        if (cachedValue === oldCached) {
+            return
         }
-    };
-
-
-    function refreshValue(throwOnError?: boolean) {
-        let promise = cachedValue.promise!;
-        if (!cachedValue.isLoading) {
-            cachedValue.isLoading = true;
-            promise = load();
-            cachedValue.promise = promise;
-        }
-        if (promise.type == 'error') {
-            cachedValue.isLoading = false;
-            if (throwOnError) {
-                throw promise.error;
-            } else {
-                return;
+        
+        cachedValue._referenceCount++
+        if (oldCached) {
+            oldCached._referenceCount--
+            if (oldCached._referenceCount <= 0) {
+                delete cache[oldKey]
             }
-        } else {
-            promise.result.then(p => cachedValue.isLoading = false, err => {
-                cachedValue.isLoading = false
-                logWarning("rest-api", err)
-            });
-            return promise.result;
         }
+
+        if (cachedValue._stateManager == null)
+        {
+            const updateEvent = new DotvvmEvent("apiObject.newState")
+            cachedValue._stateManager = new StateManager<any>({ data: null, $type }, updateEvent)
+            reloadApi()
+        }
+        stateManager(cachedValue._stateManager)
     }
-    if (isNew) {
-        refreshValue();
+    function reloadApi(): PromiseLike<any> {
+        if (!cachedValue._isLoading) {
+            cachedValue._isLoading = true
+            cachedValue._promise = load()
+            cachedValue._promise.then(p => {
+                cachedValue._isLoading = false
+            }, err => {
+                cachedValue._isLoading = false
+                logWarning("rest-api", err)
+            })
+        }
+        return cachedValue._promise!
     }
+    async function load(): Promise<any> {
+        let val = await ko.ignoreDependencies(() => target[methodName].apply(target, args))
+        if (val) {
+            const s = stateManager().setState({ data: unmapKnockoutObservables(deserialize(val)), $type })
+            val = s.data
+        }
+        for (const t of notifyTriggers(args)) {
+            eventHub.notify(t)
+        }
+        return val;
+    }
+
+    function refreshValue() {
+        refreshArgs()
+        return reloadApi()
+    }
+
+    refreshArgs()
     ko.computed(() =>
         refreshTriggers(args).map(trigger => typeof trigger == "string" ? eventHub.get(trigger)() : trigger())
     )
         .subscribe(_ => refreshValue());
 
-
-    const cmp = <ApiComputed<T>> <any> ko.pureComputed(() => stateManager.stateObservable().data());
+    const cmp = <ApiComputed<T>> <any> ko.pureComputed(() => stateManager().stateObservable().data());
     cmp.refreshValue = refreshValue
     return cmp;
 }
@@ -120,9 +116,6 @@ export function refreshOn<T>(
         logError("rest-api", `The object is not refreshable.`);
     }
     watch.subscribe(() => {
-        if (typeof value.refreshValue != "function") {
-            logError("rest-api", `The object is not refreshable.`);
-        }
         value.refreshValue();
     });
     return value;
