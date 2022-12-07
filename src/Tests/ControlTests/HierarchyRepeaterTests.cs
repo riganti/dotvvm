552df20dcd796b3f1d2acc09ec3839de2d20d365
Standa Lukeš <exyi@outlook.cz>
2022-12-07 22:37:29 +0100

    Add unit test for HierarchyRepeater+markup control+command

--- src/Tests/ControlTests/HierarchyRepeaterTests.cs
+++ src/Tests/ControlTests/HierarchyRepeaterTests.cs
@@ -20,6 +20,7 @@ namespace DotVVM.Framework.Tests.ControlTests
     {
         static readonly ControlTestHelper cth = new ControlTestHelper(config: config => {
             config.RouteTable.Add("Simple", "Simple", "Simple.dothtml");
+            config.Markup.AddMarkupControl("cc", "ButtonControl", "ButtonControl.dotcontrol");
         });
         OutputChecker check = new OutputChecker("testoutputs");
 
@@ -38,6 +39,42 @@ namespace DotVVM.Framework.Tests.ControlTests
             );
 
             check.CheckString(r.FormattedHtml, fileExtension: "html");
+        }
+
+        [TestMethod]
+        [DataRow(true)]
+        [DataRow(false)]
+        public async Task CommandInMarkupControl(bool clientRendering)
+        {
+            var r = await cth.RunPage(typeof(BasicTestViewModel), $$"""
+
+                    <dot:HierarchyRepeater DataSource={value: HItems}
+                                           ItemChildrenBinding={value: Children}
+                                           RenderSettings.Mode={{(clientRendering ? "Client" : "Server")}}>
+                        <ItemTemplate>
+                            <cc:ButtonControl DataContext={value: _this} Click={command: _root.SelectedLabel = _this.Label} />
+                        </ItemTemplate>
+                    </dot:HierarchyRepeater>
+                """,
+                directives: $"@service s = {typeof(TestService)}",
+                markupFiles: new Dictionary<string, string> {
+                    ["ButtonControl.dotcontrol"] = """
+                        @viewModel DotVVM.Framework.Tests.ControlTests.HierarchyRepeaterTests.BasicTestViewModel.SaneHierarchicalItem
+                        @property DotVVM.Framework.Binding.Expressions.Command Click
+
+                                    <dot:Button Click={command: _control.Click()} Text={value: _this.Label} />
+                        """
+                },
+                fileName: "CommandInMarkupControl" + (clientRendering ? "Client" : "Server")
+            );
+
+            // await r.RunCommand("_control.Click()", vm => vm is BasicTestViewModel.SaneHierarchicalItem { Label: "A_1_2" });
+            // Assert.AreEqual("A_1_2", (string)r.ViewModel.SelectedLabel);
+            check.CheckString(
+                r.OutputString,
+                checkName: clientRendering ? "client" : "server",
+                fileExtension: "html"
+            );
         }
 
         public class BasicTestViewModel: DotvvmViewModelBase
@@ -63,6 +100,8 @@ namespace DotVVM.Framework.Tests.ControlTests
                 public string Label { get; set; }
                 public List<SaneHierarchicalItem> Children { get; set; } = new List<SaneHierarchicalItem>();
             }
+
+            public string SelectedLabel { get; set; }
         }
     }
 }
