971dc4c11eed9cc54fe18179e8ae30900ad59793
Andrej Čižmárik <andrej.cizmarik@riganti.cz>
2023-01-12 18:32:30 +0100

    Added compilation warning when unsupported method called from resource binding

---
+++ src/Framework/Framework/Compilation/ControlTree/UnsupportedCallSiteCheckingVisitor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using DotVVM.Core.CodeAnalysis;
+using DotVVM.Framework.Binding.Expressions;
+using DotVVM.Framework.Compilation.ControlTree.Resolved;
+
+namespace DotVVM.Framework.Compilation.ControlTree
+{
+    public class UnsupportedCallSiteCheckingVisitor : ResolvedControlTreeVisitor
+    {
+        class ExpressionInspectingVisitor : ExpressionVisitor
+        {
+            public event Action<MethodInfo>? InvalidCallSiteDetected;
+
+            protected override Expression VisitMethodCall(MethodCallExpression node)
+            {
+                var callSiteAttr = node.Method.CustomAttributes.FirstOrDefault(a => a.AttributeType == typeof(UnsupportedCallSiteAttribute));
+                if (callSiteAttr is not null && callSiteAttr.ConstructorArguments.Any())
+                {
+                    if (callSiteAttr.ConstructorArguments.First().Value is int type && type == (int)CallSiteType.ServerSide)
+                        InvalidCallSiteDetected?.Invoke(node.Method);
+                }
+
+                return base.VisitMethodCall(node);
+            }
+        }
+
+        public override void VisitBinding(ResolvedBinding binding)
+        {
+            base.VisitBinding(binding);
+            if (binding.Binding is not ResourceBindingExpression)
+                return;
+
+            var expressionVisitor = new ExpressionInspectingVisitor();
+            expressionVisitor.InvalidCallSiteDetected += method =>
+                binding.DothtmlNode?.AddWarning($"Evaluation of method \"{method.Name}\" on server-side may yield unexpected results.");
+
+            expressionVisitor.Visit(binding.Expression);
+        }
+    }
+}
