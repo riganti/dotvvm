8e8eaf73db55d70f8fb39c0b57f10266cdbfc9d3
Tomáš Herceg <tomas.herceg@riganti.cz>
2023-03-01 22:06:22 +0100

    Added _api.IsLoading property

---
+++ src/Samples/Api.AspNetCore/Controllers/SlowController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotVVM.Samples.BasicSamples.Api.AspNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    public class SlowController : Controller
+    {
+        
+        private static readonly Random random = new();
+
+        [HttpPost]
+        [Route("random-number")]
+        public async Task<int> RandomNumber()
+        {
+            await Task.Delay(2000);
+            lock (random)
+            {
+                return random.Next(100);
+            }
+        }
+
+    }
+}
--- src/Samples/Common/TestWebApiClientAspNetCore.cs
+++ src/Samples/Common/TestWebApiClientAspNetCore.cs
@@ -7,6 +7,8 @@
 #pragma warning disable 108 // Disable "CS0108 '{derivedDto}.ToJson()' hides inherited member '{dtoBase}.ToJson()'. Use the new keyword if hiding was intended."
 #pragma warning disable 114 // Disable "CS0114 '{derivedDto}.RaisePropertyChanged(String)' hides inherited member 'dtoBase.RaisePropertyChanged(String)'. To make the current member override that implementation, add the override keyword. Otherwise add the new keyword."
 #pragma warning disable 472 // Disable "CS0472 The result of the expression is always 'false' since a value of type 'Int32' is never equal to 'null' of type 'Int32?'
+#pragma warning disable 1573 // Disable "CS1573 Parameter '...' has no matching param tag in the XML comment for ...
+#pragma warning disable 1591 // Disable "CS1591 Missing XML comment for publicly visible type or member ..."
 #pragma warning disable 8073 // Disable "CS8073 The result of the expression is always 'false' since a value of type 'T' is never equal to 'null' of type 'T?'"
 #pragma warning disable 3016 // Disable "CS3016 Arrays as attribute arguments is not CLS-compliant"
 
@@ -1074,6 +1076,87 @@ namespace DotVVM.Samples.Common.Api.AspNetCore
                         if (status_ == 200)
                         {
                             return;
+                        }
+                        else
+                        {
+                            var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            throw new ApiException("The HTTP status code of the response was not expected (" + status_ + ").", status_, responseData_, headers_, null);
+                        }
+                    }
+                    finally
+                    {
+                        if (disposeResponse_)
+                            response_.Dispose();
+                    }
+                }
+            }
+            finally
+            {
+                if (disposeClient_)
+                    client_.Dispose();
+            }
+        }
+    
+        /// <returns>Success</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        [DotVVM.Framework.Configuration.RestApiRegistrationHelpers.HttpMethod("Post")]
+        [DotVVM.Framework.Configuration.RestApiRegistrationHelpers.MethodUrl("api/Slow/random-number")]
+        [DotVVM.Framework.Configuration.RestApiRegistrationHelpers.AutoRefreshKey("Slow")]
+        public int SlowPostRandomNumber()
+        {
+            return System.Threading.Tasks.Task.Run(async () => await SlowPostRandomNumberAsync(System.Threading.CancellationToken.None)).GetAwaiter().GetResult();
+        }
+    
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>Success</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        [DotVVM.Framework.Configuration.RestApiRegistrationHelpers.HttpMethod("Post")]
+        [DotVVM.Framework.Configuration.RestApiRegistrationHelpers.MethodUrl("api/Slow/random-number")]
+        [DotVVM.Framework.Configuration.RestApiRegistrationHelpers.AutoRefreshKey("Slow")]
+        public async System.Threading.Tasks.Task<int> SlowPostRandomNumberAsync(System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken))
+        {
+            var urlBuilder_ = new System.Text.StringBuilder();
+            urlBuilder_.Append(BaseUrl != null ? BaseUrl.TrimEnd('/') : "").Append("/api/Slow/random-number");
+    
+            var client_ = _httpClient;
+            var disposeClient_ = false;
+            try
+            {
+                using (var request_ = new System.Net.Http.HttpRequestMessage())
+                {
+                    request_.Content = new System.Net.Http.StringContent(string.Empty, System.Text.Encoding.UTF8, "application/json");
+                    request_.Method = new System.Net.Http.HttpMethod("POST");
+                    request_.Headers.Accept.Add(System.Net.Http.Headers.MediaTypeWithQualityHeaderValue.Parse("application/json"));
+    
+                    PrepareRequest(client_, request_, urlBuilder_);
+    
+                    var url_ = urlBuilder_.ToString();
+                    request_.RequestUri = new System.Uri(url_, System.UriKind.RelativeOrAbsolute);
+    
+                    PrepareRequest(client_, request_, url_);
+    
+                    var response_ = await client_.SendAsync(request_, System.Net.Http.HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+                    var disposeResponse_ = true;
+                    try
+                    {
+                        var headers_ = System.Linq.Enumerable.ToDictionary(response_.Headers, h_ => h_.Key, h_ => h_.Value);
+                        if (response_.Content != null && response_.Content.Headers != null)
+                        {
+                            foreach (var item_ in response_.Content.Headers)
+                                headers_[item_.Key] = item_.Value;
+                        }
+    
+                        ProcessResponse(client_, response_);
+    
+                        var status_ = (int)response_.StatusCode;
+                        if (status_ == 200)
+                        {
+                            var objectResponse_ = await ReadObjectResponseAsync<int>(response_, headers_, cancellationToken).ConfigureAwait(false);
+                            if (objectResponse_.Object == null)
+                            {
+                                throw new ApiException("Response was null which was not expected.", status_, objectResponse_.Text, headers_, null);
+                            }
+                            return objectResponse_.Object;
                         }
                         else
                         {
