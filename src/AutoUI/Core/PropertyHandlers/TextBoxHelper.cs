546a48478c710e7a63a9358c7e8f482f6fc76472
Standa Lukeš <exyi@outlook.cz>
2023-06-22 12:47:45 +0200

    autoui: add support for DateOnly and TimeOnly

--- src/AutoUI/Core/PropertyHandlers/TextBoxHelper.cs
+++ src/AutoUI/Core/PropertyHandlers/TextBoxHelper.cs
@@ -7,7 +7,7 @@ namespace DotVVM.AutoUI.PropertyHandlers
     public static class TextBoxHelper
     {
         private static readonly HashSet<Type> stringTypes = new HashSet<Type>() { typeof(string), typeof(Guid) };
-        private static readonly HashSet<Type> dateTypes = new HashSet<Type>() { typeof(DateTime) };
+        private static readonly HashSet<Type> dateTypes = new HashSet<Type>() { typeof(DateTime), typeof(DateOnly), typeof(TimeOnly) };
 
         public static bool CanHandleProperty(Type type)
         {
