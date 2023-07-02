e34d27fb268f3b382b7b6857c796997a4e0a4c54
Standa Lukeš <exyi@outlook.cz>
2023-07-02 17:12:49 +0200

    Trim whitespace tokens from BindingCompilationException

--- src/Framework/Framework/Compilation/Binding/BindingCompilationException.cs
+++ src/Framework/Framework/Compilation/Binding/BindingCompilationException.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using DotVVM.Framework.Compilation.Parser;
 using DotVVM.Framework.Compilation.Parser.Binding.Parser;
+using DotVVM.Framework.Compilation.Parser.Binding.Tokenizer;
 
 namespace DotVVM.Framework.Compilation.Binding
 {
@@ -13,6 +15,21 @@ namespace DotVVM.Framework.Compilation.Binding
         public BindingCompilationException(string message, Exception? innerException, BindingParserNode node)
             : this(message, innerException, node.Tokens)
         {
+        }
+
+        public BindingCompilationException(string message, Exception? innerException, IEnumerable<BindingToken> tokens)
+            : base(message, innerException)
+        {
+            // trim leading and trailing whitespace (unless the whole expression is whitespace)
+            var tokensList = new List<BindingToken>(tokens);
+            var leadingWhitespace = tokensList.FindIndex(t => t.Type != BindingTokenType.WhiteSpace);
+            var trailingWhitespace = tokensList.FindLastIndex(t => t.Type != BindingTokenType.WhiteSpace);
+            if (leadingWhitespace >= 0 && trailingWhitespace >= 0)
+            {
+                tokensList.RemoveRange(trailingWhitespace + 1, tokensList.Count - trailingWhitespace - 1);
+                tokensList.RemoveRange(0, leadingWhitespace);
+            }
+            Tokens = tokensList;
         }
 
         public BindingCompilationException(string message, Exception? innerException, IEnumerable<TokenBase> tokens)
