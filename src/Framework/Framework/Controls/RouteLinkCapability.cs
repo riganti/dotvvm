47fa35ce55ff8774e92704664e34b211cad2f9dc
Tomáš Herceg <tomas.herceg@riganti.cz>
2023-07-28 14:14:29 +0200

    Fixed warning and test errors in RouteLinkCapability

--- src/Framework/Framework/Controls/RouteLinkCapability.cs
+++ src/Framework/Framework/Controls/RouteLinkCapability.cs
@@ -5,20 +5,19 @@ using DotVVM.Framework.Binding;
 namespace DotVVM.Framework.Controls
 {
     [DotvvmControlCapability()]
-    public sealed class RouteLinkCapability
+    public sealed record RouteLinkCapability
     {
         [PropertyGroup("Query-")]
         [DefaultValue(null)]
-        public IReadOnlyDictionary<string, ValueOrBinding<object>> QueryParameters { get; private set; } = new Dictionary<string, ValueOrBinding<object>>();
+        public IReadOnlyDictionary<string, ValueOrBinding<object>> QueryParameters { get; init; } = new Dictionary<string, ValueOrBinding<object>>();
 
         [PropertyGroup("Param-")]
         [DefaultValue(null)]
-        public IReadOnlyDictionary<string, ValueOrBinding<object>> Params { get; private set; } = new Dictionary<string, ValueOrBinding<object>>();
+        public IReadOnlyDictionary<string, ValueOrBinding<object>> Params { get; init; } = new Dictionary<string, ValueOrBinding<object>>();
 
-        public string RouteName { get; private set; }
+        public string RouteName { get; init; } = null!;
 
         [DefaultValue(null)]
-        public ValueOrBinding<string>? UrlSuffix { get; private set; }
-        
+        public ValueOrBinding<string>? UrlSuffix { get; init; }
     }
 }
