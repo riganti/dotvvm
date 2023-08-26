429f533eb3c5c3a071472c150817071f5a82ae99
Tomáš Herceg <tomas.herceg@riganti.cz>
2023-08-26 13:14:02 +0200

    Public helper methods for implementators of custom providers

--- src/AutoUI/Core/Controls/AutoFormBase.cs
+++ src/AutoUI/Core/Controls/AutoFormBase.cs
@@ -66,7 +66,7 @@ namespace DotVVM.AutoUI.Controls
         /// <summary>
         /// Gets the list of properties that should be displayed.
         /// </summary>
-        protected internal static PropertyDisplayMetadata[] GetPropertiesToDisplay(AutoUIContext context, FieldSelectorProps props)
+        public static PropertyDisplayMetadata[] GetPropertiesToDisplay(AutoUIContext context, FieldSelectorProps props)
         {
             var entityPropertyListProvider = context.Services.GetRequiredService<IEntityPropertyListProvider>();
             var properties = entityPropertyListProvider.GetProperties(context.EntityType, context.CreateViewContext());
