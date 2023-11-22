03dea5c0993d9629c324f56668ae55e299f82d4a
Tomáš Herceg <tomas.herceg@riganti.cz>
2023-11-22 13:51:34 +0100

    Added repro for a bug

--- src/Samples/Common/DotvvmStartup.cs
+++ src/Samples/Common/DotvvmStartup.cs
@@ -211,7 +211,7 @@ namespace DotVVM.Samples.BasicSamples
                     newDict["Id"] = 1221;
                     return newDict;
                 });
-            
+
             config.RouteTable.AddRouteRedirection("ComplexSamples_SPA_redirect", "ComplexSamples/SPA/redirect", "ComplexSamples_SPA_test");
         }
 
@@ -286,6 +286,9 @@ namespace DotVVM.Samples.BasicSamples
             config.Markup.AddMarkupControl("sample", "EmbeddedResourceControls_Button", "embedded://EmbeddedResourceControls/Button.dotcontrol");
             config.Markup.AddMarkupControl("cc", "NodeControl", "Views/ControlSamples/HierarchyRepeater/NodeControl.dotcontrol");
             config.Markup.AddMarkupControl("cc", "CommandInsideWhereControl", "Views/FeatureSamples/JavascriptTranslation/CommandInsideWhereControl.dotcontrol");
+            config.Markup.AddMarkupControl("cc", "CommandAsProperty", "Views/FeatureSamples/MarkupControl/CommandAsProperty.dotcontrol");
+            config.Markup.AddMarkupControl("cc", "CommandAsPropertyWrapper", "Views/FeatureSamples/MarkupControl/CommandAsPropertyWrapper.dotcontrol");
+
             config.Markup.AutoDiscoverControls(new DefaultControlRegistrationStrategy(config, "sample", "Views/"));
 
             if (config.Markup.Controls.FirstOrDefault(c => c.Src is not null && Path.IsPathRooted(c.Src)) is {} invalidControl)
---
+++ src/Samples/Common/ViewModels/FeatureSamples/MarkupControl/CommandAsPropertyPageViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotVVM.Framework.ViewModel;
+using DotVVM.Framework.Hosting;
+
+namespace DotVVM.Samples.Common.ViewModels.FeatureSamples.MarkupControl
+{
+    public class CommandAsPropertyPageViewModel : DotvvmViewModelBase
+    {
+
+        public List<ItemModel> Items { get; set; } = new() {
+            new ItemModel() { Name = "One", IsTrue = true },
+            new ItemModel() { Name = "Two", IsTrue = false },
+            new ItemModel() { Name = "Three", IsTrue = true }
+        };
+
+        public ItemModel SelectedItem { get; set; }
+
+        public Task MyFunction(string name, bool isTrue)
+        {
+            SelectedItem = new ItemModel() { Name = name, IsTrue = isTrue };
+            return Task.CompletedTask;
+        }
+
+
+        public class ItemModel
+        {
+            public string Name { get; set; }
+            public bool IsTrue { get; set; }
+
+        }
+    }
+}
+
