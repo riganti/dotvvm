7ced2df3a494730995cd50abfd2fcc3061008058
Miroslav Adamec <miroslav.adamec@hotmail.com>
2024-04-18 13:16:09 +0200

    feat: app insights - add grouped operation name

---
+++ src/Tracing/ApplicationInsights.AspNetCore/OperationNameTelemetryInitializer.cs
@@ -0,0 +1,31 @@
+﻿using DotVVM.Framework.Hosting;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.AspNetCore.Http;
+
+namespace DotVVM.Tracing.ApplicationInsights.AspNetCore;
+
+public class OperationNameTelemetryInitializer : ITelemetryInitializer
+{
+    private readonly IHttpContextAccessor _accessor;
+
+    public OperationNameTelemetryInitializer(IHttpContextAccessor accessor)
+    {
+        _accessor = accessor;
+    }
+
+    public void Initialize(ITelemetry telemetry)
+    {
+        var url = _accessor.HttpContext?.GetDotvvmContext()?.Route?.Url;
+        if (string.IsNullOrWhiteSpace(url) == false && telemetry is RequestTelemetry)
+        {
+            var method = _accessor.HttpContext.Request.Method;
+            var operationName = $"{method} /{url}";
+
+            var requestTelemetry = telemetry as RequestTelemetry;
+            requestTelemetry.Name = operationName;
+            requestTelemetry.Context.Operation.Name = operationName;
+        }
+    }
+}
---
+++ src/Tracing/ApplicationInsights.Owin/OperationNameTelemetryInitializer.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using DotVVM.Framework.Hosting;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.Channel;
+
+namespace DotVVM.Tracing.ApplicationInsights.Owin;
+
+public class OperationNameTelemetryInitializer : ITelemetryInitializer
+{
+    public void Initialize(ITelemetry telemetry)
+    {
+        var url = HttpContext.Current?.GetOwinContext()?.GetDotvvmContext()?.Route?.Url;
+        if (string.IsNullOrWhiteSpace(url) == false && telemetry is RequestTelemetry)
+        {
+            var method = HttpContext.Current.Request.HttpMethod;
+            var operationName = $"{method} /{url}";
+
+            var requestTelemetry = telemetry as RequestTelemetry;
+            requestTelemetry.Name = operationName;
+            requestTelemetry.Context.Operation.Name = operationName;
+        }
+    }
+}
