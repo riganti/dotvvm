8c433d6080c823b4e2e68a4f814f49e2ad317db1
Ladislav Šesták <ladislav.sestak@riganti.cz>
2024-07-07 21:18:45 +0100

    Added authorization check for tests dependent on GitHub API

--- src/Samples/Tests/Tests/Feature/DataSetTests.cs
+++ src/Samples/Tests/Tests/Feature/DataSetTests.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using DotVVM.Samples.Tests.Base;
 using DotVVM.Testing.Abstractions;
+using OpenQA.Selenium;
 using Riganti.Selenium.Core;
+using Riganti.Selenium.Core.Abstractions;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -22,9 +20,9 @@ namespace DotVVM.Samples.Tests.Feature
         [InlineData(SamplesRouteUrls.FeatureSamples_DataSet_GitHubApiStaticCommands)]
         public void Feature_DataSet_GitHubApi_Next(string url)
         {
-            RunInAllBrowsers(browser =>
-            {
+            RunInAllBrowsers(browser => {
                 browser.NavigateToUrl(url);
+                VerifyPageIsNotThrowingAuthError(browser);
 
                 var grid = browser.Single("next-grid", SelectByDataUi);
                 var pager = browser.Single("next-pager", SelectByDataUi);
@@ -49,6 +47,7 @@ namespace DotVVM.Samples.Tests.Feature
         {
             RunInAllBrowsers(browser => {
                 browser.NavigateToUrl(url);
+                VerifyPageIsNotThrowingAuthError(browser);
 
                 var grid = browser.Single("next-history-grid", SelectByDataUi);
                 var pager = browser.Single("next-history-pager", SelectByDataUi);
@@ -81,6 +80,15 @@ namespace DotVVM.Samples.Tests.Feature
                 pager.ElementAt("li", 1).Single("a").Click().Wait(500);
                 AssertUI.TextEquals(grid.ElementAt("tbody tr td", 0), issueId3);
             });
+        }
+        private void VerifyPageIsNotThrowingAuthError(IBrowserWrapper browser)
+        {
+            var elms = browser.FindElements(".exceptionMessage", By.CssSelector);
+            var errorMessage = elms.FirstOrDefault();
+
+            if (errorMessage is null) return;
+            AssertUI.Text(errorMessage, e => !e.Contains("401"), "GitHub Authentication Failed!",
+            waitForOptions: WaitForOptions.FromTimeout(1000));
         }
     }
 }
