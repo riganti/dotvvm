f8f4dd243fb2b664b2b8445564d444bd2cd46680
Tomáš Herceg <tomas.herceg@riganti.cz>
2024-07-14 09:53:19 +0200

    .NET Core projects updated to .NET 6

--- src/Samples/Common/regen_api_client.cmd
+++ src/Samples/Common/regen_api_client.cmd
@@ -1,5 +1,5 @@
-dotnet ..\..\Tools\CommandLine\bin\Debug\netcoreapp3.1\dotnet-dotvvm.dll api regen http://localhost:50001/swagger/v1/swagger.json
+dotnet ..\..\Tools\CommandLine\bin\Debug\net6.0\dotnet-dotvvm.dll api regen http://localhost:50001/swagger/v1/swagger.json
 tsc .\Scripts\TestWebApiClientAspNetCore.ts
 
-dotnet ..\..\Tools\CommandLine\bin\Debug\netcoreapp3.1\dotnet-dotvvm.dll api regen http://localhost:61453/swagger/v1/swagger.json
+dotnet ..\..\Tools\CommandLine\bin\Debug\net6.0\dotnet-dotvvm.dll api regen http://localhost:61453/swagger/v1/swagger.json
 tsc .\Scripts\TestWebApiClientOwin.ts
