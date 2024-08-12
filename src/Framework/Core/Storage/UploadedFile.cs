a8c784d9fbe2cd12170e6c739998d7d5337674d6
Standa Lukeš <exyi@outlook.cz>
2024-08-12 15:54:11 +0200

    Add some missing doccomments

--- src/Framework/Core/Storage/UploadedFile.cs
+++ src/Framework/Core/Storage/UploadedFile.cs
@@ -4,16 +4,21 @@ namespace DotVVM.Core.Storage
 {
     public class UploadedFile
     {
+        /// <summary> A unique, randomly generate ID of the uploaded file. Use this ID to get the file from <see cref="IUploadedFileStorage.GetFileAsync(Guid)" /> </summary>
         public Guid FileId { get; set; }
 
+        /// <summary> A user-specified name of the file. Use with caution, the user may specify this to be any string (for example <c>../../Web.config</c>) </summary>
         public string? FileName { get; set; }
 
         public FileSize FileSize { get; set; } = new FileSize();
 
+        /// <summary> If the file type matched one of type MIME types or extensions in <c>FileUpload.AllowedFileTypes</c>. Use with caution, the user may manipulate with this property. </summary>
         public bool IsFileTypeAllowed { get; set; } = true;
 
+        /// <summary> If the file size is larger that the limit specified in <c>FileUpload.MaxFileSize</c>. Use with caution, the user may manipulate with this property. </summary>
         public bool IsMaxSizeExceeded { get; set; } = false;
 
+        /// <summary> If the file satisfies both allowed file types and the size limit. Use with caution, the user may manipulate with this property. </summary>
         public bool IsAllowed
             => IsFileTypeAllowed && !IsMaxSizeExceeded;
     }
--- src/Framework/Framework/Controls/ValidatorPlacement.cs
+++ src/Framework/Framework/Controls/ValidatorPlacement.cs
@@ -5,8 +5,11 @@ namespace DotVVM.Framework.Controls
     [Flags]
     public enum ValidatorPlacement
     {
+        /// <summary> No validators are placed (automatically). </summary>
         None = 0,
+        /// <summary> Validator.Value is attached to the primary editor control (i.e. a TextBox in GridViewTextColumn) </summary>
         AttachToControl = 1,
+        /// <summary> A standalone Validator (span) control is placed after the editor control. </summary>
         Standalone = 2
     }
 }
