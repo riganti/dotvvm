8a148e68b1bd9b56642ad3cb3476408789714053
Standa Lukeš <exyi@outlook.cz>
2024-08-18 00:16:54 +0200

    AlternateCultureLinks: doccomments

--- src/Framework/Framework/Controls/AlternateCultureLinks.cs
+++ src/Framework/Framework/Controls/AlternateCultureLinks.cs
@@ -9,16 +9,18 @@ using DotVVM.Framework.Routing;
 
 namespace DotVVM.Framework.Controls
 {
+    /// <summary>
+    /// Renders a <c>&lt;link rel=alternate</c> element for each localized route equivalent to the current route.
+    /// On non-localized routes, it renders nothing (the control is therefore safe to use in a master page).
+    /// The href must be an absolute URL, so it will only work correctly if <c>Context.Request.Url</c> contains the corrent domain.
+    /// </summary>
+    /// <seealso href="https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/rel#alternate" />
+    /// <seealso href="https://developers.google.com/search/docs/specialty/international/localized-versions#html" />
+    /// <seealso cref="DotvvmRouteTable.Add(string, string, Type, object, LocalizedRouteUrl[])"/>
     public class AlternateCultureLinks : CompositeControl 
     {
-        private readonly IDotvvmRequestContext context;
-
-        public AlternateCultureLinks(IDotvvmRequestContext context)
-        {
-            this.context = context;
-        }
-
-        public IEnumerable<DotvvmControl> GetContents(string? routeName = null)
+        /// <param name="routeName">The name of the route to generate alternate links for. If not set, the current route is used. </param>
+        public IEnumerable<DotvvmControl> GetContents(IDotvvmRequestContext context, string? routeName = null)
         {
             var route = routeName != null ? context.Configuration.RouteTable[routeName] : context.Route;
             if (route is LocalizedDotvvmRoute localizedRoute)
@@ -31,7 +33,7 @@ namespace DotVVM.Framework.Controls
 
                     var languageCode = alternateCultureRoute.Key == "" ? "x-default" : alternateCultureRoute.Key.ToLowerInvariant();
                     var alternateUrl = context.TranslateVirtualPath(alternateCultureRoute.Value.BuildUrl(context.Parameters!));
-                    var absoluteAlternateUrl = BuildAbsoluteAlternateUrl(alternateUrl);
+                    var absoluteAlternateUrl = BuildAbsoluteAlternateUrl(context, alternateUrl);
 
                     yield return new HtmlGenericControl("link")
                         .SetAttribute("rel", "alternate")
@@ -42,7 +44,7 @@ namespace DotVVM.Framework.Controls
             }
         }
 
-        protected virtual string BuildAbsoluteAlternateUrl(string alternateUrl)
+        protected virtual string BuildAbsoluteAlternateUrl(IDotvvmRequestContext context, string alternateUrl)
         {
             return new Uri(context.HttpContext.Request.Url, alternateUrl).AbsoluteUri;
         }
