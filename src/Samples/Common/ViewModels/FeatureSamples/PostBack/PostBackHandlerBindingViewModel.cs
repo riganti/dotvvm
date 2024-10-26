176441a12591bd061589ca42063a4059bc94bbfe
Tomáš Herceg <tomas.herceg@riganti.cz>
2024-10-26 13:23:34 +0200

    Repro for postback handler binding translation issue

---
+++ src/Samples/Common/ViewModels/FeatureSamples/PostBack/PostBackHandlerBindingViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotVVM.Framework.ViewModel;
+using DotVVM.Framework.Hosting;
+
+namespace DotVVM.Samples.Common.ViewModels.FeatureSamples.PostBack
+{
+    public class PostBackHandlerBindingViewModel : DotvvmViewModelBase
+    {
+        public bool Enabled { get; set; } = false;
+
+        public int Counter { get; set; } = 0;
+
+        public string[] Items { get; set; } = new string[] { "Item 1", "Item 2", "Item 3" };
+    }
+}
+
