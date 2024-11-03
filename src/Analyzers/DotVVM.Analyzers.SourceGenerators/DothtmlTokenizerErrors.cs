8c676154082476f83de5126fa26cc71bb57ed712
Tomáš Herceg (RIGANTI s.r.o.) <tomas.herceg@riganti.cz>
2024-11-03 18:32:20 +0100

    Experiment with route source generators

---
+++ src/Analyzers/DotVVM.Analyzers.SourceGenerators/DothtmlTokenizerErrors.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotVVM.Framework.Resources
+{
+    public class DothtmlTokenizerErrors
+    {
+        internal static string BindingNotClosed;
+        internal static string DoubleBraceBindingNotClosed;
+        internal static string BindingInvalidFormat;
+        internal static string AttributeValueNotClosed;
+        internal static string MissingAttributeValue;
+        internal static string MissingTagName;
+        internal static string MissingTagPrefix;
+        internal static string XmlProcessingInstructionNotClosed;
+        internal static string DoctypeNotClosed;
+        internal static string CommentNotClosed;
+        internal static string CDataNotClosed;
+        internal static string TagNotClosed;
+        internal static string InvalidCharactersInTag;
+        internal static string TagNameExpected;
+        internal static string DirectiveValueExpected;
+        internal static string DirectiveNameExpected;
+    }
+}
