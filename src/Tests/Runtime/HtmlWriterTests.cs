94b959d7bec0e98bc4d19b6ed5414b7df45bd4a5
Tomáš Herceg <tomas.herceg@riganti.cz>
2026-06-12 14:31:06 +0200

    Fixed case sensitiviy issue in HtmlWriter

--- src/Tests/Runtime/HtmlWriterTests.cs
+++ src/Tests/Runtime/HtmlWriterTests.cs
@@ -75,5 +75,84 @@ namespace DotVVM.Framework.Tests.Runtime
             });
             Assert.AreEqual("<img a=\"&amp;amp;\" b=\"a&amp;b\" />", text);
         }
+
+        [TestMethod]
+        public void AttributeMerging_SameCasingAppend()
+        {
+            // Two "class" attributes (same casing) with append=true should be joined with a space
+            var text = WriteHtml(a => {
+                a.AddAttribute("class", "foo", append: true);
+                a.AddAttribute("class", "bar", append: true);
+                a.RenderBeginTag("div");
+                a.RenderEndTag();
+            });
+            Assert.AreEqual("<div class=\"foo bar\"></div>", text);
+        }
+
+        [TestMethod]
+        public void AttributeMerging_DifferentCasingAppend()
+        {
+            // "class" and "Class" (different casing) with append=true must be treated as the same attribute and joined
+            var text = WriteHtml(a => {
+                a.AddAttribute("class", "foo", append: true);
+                a.AddAttribute("Class", "bar", append: true);
+                a.RenderBeginTag("div");
+                a.RenderEndTag();
+            });
+            Assert.AreEqual("<div class=\"foo bar\"></div>", text);
+        }
+
+        [TestMethod]
+        public void AttributeMerging_DifferentCasingAppend_ThreeVariants()
+        {
+            // All three casing variants of "class" must collapse into one joined attribute
+            var text = WriteHtml(a => {
+                a.AddAttribute("class", "a", append: true);
+                a.AddAttribute("CLASS", "b", append: true);
+                a.AddAttribute("Class", "c", append: true);
+                a.RenderBeginTag("div");
+                a.RenderEndTag();
+            });
+            Assert.AreEqual("<div class=\"a b c\"></div>", text);
+        }
+
+        [TestMethod]
+        public void AttributeMerging_DifferentCasingOverwrite()
+        {
+            // When append=false (overwrite), the last value for the case-insensitive attribute name wins
+            var text = WriteHtml(a => {
+                a.AddAttribute("class", "first", append: false);
+                a.AddAttribute("Class", "second", append: false);
+                a.RenderBeginTag("div");
+                a.RenderEndTag();
+            });
+            Assert.AreEqual("<div class=second></div>", text);
+        }
+
+        [TestMethod]
+        public void AttributeMerging_DifferentCasingStyle()
+        {
+            // "style" and "Style" (different casing) with append=true must be joined with ";"
+            var text = WriteHtml(a => {
+                a.AddAttribute("style", "color:red", append: true, appendSeparator: ";");
+                a.AddAttribute("Style", "font-size:12px", append: true, appendSeparator: ";");
+                a.RenderBeginTag("div");
+                a.RenderEndTag();
+            });
+            Assert.AreEqual("<div style=\"color:red;font-size:12px\"></div>", text);
+        }
+
+        [TestMethod]
+        public void AttributeMerging_DifferentCasingNonAppendedAndAppended()
+        {
+            // mix of overwrite and append with different casing: first sets "foo", second appends "bar"
+            var text = WriteHtml(a => {
+                a.AddAttribute("class", "foo", append: false);
+                a.AddAttribute("Class", "bar", append: true);
+                a.RenderBeginTag("div");
+                a.RenderEndTag();
+            });
+            Assert.AreEqual("<div class=\"foo bar\"></div>", text);
+        }
     }
 }
