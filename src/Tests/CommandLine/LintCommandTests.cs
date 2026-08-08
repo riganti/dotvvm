0f9823c6acac59f401c72cdb061b6134810ec7a0
Tomáš Herceg <tomas.herceg@riganti.cz>
2026-08-08 18:31:56 +0200

    Fixed path discovery in CLI E2E tests

--- src/Tests/CommandLine/LintCommandTests.cs
+++ src/Tests/CommandLine/LintCommandTests.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -16,6 +17,12 @@ namespace DotVVM.Framework.Tests.CommandLine
         [DataRow("Owin")]
         public async Task LintCommand_ReportsDiagnosticsAndReturnsFailure(string sampleName)
         {
+            if (sampleName == "Owin" && Environment.OSVersion.Platform != PlatformID.Win32NT)
+            {
+                // Owin sample is not supported on non-Windows platforms, so we skip this test case.
+                return;
+            }
+
             var repositoryRoot = FindRepositoryRoot();
             var cliAssembly = Path.Combine(repositoryRoot, "Tools", "CommandLine", "bin", "Debug", "net8.0", "dotnet-dotvvm.dll");
             var sampleDirectory = Path.Combine(repositoryRoot, "Samples", sampleName);
@@ -43,18 +50,18 @@ namespace DotVVM.Framework.Tests.CommandLine
             StringAssert.Contains(output, ": error:");
         }
 
-        private static string FindRepositoryRoot()
+        private static string FindRepositoryRoot([CallerFilePath] string testFilePath = "")
         {
-            var directory = new DirectoryInfo(AppContext.BaseDirectory);
-            while (directory is not null)
-            {
-                if (File.Exists(Path.Combine(directory.FullName, "DotVVM.sln")))
-                    return directory.FullName;
-                directory = directory.Parent;
-            }
+            var sourceDirectory = Directory.GetParent(Path.GetDirectoryName(testFilePath)!)?.Parent?.FullName;
+            if (sourceDirectory is not null && IsSourceDirectory(sourceDirectory))
+                return sourceDirectory;
 
             throw new DirectoryNotFoundException("The repository root could not be found.");
         }
+
+        private static bool IsSourceDirectory(string directory) =>
+            File.Exists(Path.Combine(directory, "Tools", "CommandLine", "DotVVM.CommandLine.csproj")) &&
+            Directory.Exists(Path.Combine(directory, "Samples"));
     }
 }
 #endif
