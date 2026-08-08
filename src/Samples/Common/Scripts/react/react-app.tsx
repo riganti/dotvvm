481292ec210f6c13cb6428e0ae1097bafb77d6d9
Standa Lukeš <exyi@outlook.cz>
2026-08-08 14:03:10 +0200

    maybe fix React recharts sample

--- src/Samples/Common/Scripts/react/react-app.tsx
+++ src/Samples/Common/Scripts/react/react-app.tsx
@@ -1,25 +1,25 @@
 ﻿import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 import { KnockoutTemplateReactComponent, registerReactControl } from 'dotvvm-jscomponent-react';
-import { LineChart, XAxis, Tooltip, CartesianGrid, Line, Dot } from 'recharts';
+import { LineChart, XAxis, YAxis, Tooltip, CartesianGrid, Line } from 'recharts';
 
 // react component
 function RechartComponent(props: any) {
+    const seriesNames = ["Line1", "Line2", "Line3"]
     return (
         <LineChart
             width={400}
             height={400}
             data={props.data}
             margin={{ top: 5, right: 20, left: 10, bottom: 5 }} >
-            <XAxis dataKey="name" />
+            <XAxis dataKey="Name" />
+            <YAxis hide />
             <Tooltip />
             <CartesianGrid stroke="#f5f5f5" />
             {
-                Object.keys(props.data[0]).slice(1).map((s, i) =>
+                seriesNames.map((s, i) =>
                     <Line type="monotone"
                         dataKey={s}
                         stroke={"#" + (i * 4).toString() + "87908"}
-                        yAxisId={i}
                         onMouseEnter={(_) => props.onMouse(s)} />)
             }
         </LineChart>
