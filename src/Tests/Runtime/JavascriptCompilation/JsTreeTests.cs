ec9790206b430a8938cf3e08c8bfff5b42e7c0a6
Standa Lukeš <exyi@outlook.cz>
2026-08-14 00:11:22 +0200

    JS ast: avoid temporary variable collisions with JS parameters

--- src/Tests/Runtime/JavascriptCompilation/JsTreeTests.cs
+++ src/Tests/Runtime/JavascriptCompilation/JsTreeTests.cs
@@ -1,4 +1,5 @@
-﻿using DotVVM.Framework.Compilation.Javascript.Ast;
+﻿using DotVVM.Framework.Compilation.Javascript;
+using DotVVM.Framework.Compilation.Javascript.Ast;
 using DotVVM.Framework.Utils;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
@@ -20,6 +21,18 @@ namespace DotVVM.Framework.Tests.Runtime.JavascriptCompilation
             Assert.ThrowsException<Configuration.FreezableUtils.ObjectIsFrozenException>(() => left.MemberName = "omg");
             Assert.AreEqual(left.MemberName, "lol");
             Assert.ThrowsException<Configuration.FreezableUtils.ObjectIsFrozenException>(() => tree.Right.CastTo<JsLiteral>().Value = 8);
+        }
+
+        [TestMethod]
+        public void TemporaryVariableResolver_DoesNotCollideWithArrowParameter()
+        {
+            var temporary = new JsTemporaryVariableParameter(allowInlining: false);
+            var body = new JsBinaryExpression(temporary.ToExpression(), BinaryOperatorType.Sequence, temporary.ToExpression());
+            var expression = new JsArrowFunctionExpression(new[] { new JsIdentifier("a") }, body);
+
+            var resolved = JsTemporaryVariableResolver.ResolveVariables(expression);
+
+            Assert.AreEqual("(a)=>{let b;return b,b;}", resolved.FormatScript());
         }
     }
 }
