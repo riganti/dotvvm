riganti/dotvvm

commit 54676ae10a135b8dc2d33b497a573b17c7bb3675

binding compiler: fix invocation of generic method with default(T) parameter or params T[]

src/Framework/Framework/Compilation/Binding/MemberExpressionFactory.cs
@@ -415,6 +415,28 @@ namespace DotVVM.Framework.Compilation.Binding
             public bool HasParamsAttribute { get; set; }
         }
 
+        private Expression GetDefaultValue(ParameterInfo parameter)
+        {
+            if (parameter.HasDefaultValue)
+            {
+                var value = parameter.DefaultValue;
+                if (value is null && parameter.ParameterType.IsValueType)
+                {
+                    // null with struct type means `default(T)`
+                    value = ReflectionUtils.GetDefaultValue(parameter.ParameterType);
+                }
+                return Expression.Constant(value, parameter.ParameterType);
+            }
+            else if (parameter.IsDefined(ParamArrayAttributeType))
+            {
+                return Expression.NewArrayInit(parameter.ParameterType.GetElementType().NotNull());
+            }
+            else
+            {
+                throw new Exception($"Internal error: parameter {parameter.Name} of method {parameter.Member.Name} does not have a default value.");
+            }
+        }
+
         private MethodRecognitionResult? TryCallMethod(MethodInfo method, Type[]? typeArguments, Expression[] positionalArguments, IDictionary<string, Expression>? namedArguments)
         {
             if (positionalArguments.Contains(null)) throw new ArgumentNullException("positionalArguments[]");
@@ -445,7 +467,7 @@ namespace DotVVM.Framework.Compilation.Binding
                     if (typeArgs[genericArgumentPosition] == null)
                     {
                         // try to resolve from arguments
-                        var argType = GetGenericParameterType(genericArguments[genericArgumentPosition], parameterTypes, args.Select(s => s.Type).ToArray());
+                        var argType = GetGenericParameterType(genericArguments[genericArgumentPosition], parameterTypes, args.Select(s => s?.Type).ToArray());
                         automaticTypeArgs++;
                         if (argType != null) typeArgs[genericArgumentPosition] = argType;
                         else return null;
@@ -466,11 +488,15 @@ namespace DotVVM.Framework.Compilation.Binding
             }
             else if (typeArguments != null) return null;
 
-            // cast arguments
+            // cast arguments and fill defaults
             for (int i = 0; i < args.Length; i++)
             {
+                if (args[i] == null)
+                {
+                    args[i] = GetDefaultValue(parameters[i]);
+                }
                 Type elm;
-                if (args.Length == i + 1 && hasParamsArrayAttributes && !args[i].Type.IsArray)
+                if (args.Length == i + 1 && hasParamsArrayAttributes && !args[i]!.Type.IsArray)
                 {
                     elm = parameters[i].ParameterType.GetElementType().NotNull();
                     if (positionalArguments.Skip(i).Any(s => TypeConversion.ImplicitConversion(s, elm) is null))
@@ -482,7 +508,7 @@ namespace DotVVM.Framework.Compilation.Binding
                 {
                     elm = parameters[i].ParameterType;
                 }
-                var casted = TypeConversion.ImplicitConversion(args[i], elm);
+                var casted = TypeConversion.ImplicitConversion(args[i]!, elm);
                 if (casted == null)
                 {
                     return null;
@@ -492,7 +518,7 @@ namespace DotVVM.Framework.Compilation.Binding
                     castCount++;
                     args[i] = casted;
                 }
-                if (args.Length == i + 1 && hasParamsArrayAttributes && !args[i].Type.IsArray)
+                if (args.Length == i + 1 && hasParamsArrayAttributes && !args[i]!.Type.IsArray)
                 {
                     var converted = positionalArguments.Skip(i)
                         .Select(a => TypeConversion.EnsureImplicitConversion(a, elm))
@@ -505,13 +531,13 @@ namespace DotVVM.Framework.Compilation.Binding
                 automaticTypeArgCount: automaticTypeArgs,
                 castCount: castCount,
                 method: method,
-                arguments: args,
+                arguments: args!,
                 paramsArrayCount: positionalArguments.Length - args.Length,
                 hasParamsAttribute: hasParamsArrayAttributes,
                 isExtension: false
             );
         }
-        private static bool TryPrepareArguments(ParameterInfo[] parameters, Expression[] positionalArguments, IDictionary<string, Expression>? namedArguments, [MaybeNullWhen(false)] out Expression[] arguments, out int castCount)
+        private static bool TryPrepareArguments(ParameterInfo[] parameters, Expression[] positionalArguments, IDictionary<string, Expression>? namedArguments, [MaybeNullWhen(false)] out Expression?[] arguments, out int castCount)
         {
             castCount = 0;
             arguments = null;
@@ -522,7 +548,7 @@ namespace DotVVM.Framework.Compilation.Binding
             if (!hasParamsArrayAttribute && parameters.Length < positionalArguments.Length)
                 return false;
 
-            arguments = new Expression[parameters.Length];
+            arguments = new Expression?[parameters.Length];
             var copyItemsCount = !hasParamsArrayAttribute ? positionalArguments.Length : parameters.Length;
 
             if (hasParamsArrayAttribute && parameters.Length > positionalArguments.Length)
@@ -530,8 +556,9 @@ namespace DotVVM.Framework.Compilation.Binding
                 var parameter = parameters.Last();
                 var elementType = parameter.ParameterType.GetElementType().NotNull();
 
-                // User specified no arguments for the `params` array, we need to create an empty array
-                arguments[arguments.Length - 1] = Expression.NewArrayInit(elementType);
+                // User specified no arguments for the `params` array => use default value
+                // created later by the GetDefaultValue, after we know the generic arguments
+                arguments[arguments.Length - 1] = null;
 
                 // Last argument was just generated => do not copy
                 addedArguments++;
@@ -561,7 +588,7 @@ namespace DotVVM.Framework.Compilation.Binding
                 else if (parameters[i].HasDefaultValue)
                 {
                     castCount++;
-                    arguments[i] = Expression.Constant(parameters[i].DefaultValue, parameters[i].ParameterType);
+                    arguments[i] = null;
                 }
                 else if (parameters[i].IsDefined(ParamArrayAttributeType))
                 {
@@ -577,29 +604,30 @@ namespace DotVVM.Framework.Compilation.Binding
             return true;
         }
 
-        private Type? GetGenericParameterType(Type genericArg, Type[] searchedGenericTypes, Type[] expressionTypes)
+        private Type? GetGenericParameterType(Type genericArg, Type[] searchedGenericTypes, Type?[] expressionTypes)
         {
             for (var i = 0; i < searchedGenericTypes.Length; i++)
             {
                 if (expressionTypes.Length <= i) return null;
+                var expression = expressionTypes[i];
+                if (expression == null) continue;
                 var sgt = searchedGenericTypes[i];
                 if (sgt == genericArg)
                 {
-                    return expressionTypes[i];
+                    return expression;
                 }
                 if (sgt.IsArray)
                 {
                     var elementType = sgt.GetElementType();
-                    var expressionElementType = expressionTypes[i].GetElementType();
+                    var expressionElementType = expression.GetElementType();
                     if (elementType == genericArg)
                         return expressionElementType;
                     else
-                        return GetGenericParameterType(genericArg, searchedGenericTypes[i].GetGenericArguments(), expressionTypes[i].GetGenericArguments());
+                        return GetGenericParameterType(genericArg, searchedGenericTypes[i].GetGenericArguments(), expression.GetGenericArguments());
                 }
                 else if (sgt.IsGenericType)
                 {
                     Type[]? genericArguments = null;
-                    var expression = expressionTypes[i];
 
                     if (expression.IsArray)
                     {

src/Tests/Binding/BindingCompilationTests.cs
@@ -1055,6 +1055,39 @@ namespace DotVVM.Framework.Tests.Binding
             Assert.AreEqual(42, result(42));
         }
 
+        [DataTestMethod]
+        [DataRow("100", typeof(int))]
+        [DataRow("'aa'", null)]
+        [DataRow("NullableDateOnly", null)]
+        [DataRow("DateOnly", typeof(DateOnly))]
+        public void BindingCompiler_GenericMethod_DefaultArgument(string expression, Type resultType)
+        {
+            var result = ExecuteBinding($"_this.GenericDefault({expression})", new [] { new TestViewModel() });
+            if (resultType == null)
+            {
+                Assert.IsNull(result);
+            }
+            else
+            {
+                Assert.AreEqual(resultType, result.GetType(), message: $"_this.GenericDefault({expression}) returned {result} of type {result?.GetType().FullName ?? "null"}");
+                Assert.AreEqual(ReflectionUtils.GetDefaultValue(resultType), result);
+            }
+        }
+
+        [TestMethod]
+        public void BindingCompiler_GenericMethod_ParamsEmpty()
+        {
+            var result = ExecuteBinding("_this.GenericParams<int>()", new [] { new TestViewModel() });
+            Assert.AreEqual((0, 0), result);
+        }
+
+        [TestMethod]
+        public void BindingCompiler_GenericMethod_Params()
+        {
+            var result = ExecuteBinding("_this.GenericParams(10, 20, 30)", new [] { new TestViewModel() });
+            Assert.AreEqual((10, 3), result);
+        }
+
         [TestMethod]
         public void BindingCompiler_ComparisonOperators()
         {
@@ -1356,6 +1389,16 @@ namespace DotVVM.Framework.Tests.Binding
         public string MethodWithOverloads(string i) => i;
         public string MethodWithOverloads(DateTime i) => i.ToString();
         public int MethodWithOverloads(int a, int b) => a + b;
+
+        public T GenericDefault<T>(T something, T somethingElse = default)
+        {
+            return somethingElse;
+        }
+
+        public (T, int) GenericParams<T>(params T[] something)
+        {
+            return (something.FirstOrDefault(), something.Length);
+        }
     }
 
 

commit dc304de9b025e0f40f5987bd49c00749feab65a8

Fix staticCommand plan for unserializable parameter default values
This mainly affected CancellationToken, which is fixed in a better way
by #2100. However, same problem affects all unserializable value types.

This is regression in v5.0, probably introduced in 54676ae10

src/Framework/Framework/Compilation/Binding/MemberExpressionFactory.cs
@@ -420,10 +420,10 @@ namespace DotVVM.Framework.Compilation.Binding
             if (parameter.HasDefaultValue)
             {
                 var value = parameter.DefaultValue;
-                if (value is null && parameter.ParameterType.IsValueType)
+                if (value is null)
                 {
-                    // null with struct type means `default(T)`
-                    value = ReflectionUtils.GetDefaultValue(parameter.ParameterType);
+                    // null with struct type also means `default(T)`
+                    return Expression.Default(parameter.ParameterType);
                 }
                 return Expression.Constant(value, parameter.ParameterType);
             }

src/Framework/Framework/Compilation/Binding/StaticCommandBindingCompiler.cs
@@ -173,6 +173,7 @@ namespace DotVVM.Framework.Compilation.Binding
         Argument,
         Inject,
         Constant,
+        /// Parameter default value (not necessarily default(T))
         DefaultValue,
         Invocation
     }

src/Framework/Framework/Compilation/Binding/StaticCommandMethodTranslator.cs
@@ -75,6 +75,13 @@ namespace DotVVM.Framework.Compilation.Binding
             var argPlans = allArguments.Select((arg, index) => {
                 if (arg.OriginalExpression.GetParameterAnnotation() is BindingParameterAnnotation { ExtensionParameter:  InjectedServiceExtensionParameter service })
                     return new StaticCommandParameterPlan(StaticCommandParameterType.Inject, ResolvedTypeDescriptor.ToSystemType(service.ParameterType));
+                else if (arg.OriginalExpression is DefaultExpression)
+                {
+                    if (method.GetParameters()[index - (method.IsStatic ? 0 : 1)].DefaultValue is null)
+                        return new StaticCommandParameterPlan(StaticCommandParameterType.DefaultValue, null);
+                    else
+                        return new StaticCommandParameterPlan(StaticCommandParameterType.Constant, ReflectionUtils.GetDefaultValue(arg.OriginalExpression.Type));
+                }
                 else if (arg.OriginalExpression is ConstantExpression constant)
                 {
                     if (constant.Value == method.GetParameters()[index - (method.IsStatic ? 0 : 1)].DefaultValue)

src/Tests/Binding/StaticCommandCompilationTests.cs
@@ -13,6 +13,8 @@ using DotVVM.Framework.ViewModel;
 using Microsoft.Extensions.DependencyInjection;
 using DotVVM.Framework.Compilation.ControlTree.Resolved;
 using System.Collections.Immutable;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using DotVVM.Framework.Compilation.Javascript.Ast;
 using DotVVM.Framework.Binding;
 using DotVVM.Framework.Testing;
@@ -152,6 +154,23 @@ namespace DotVVM.Framework.Tests.Binding
         {
             var result = CompileBinding("DateFrom = DateTo", niceMode: false, typeof(TestViewModel));
             Assert.AreEqual("{let vm=options.viewModel;vm.DateFrom(dotvvm.serialization.serializeDate(vm.DateTo.state,false));}", result);
+        }
+
+        [TestMethod]
+        public void StaticCommandCompilation_DefaultStructParameterIsNotSerialized()
+        {
+            var binding = releaseHelper.StaticCommand("StaticCommands.GetValueWithUnserializableDefault()", [ typeof(TestViewModel) ]);
+            var result = BindingTestHelper.GetStaticCommandJavascriptBody(binding);
+            var plan = binding.GetProperty<StaticCommandJsAstProperty>().Expression
+                .DescendantNodesAndSelf()
+                .Select(n => n.Annotation<StaticCommandMethodTranslator.StaticCommandInvocationJsAnnotation>())
+                .OfType<StaticCommandMethodTranslator.StaticCommandInvocationJsAnnotation>()
+                .Single()
+                .Plan;
+
+            Assert.AreEqual("{await dotvvm.staticCommandPostback(\"XXXX\",[],options);}", result);
+            Assert.AreEqual(StaticCommandParameterType.DefaultValue, plan.Arguments.Single().Type);
+            Assert.AreEqual(0, plan.Method.Invoke(null, plan.Arguments.Select(a => a.Arg).ToArray()));
         }
 
         [TestMethod]
@@ -461,6 +480,21 @@ namespace DotVVM.Framework.Tests.Binding
 
         [AllowStaticCommand]
         public static DateTime GetDate() => DateTime.UtcNow;
+
+        [AllowStaticCommand]
+        public static int GetValueWithUnserializableDefault(UnserializableStruct value = default) => value.Value;
+    }
+
+    [JsonConverter(typeof(UnserializableStructJsonConverter))]
+    public struct UnserializableStruct
+    {
+        public int Value { get; set; }
+    }
+
+    public class UnserializableStructJsonConverter : JsonConverter<UnserializableStruct>
+    {
+        public override UnserializableStruct Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) => throw new NotSupportedException();
+        public override void Write(Utf8JsonWriter writer, UnserializableStruct value, JsonSerializerOptions options) => throw new NotSupportedException();
     }
 
     public abstract class TestInnerService<TOutput>